4f508cc32a1fc8a07f6b6e76c7d53402bf2a454f
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-10-24 11:24:32 +0200

    Update index.tsx with more wrapped components

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,12 @@
-import { Button, ButtonProps } from './components/Button';
+import { Button, ButtonProps } from './components/Button/Button';
+import { Checkbox } from './components/Checkbox/Checkbox';
+import { FormControlLabel } from './components/FormControlLabel/FormControlLabel'
 
 /**
  * Primary UI component for user interaction
  */
-export { Button, ButtonProps };
+export { Button,
+          ButtonProps,
+          Checkbox,
+          FormControlLabel
+        };
