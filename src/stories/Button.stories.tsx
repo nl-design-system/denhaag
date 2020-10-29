7286f42dd025d16f7556fbaba2b219b05a2bffca
Jeroen de Klerk <jeroen.deklerk@hotmail.com>
2020-10-29 14:12:20 +0100

    Ran Linter on story file.

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,48 +1,53 @@
-import React from 'react';
+import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 import Button, { ButtonProps } from '../components/Button/Button';
 
 export default {
   title: 'Components/Button',
-  component: Button,
-} as Meta;
+  component: Button
+} as Meta
 
-const Template: Story<ButtonProps> = (args: any) => <Button {...args}>Button</Button>;
+const Template: Story<ButtonProps> = (args: any) => (
+  <Button {...args}>Button</Button>
+)
 
 /**
  * Default Button
  */
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
+  onClick: () => {
+    action('Default button clicked')
+  },
   color: 'default',
-  variant: 'contained',
-};
+  variant: 'contained'
+}
 
 /**
  * Primary Button
  */
-export const Primary : Story<ButtonProps> = Template.bind({});
+export const Primary: Story<ButtonProps> = Template.bind({})
 Primary.args = {
   color: 'primary',
   variant: 'contained'
-};
+}
 
 /**
  * Secondary Button
  */
-export const Secondary: Story<ButtonProps> = Template.bind({});
+export const Secondary: Story<ButtonProps> = Template.bind({})
 Secondary.args = {
   color: 'secondary',
-  variant: 'contained',
-};
+  variant: 'contained'
+}
 
 /**
  * Disabled Button
  */
-export const Disabled: Story<ButtonProps> = Template.bind({});
+export const Disabled: Story<ButtonProps> = Template.bind({})
 Disabled.args = {
   color: 'default',
   variant: 'contained',
-  disabled: true,
-};
+  disabled: true
+}
