08883df4680df281e61aee209ec2619142a91fde
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-11-05 15:10:26 +0100

    Add formgroup component

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import { Button, ButtonProps } from './components/Button/Button';
-import { Checkbox, CheckboxProps } from './components/Checkbox/Checkbox';
-import { FormControlLabel, FormControlLabelProps } from './components/FormControlLabel/FormControlLabel'
+import Button, { ButtonProps } from './components/Button/Button'
+import Checkbox, { CheckboxProps } from './components/Checkbox/Checkbox'
+import FormControlLabel, { FormControlLabelProps } from './components/FormControlLabel/FormControlLabel'
+import FormGroup, { FormGroupProps } from './components/FormGroup/FormGroup'
 
 /**
  * Primary UI component for user interaction
  */
-export { Button,
-          ButtonProps,
-          Checkbox,
-          CheckboxProps,
-          FormControlLabel,
-          FormControlLabelProps
-        };
+export {
+  Button,
+  ButtonProps,
+  Checkbox,
+  CheckboxProps,
+  FormControlLabel,
+  FormControlLabelProps,
+  FormGroup
+}
