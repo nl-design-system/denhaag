59ee51a1768678db6e9dedcef38b4b09f67f2a95
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-11-05 15:16:53 +0100

    Added href attribute to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -11,6 +11,11 @@ export interface ButtonProps {
    */
   color?: 'primary' | 'secondary' | 'default'
   /**
+   * The url to link to when the button is clicked.
+   * If defined, an a element will be used as the root node.
+   */
+  href?: string
+  /**
    * Size of the component
    */
   size?: 'small' | 'medium' | 'large'
@@ -33,6 +38,7 @@ export const Button: React.FC<ButtonProps> = ({
   variant = 'contained',
   onClick,
   size,
+  href,
   disabled = false,
   children,
   ...props
@@ -43,6 +49,7 @@ export const Button: React.FC<ButtonProps> = ({
       variant={variant}
       color={color}
       size={size}
+      href={href}
       disabled={disabled}
       {...props}
     >
