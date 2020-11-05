8b2a09d41a0d766881296536100ab4a54d8a0187
Jeroen de Klerk <jeroen.deklerk@hotmail.com>
2020-11-05 16:38:56 +0100

    Removed redundant curly braces.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from './components/Button/Button'
-import { Checkbox } from './components/Checkbox/Checkbox'
-import { FormControlLabel } from './components/FormControlLabel/FormControlLabel'
-import { Box } from './components/layout/Box/Box'
-import { Container } from './components/layout/Container/Container'
-import { Grid } from './components/layout/Grid/Grid'
-import { GridList } from './components/layout/GridList/GridList'
-import { Hidden } from './components/layout/Hidden/Hidden'
+import Button from './components/Button/Button'
+import Checkbox from './components/Checkbox/Checkbox'
+import FormControlLabel from './components/FormControlLabel/FormControlLabel'
+import Box from './components/layout/Box/Box'
+import Container from './components/layout/Container/Container'
+import Grid from './components/layout/Grid/Grid'
+import GridList from './components/layout/GridList/GridList'
+import Hidden from './components/layout/Hidden/Hidden'
 
 /**
  * Primary UI component for user interaction
