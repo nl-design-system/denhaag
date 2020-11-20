b20d274b62532b841bdae69d0a6b8b069d99c571
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-11-20 11:30:26 +0100

    Fixed imports DatePicker Co-authored-by: Maarten Carsjens <maarten.carsjens@denhaag.nl>

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
-import DatePicker, { DatePickerProps, PickersUtilsProvider } from '../components/DatePicker/DatePicker'
+import { DatePicker, DatePickerProps } from '../components/DatePicker/DatePicker'
+import { PickersUtilsProvider } from '../components/PickersUtilsProvider/PickersUtilsProvider'
 import DateFnsUtils from '@date-io/date-fns'
 import pkg from '../components/DatePicker/package.json'
 
