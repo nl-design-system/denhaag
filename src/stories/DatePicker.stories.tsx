d7d250bafb51dcb87de12eb55b98b896c06f9d36
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-11-20 15:26:26 +0100

    Modified location of PickerUtilsProvider

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { DatePicker, DatePickerProps } from '../components/DatePicker/DatePicker'
-import { PickersUtilsProvider } from '../components/PickersUtilsProvider/PickersUtilsProvider'
+import { DatePicker, DatePickerProps } from '../components/Pickers/DatePicker/DatePicker'
+import { PickersUtilsProvider } from '../components/Pickers/PickersUtilsProvider/PickersUtilsProvider'
 import DateFnsUtils from '@date-io/date-fns'
-import pkg from '../components/DatePicker/package.json'
+import pkg from '../components/Pickers/DatePicker/package.json'
 
 export default {
   title: 'Components/DatePicker',
@@ -16,7 +16,7 @@ export default {
 
 const Template: Story<DatePickerProps> = (args: any) => (
   <PickersUtilsProvider utils={DateFnsUtils}>
-    <DatePicker value='30-08-1997' onChange={null} />
+    <DatePicker value='1970-01-01' onChange={null} />
   </PickersUtilsProvider>
 )
 
@@ -24,5 +24,4 @@ const Template: Story<DatePickerProps> = (args: any) => (
  * Default Checkbox
  */
 export const Default: Story<DatePickerProps> = Template.bind({})
-Default.args = {
-}
+Default.args = {}
