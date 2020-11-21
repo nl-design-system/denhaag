514c31a87fbee9485b82426edfb88624ca4521cb
Bryan de Jong <bryandejong99@gmail.com>
2020-11-21 13:48:57 +0100

    Change formgroup version definition in compoennts/package.json from ^0.1 to ^0.1.0

--- src/stories/forms/Switch.stories.tsx
+++ src/stories/forms/Switch.stories.tsx
@@ -1,35 +1,43 @@
 import React from 'react'
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0'
+import { Meta, Story } from '@storybook/react'
+import pkg from '../../components/package.json'
 import Switch, { SwitchProps } from '../../components/forms/Switch/Switch'
 import { FormControlLabel } from '../../components/FormControlLabel/FormControlLabel'
 
 export default {
   title: 'Components/Forms/Switch',
+  parameters: {
+    componentSubtitle: `${pkg.name} - ${pkg.version}`
+  },
   component: Switch
 } as Meta
 
 const Template: Story<SwitchProps> = (args: any) => {
-  return <FormControlLabel label='Switch' control={<Switch {...args} />} />
+  return (
+    <div>
+      <FormControlLabel
+        label='Default checked switch'
+        control={<Switch defaultChecked {...args} />}
+      />
+      <FormControlLabel
+        label='Unchecked switch'
+        control={<Switch {...args} />}
+      />
+    </div>
+  )
 }
 
 /**
  * Default Switch
  */
 export const Default: Story<SwitchProps> = Template.bind({})
-Default.args = {
-  color: 'default',
-  disabled: false
-}
 
 /**
  * Primary Switch
  */
 export const Primary: Story<SwitchProps> = Template.bind({})
 Primary.args = {
-  color: 'primary',
-  defaultChecked: true,
-  disabled: false
+  color: 'primary'
 }
 
 /**
@@ -37,9 +45,7 @@ Primary.args = {
  */
 export const Secondary: Story<SwitchProps> = Template.bind({})
 Secondary.args = {
-  color: 'secondary',
-  defaultChecked: true,
-  disabled: false
+  color: 'secondary'
 }
 
 /**
