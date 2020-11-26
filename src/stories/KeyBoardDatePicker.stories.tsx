eaaafec8a1d081bb49786ca311f4c4164f897925
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-11-26 13:23:28 +0100

    Added KeyboardDatePicker Story to storybook

--- src/stories/KeyBoardDatePicker.stories.tsx
+++ src/stories/KeyBoardDatePicker.stories.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { DatePicker, DatePickerProps } from '../components/Pickers/DatePicker/DatePicker'
+import { KeyboardDatePicker, KeyboardDatePickerProps } from '../components/Pickers/KeyboardDatePicker/KeyboardDatePicker'
 import { PickersUtilsProvider } from '../components/Pickers/PickersUtilsProvider/PickersUtilsProvider'
 import DateFnsUtils from '@date-io/date-fns'
-import pkg from '../components/Pickers/DatePicker/package.json'
+import pkg from '../components/Pickers/KeyboardDatePicker/package.json'
 
 export default {
-  title: 'Components/Pickers/DatePicker',
+  title: 'Components/Pickers/KeyboardDatePicker',
   parameters: {
     componentSubtitle: `${pkg.name} - ${pkg.version}`
   },
-  component: DatePicker
+  component: KeyboardDatePicker
 } as Meta
 
-const Template: Story<DatePickerProps> = (args: any) => (
+const Template: Story<KeyboardDatePickerProps> = (args: any) => (
   <PickersUtilsProvider utils={DateFnsUtils}>
-    <DatePicker value='1970-01-01' onChange={null} />
+    <KeyboardDatePicker value='1970-01-01' onChange={null} />
   </PickersUtilsProvider>
 )
 
