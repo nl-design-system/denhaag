c06f0b58f2a915f0b875b8ea5358be4335db4741
Bryan de Jong <bryan.dejong@denhaag.nl>
2020-11-28 12:17:18 +0100

    Add spacing in StandardTextFieldProps between props and docs

--- src/components/forms/TextField/StandardTextFieldProps.ts
+++ src/components/forms/TextField/StandardTextFieldProps.ts
@@ -3,6 +3,7 @@ import { BaseTextFieldProps } from './BaseTextFieldProps'
 
 export interface StandardTextFieldProps extends BaseTextFieldProps {
   onBlur?: InputProps['onBlur']
+
   /**
    * Callback fired when the value is changed.
    *
@@ -10,7 +11,9 @@ export interface StandardTextFieldProps extends BaseTextFieldProps {
    * You can pull out the new value by accessing `event.target.value` (string).
    */
   onChange?: InputProps['onChange']
+
   onFocus?: InputProps['onFocus']
+
   /**
    * Props applied to the Input element.
    * It will be a [`FilledInput`](/api/filled-input/),
@@ -18,6 +21,7 @@ export interface StandardTextFieldProps extends BaseTextFieldProps {
    * component depending on the `variant` prop value.
    */
   InputProps?: Partial<InputProps>
+
   /**
    * [Attributes](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input#Attributes) applied to the `input` element.
    */
