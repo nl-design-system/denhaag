115d2dd6749a017c85137ed2da9e49a5541d5491
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-12-05 14:08:55 +0100

    Add space between properties

--- src/components/Pickers/BaseDatePickerProps/BaseDatePickerProps.tsx
+++ src/components/Pickers/BaseDatePickerProps/BaseDatePickerProps.tsx
@@ -18,6 +18,7 @@ export interface BaseDatePickerProps {
    * Onchange callback @DateIOType
    */
   onChange: (date: any) => void
+
   /**
    * Picker value
    */
@@ -26,130 +27,162 @@ export interface BaseDatePickerProps {
    * Enables keyboard listener for moving between days in calendar
    */
   allowKeyBoardControl?: boolean
+
   /**
    * To animate scrolling to current year (using scrollIntoView)
    */
   animateYearScrolling?: boolean
+
   /**
    * Auto accept date on selection
    */
   AutoOk?: boolean
+
   /**
    * Disable picker and text field
    */
   disabled?: boolean
+
   /**
    * Disables future dates
    */
   disableFuture?: boolean
+
   /**
    * Disables past dates
    */
   disablePast?: boolean
+
   /**
    * Hide toolbar and show only date/time views
    */
   disableToolbar?: boolean
+
   /**
    * Message displaying in text field, if null passed (doesn't work in keyboard mode)
    */
   emptyLabel?: string
+
   /**
    * Format string
    */
   format?: string
+
   /**
    * Date that will be initially highlighted if null was passed
    */
   initialFocusedDate?: ParsableDate
+
   /**
    * Pass material-ui text field variant down, bypass internal variant prop
    */
   inputVariant?: 'standard' | 'outlined' | 'filled'
+
   /**
    * Message, appearing when date cannot be parsed
    */
   invalidDateMessage?: ReactNode
+
   /**
    * 	Message displaying in text field if date is invalid (doesn't work in keyboard mode)
    */
   invalidLabel?: string
+
   /**
    * Dynamic formatter of text field value
    */
   labelFunc?: (date: DateIOType, invalidLabel: string) => string
+
   /**
    * Props to pass to left arrow button
    */
   leftArrowButtonProps?: Partial<IconButtonProps>
+
   /**
    * Left arrow icon
    */
   leftArrowIcon?: ReactNode
+
   /**
    * Custom loading indicator
    */
   loadingIndicator?: ReactElement
+
   /**
    * Max selectable date
    */
   maxDate?: ParsableDate
+
   /**
    * Error message, shown if date is more then maximal date
    */
   maxDateMessage?: ReactNode
+
   /**
    * Min selectable date
    */
   minDate?: ParsableDate
+
   /**
    * Error message, showf if date is less than minimal date
    */
   minDateMessage?: ReactNode
+
   /**
    * Callback fired when date is accepted
    */
   onAccept?: (date: DateIOType) => void
+
   /**
    * On close callback
    */
   onClose?: () => void
+
   /**
    * Callback fired when new error should be displayed (!! This is a side effect. Be careful if you want to rerender the component)
    */
   onError?: (error: ReactNode, value: DateIOType) => void
+
   /**
    * Callback firing on month change. Return promise to render spinner till it will not be resolved
    */
   onMonthChange?: (date: DateIOType) => void
+
   /**
    * Callback fired when the picker is opened
    */
   onOpen?: () => void
+
   /**
    * Callback fired when the year changes
    */
   onYearChange?: (date: DateIOType) => void
+
   /**
    * Controls picker open state
    */
   open?: boolean
+
   /**
    * First view to show in DatePicker
    */
   openTo?: 'date' | 'year' | 'month'
+
   /**
    * Force rendering in particular orientation
    */
   orientation?: 'portrait' | 'landscape'
+
   /**
    * Popover props passed to material-ui Popover (with variant="inline")
    */
   PopoverProps?: Partial<PopoverProps>
+
   /**
    * Make picker readonly
    */
   readOnly?: boolean
+
   /**
    * Custom renderer for day
    */
@@ -159,28 +192,34 @@ export interface BaseDatePickerProps {
     dayInCurrentMonth: boolean,
     dayComponent: ReactElement
   ) => ReactElement
+
   /**
    * 	Props to pass to right arrow button
    */
   rightArrowButtonProps?: Partial<IconButtonProps>
+
   /**
    * Right arrow icon
    */
   rightArrowIcon?: ReactNode
+
   /**
    * Disable specific date
    */
   shouldDisableDate?: (date: DateIOType) => boolean
+
   /**
    * Compare dates by the exact timestamp, instead of start/end of date
    */
   strictCompareDates?: boolean
+
   /**
    * Override input component
    */
   TextFieldComponent?:
     | ComponentClass<TextFieldProps, any>
     | FunctionComponent<TextFieldProps>
+
   /**
    * Component that will replace default toolbar renderer
    */
@@ -192,6 +231,7 @@ export interface BaseDatePickerProps {
    * Picker container option
    */
   variant?: 'dialog' | 'inline' | 'static'
+
   /**
    * Array of views to show
    */
--- src/components/Pickers/KeyboardDatePicker/KeyboardDatePicker.tsx
+++ src/components/Pickers/KeyboardDatePicker/KeyboardDatePicker.tsx
@@ -9,34 +9,42 @@ export interface KeyboardDatePickerProps extends BaseDatePickerProps {
    * 	Keyboard onChange callback
    */
   onChange: (date: DateIOType, value?: string | null) => void
+
   /**
    * 	Props to pass to keyboard input adornment
    */
   inputAdormentProps?: Partial<InputAdornmentProps>
+
   /**
    * String value for controlling value with pure input string. Overrides value prop.
    */
   inputValue?: string
+
   /**
    * Props to pass to keyboard adornment button
    */
   KeyboardButtonProps?: Partial<IconButtonProps>
+
   /**
    * Icon displaying for open picker button
    */
   keyboardIcon?: ReactNode
+
   /**
    * Custom mask. Can be used to override generate from format. (e.g. __/__/____ __:__)
    */
   mask?: string
+
   /**
    * Char string that will be replaced with number (for "_" mask will be "__/__/____")
    */
   maskChar?: string
+
   /**
    * Refuse values regexp
    */
   refuse?: RegExp
+
   /**
    * Custom formatter to be passed into Rifm component
    */
