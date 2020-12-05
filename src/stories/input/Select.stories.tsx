dab080c256e39e5502c9d932a86bc064a99e4fb4
Bryan de Jong <bryan.dejong@denhaag.nl>
2020-12-05 13:02:55 +0100

    Wrap select component with story

---
+++ src/stories/input/Select.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Meta } from '@storybook/react'
+import pkg from '../../components/input/Select/package.json'
+import Select, { SelectProps } from '../../components/input/Select/Select'
+import {
+  createStyles,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Theme
+} from '@material-ui/core'
+
+export default {
+  title: 'Components/Input/Select',
+  parameters: {
+    componentSubtitle: `${pkg.name} - ${pkg.version}`
+  },
+  component: Select
+} as Meta
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120
+    }
+  })
+)
+
+export function DefaultSelect(props: SelectProps): React.ReactElement {
+  const classes = useStyles()
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id='default-select-label'>Amount</InputLabel>
+      <Select labelId='default-select-label' {...props}>
+        <MenuItem value={10}>10</MenuItem>
+        <MenuItem value={20}>20</MenuItem>
+        <MenuItem value={30}>30</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export function NativeSelect(props: SelectProps): React.ReactElement {
+  const classes = useStyles()
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id='default-select-label'>Amount</InputLabel>
+      <Select native labelId='default-select-label' {...props}>
+        <option aria-label='None' value='' />
+        <option value={10}>10</option>
+        <option value={20}>20</option>
+        <option value={30}>30</option>
+      </Select>
+    </FormControl>
+  )
+}
