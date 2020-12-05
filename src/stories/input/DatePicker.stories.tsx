07444b44ee9c624fed6c380b504dea8c46cbe682
Joost van Viegen <joost.vanviegen@denhaag.nl>
2020-12-05 14:58:29 +0100

    Moved Datpicker stories to input

--- src/stories/input/DatePicker.stories.tsx
+++ src/stories/input/DatePicker.stories.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
-import  DatePicker, { DatePickerProps } from '../components/input/Pickers/DatePicker/DatePicker'
-import { PickersUtilsProvider } from '../components/input/Pickers/PickersUtilsProvider/PickersUtilsProvider'
+import DatePicker, {
+  DatePickerProps
+} from '../../components/input/Pickers/DatePicker/DatePicker'
+import { PickersUtilsProvider } from '../../components/input/Pickers/PickersUtilsProvider/PickersUtilsProvider'
 import DateFnsUtils from '@date-io/date-fns'
-import pkg from '../components/input/Pickers/package.json'
+import pkg from '../../components/input/Pickers/package.json'
 
 export default {
   title: 'Components/Pickers/DatePicker',
--- src/stories/input/KeyBoardDatePicker.stories.tsx
+++ src/stories/input/KeyBoardDatePicker.stories.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { KeyboardDatePicker, KeyboardDatePickerProps } from '../components/Pickers/KeyboardDatePicker/KeyboardDatePicker'
-import { PickersUtilsProvider } from '../components/Pickers/PickersUtilsProvider/PickersUtilsProvider'
+import {
+  KeyboardDatePicker,
+  KeyboardDatePickerProps
+} from '../../components/input/Pickers/KeyboardDatePicker/KeyboardDatePicker'
+import { PickersUtilsProvider } from '../../components/input/Pickers/PickersUtilsProvider/PickersUtilsProvider'
 import DateFnsUtils from '@date-io/date-fns'
-import pkg from '../components/Pickers/KeyboardDatePicker/package.json'
+import pkg from '../../components/input/Pickers/package.json'
 
 export default {
   title: 'Components/Pickers/KeyboardDatePicker',
