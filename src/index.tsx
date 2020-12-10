d2265e639e94be0f5b86d636e400f7038a232b92
Maarten de Klerk <maarten@deklerk.dev>
2020-12-10 15:49:19 +0100

    Organized index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,61 +1,82 @@
+/**
+ * Data display components
+ */
+import Typography from './components/datadisplay/Typography/Typography'
+import Divider from './components/datadisplay/Divider/Divider'
+/**
+ * Input components
+ */
 import Button from './components/input/Button/Button'
 import ButtonGroup from './components/input/ButtonGroup/ButtonGroup'
 import Checkbox from './components/input/Checkbox/Checkbox'
-import Radio from './components/input/Radio/Radio'
-import Switch from './components/input/Switch/Switch'
-import Select from './components/input/Select/Select'
 import FormControlLabel from './components/input/FormControlLabel/FormControlLabel'
 import FormGroup from './components/input/FormGroup/FormGroup'
+import Radio from './components/input/Radio/Radio'
+import Select from './components/input/Select/Select'
+import Switch from './components/input/Switch/Switch'
+import TextField from './components/input/TextField/TextField'
+/**
+ * Layout components
+ */
 import Box from './components/layout/Box/Box'
 import Container from './components/layout/Container/Container'
 import Grid from './components/layout/Grid/Grid'
 import GridList from './components/layout/GridList/GridList'
 import Hidden from './components/layout/Hidden/Hidden'
-import Accordion from './components/surfaces/Accordion/Accordion'
-import AppBar from './components/surfaces/AppBar/AppBar'
-import Typography from './components/datadisplay/Typography/Typography'
-import Divider from './components/datadisplay/Divider/Divider'
-import TextField from './components/input/TextField/TextField'
+/**
+ * Navigation components
+ */
+import Drawer from './components/navigation/Drawer/Drawer'
 import Menu from './components/navigation/Menu/Menu'
-import Stepper from './components/navigation/Stepper/Stepper'
+import MenuItem from './components/navigation/MenuItem/MenuItem'
+import MenuList from './components/navigation/MenuList/MenuList'
 import MobileStepper from './components/navigation/MobileStepper/MobileStepper'
 import Popover from './components/navigation/Popover/Popover'
 import Popper from './components/navigation/Popper/Popper'
-import Tabs from './components/navigation/Tabs/Tabs'
-import Drawer from './components/navigation/Drawer/Drawer'
+import Stepper from './components/navigation/Stepper/Stepper'
 import SwipeableDrawer from './components/navigation/SwipeableDrawer/SwipeableDrawer'
-import MenuItem from './components/navigation/MenuItem/MenuItem'
-import MenuList from './components/navigation/MenuList/MenuList'
+import Tabs from './components/navigation/Tabs/Tabs'
+/**
+ * Surface components
+ */
+import Accordion from './components/surfaces/Accordion/Accordion'
+import AppBar from './components/surfaces/AppBar/AppBar'
+
 /**
  * Primary UI component for user interaction
  */
 export {
+  // Data display
+  Divider,
+  Typography,
+  // Input
   Button,
   ButtonGroup,
   Checkbox,
-  Radio,
-  Switch,
-  Select,
-  TextField,
   FormControlLabel,
   FormGroup,
+  Radio,
+  Select,
+  Switch,
+  TextField,
+  // Layout
   Box,
   Container,
   Grid,
   GridList,
   Hidden,
+  // Navigation
+  Drawer,
   Menu,
   MenuItem,
   MenuList,
-  Stepper,
   MobileStepper,
   Popover,
   Popper,
-  Tabs,
-  Drawer,
+  Stepper,
   SwipeableDrawer,
+  Tabs,
+  // Surface
   Accordion,
-  AppBar,
-  Typography,
-  Divider
+  AppBar
 }
