1828c63bb0e22b771b90cca7afe97bc53f349468
Maarten <m.f.deklerk@students.uu.nl>
2020-12-10 13:37:40 +0100

    Added props to Radio component

---
+++ src/components/input/Radio/Radio.tsx
@@ -0,0 +1,66 @@
+import MaterialRadio from '@material-ui/core/Radio'
+import React from 'react'
+
+export interface RadioProps {
+  /**
+   * Id of the input element.
+   */
+  id?: string
+  /**
+   * **Signature**: `function(event: object) => void`
+   * The event source of the callback. You can pull out the new
+   * value by accessing `event.target.value` (string). You can
+   * pull out the new checked state by accessing
+   * `event.target.checked` (boolean).
+   */
+  onChange?: () => void
+  /**
+   * Name attribute of the radio button.
+   */
+  name?: string
+  /**
+   * The value of the component. The DOM casts it to `string`.
+   */
+  value?: any
+  /**
+   * If `true`, the component is checked.
+   */
+  checked?: boolean
+  /**
+   * The Color of the component. @default 'secondary'.
+   */
+  color?: 'primary' | 'secondary' | 'default'
+  /**
+   * If `true`, the radio but will be disabled.
+   */
+  disabled?: boolean
+  /**
+   * If `true`, the ripple effect will be disabled.
+   */
+  disableRipple?: boolean
+  /**
+   * If `true`, the input will be required.
+   */
+  required?: boolean
+  /**
+   * The size of the radio button. @default 'medium'.
+   */
+  size?: 'medium' | 'small'
+  /**
+   * The icon to display when the component is checked.
+   */
+  checkedIcon?: React.ReactNode
+  /**
+   * The icon to display when the component is unchecked.
+   */
+  icon?: React.ReactNode
+}
+
+export const Radio: React.FC<RadioProps> = (props: RadioProps) => {
+  return <MaterialRadio {...props} />
+}
+
+/**
+ * Default export for Radio
+ */
+export default Radio
