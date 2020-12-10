6d44242aaded4a8d2c529c43db6efb5b6b97f6fe
Maarten <m.f.deklerk@students.uu.nl>
2020-12-10 14:46:49 +0100

    Added empty Radiogroup component

---
+++ src/components/input/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,10 @@
+import MaterialRadioGroup from '@material-ui/core/RadioGroup'
+import React from 'react'
+
+export interface RadioGroupProps {}
+
+export const RadioGroup: React.FC<RadioGroupProps> = () => {
+  return <MaterialRadioGroup />
+}
+
+export default RadioGroup
