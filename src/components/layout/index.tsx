1eb010444f5a7c6fed4486b2dd09077174bae517
Maarten de Klerk <maarten@deklerk.dev>
2021-01-16 14:58:19 +0100

    Add index.tsx to group components

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,7 @@
+export * from '@gemeente-denhaag/baselayoutprops'
+export * from '@gemeente-denhaag/box'
+export * from '@gemeente-denhaag/container'
+export * from '@gemeente-denhaag/grid'
+export * from '@gemeente-denhaag/gridlist'
+export * from '@gemeente-denhaag/gridlisttile'
+export * from '@gemeente-denhaag/gridlisttilebar'
