bd6fb674169a1d7c06fdaeb4bbaf3422f1afb593
DanielvanVliet <danielvvliet@hotmail.com>
2021-03-06 12:09:04 +0100

    pickers explicit import of defaults fix

--- src/components/Pickers/index.tsx
+++ src/components/Pickers/index.tsx
@@ -1,3 +1,12 @@
+import BaseDatePickerProps from './BaseDatePickerProps/BaseDatePickerProps';
+import DatePicker from './DatePicker/DatePicker';
+import KeyboardDatePicker from './KeyboardDatePicker/KeyboardDatePicker';
+
+// these default exports must be exported explicitly
+export type { BaseDatePickerProps }
+export type { DatePicker }
+export type { KeyboardDatePicker }
+
 export * from './BaseDatePickerProps/BaseDatePickerProps';
 export * from './DatePicker/DatePicker';
 export * from './KeyboardDatePicker/KeyboardDatePicker';
