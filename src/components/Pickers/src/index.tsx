1f5c959d39c9be0db3f0b5875f080304780b7b97
DanielvanVliet <danielvvliet@hotmail.com>
2021-04-14 16:04:12 +0200

    picker potential fix

---
+++ src/components/Pickers/src/index.tsx
@@ -0,0 +1,18 @@
+// import type { BaseDatePickerProps } from "./BaseDatePickerProps/BaseDatePickerProps";
+// import from "./DatePicker/DatePicker";
+// import from "./KeyboardDatePicker/KeyboardDatePicker";
+
+// export type { BaseDatePickerProps } from "./BaseDatePickerProps/BaseDatePickerProps";
+
+import { DatePickerProps, DatePicker } from "./DatePicker/DatePicker";
+import { BaseDatePickerProps } from "./BaseDatePickerProps/BaseDatePickerProps";
+import { KeyboardDatePickerProps, KeyboardDatePicker } from "./KeyboardDatePicker/KeyboardDatePicker";
+
+export { BaseDatePickerProps };
+export { DatePickerProps, DatePicker };
+export { KeyboardDatePickerProps, KeyboardDatePicker };
+
+// export type { DatePickerProps, DatePicker } from "./DatePicker/DatePicker";
+
+// export type { KeyboardDatePickerProps, KeyboardDatePicker } from "./KeyboardDatePicker/KeyboardDatePicker";
+
