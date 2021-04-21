393c7ad8f6e48bdef6abedd61daedf54c01f782a
Bryan de Jong <bryandejong99@gmail.com>
2021-04-21 15:52:19 +0200

    Enable storybook design token plugin for globals.module.css

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,5 +4,5 @@ module.exports = {
     '../src/stories/*.stories.@(js|jsx|ts|tsx)',
     '../src/stories/*/*.stories.@(js|jsx|ts|tsx)',
   ],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', 'storybook-design-token'],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+const tokenContext = require.context('!!raw-loader!../src', true, /.\.(css|less|scss|svg)$/);
+
+const tokenFiles = tokenContext.keys().map((filename) => {
+  return { filename: filename, content: tokenContext(filename).default };
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  designToken: {
+    files: tokenFiles
+  }
 };
