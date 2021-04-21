529b4f82704dd2700ebec1d64a8c7f8c1b4d89d0
Maarten de Klerk <maarten@deklerk.dev>
2021-04-21 10:39:41 +0200

    remove old paths

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
   stories: [
-    // '../src/stories/*.stories.mdx',
-    // '../src/stories/*.stories.@(js|jsx|ts|tsx)',
-    // '../src/stories/*/*.stories.@(js|jsx|ts|tsx)',
     './stories/*.stories.mdx',
     '../src/components/*/*.stories.mdx',
     '../src/components/*/src/*.stories.@(js|jsx|ts|tsx)'
