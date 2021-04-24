49808df9258f0717ac3fafc9118495cd883f8a20
Jeroen <jtech.work@protonmail.com>
2021-04-24 12:53:33 +0200

    Defined custom code in separate variable to fix indentation for surface components.

--- src/stories/surfaces/AppBar.stories.tsx
+++ src/stories/surfaces/AppBar.stories.tsx
@@ -1,118 +1,123 @@
-import React from 'react'
-import { Meta, Story } from '@storybook/react'
-import MenuIcon from '@material-ui/icons/Menu'
-import { AppBar, AppBarProps } from '../../components/AppBar'
-import pkg from '../../components/AppBar/package.json'
-import Toolbar from '../../components/Toolbar/Toolbar'
-import IconButton from '../../components/IconButton'
-import Typography from '../../components/Typography'
-import Button from '../../components/Button'
+import React from "react";
+import { Meta, Story } from "@storybook/react";
+import MenuIcon from "@material-ui/icons/Menu";
+import { AppBar, AppBarProps } from "../../components/AppBar";
+import pkg from "../../components/AppBar/package.json";
+import Toolbar from "../../components/Toolbar/Toolbar";
+import IconButton from "../../components/IconButton";
+import Typography from "../../components/Typography";
+import Button from "../../components/Button";
 
 export default {
-  title: 'Components/Surfaces/AppBar',
+  title: "Components/Surfaces/AppBar",
   parameters: {
     componentSubtitle: `${pkg.name} - ${pkg.version}`,
     docs: {
       source: {
-        type: 'dynamic'
+        type: "dynamic"
       }
     }
   },
   component: AppBar
-} as Meta
+} as Meta;
 
 const Template: Story<AppBarProps> = (args: any) => {
   return (
     <AppBar {...args}>
       <Toolbar>
-        <IconButton edge='start' color='inherit' aria-label='menu'>
+        <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant='h6'>News</Typography>
+        <Typography variant="h6">News</Typography>
       </Toolbar>
       <Button>I am a button!</Button>
     </AppBar>
-  )
-}
+  );
+};
+
+// language=JS
+const defaultCode = `
+  <AppBar position='relative'>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon/>
+      </IconButton>
+      <Typography variant="h6">News</Typography>
+    </Toolbar>
+    <Button>I am a button!</Button>
+  </AppBar>
+`;
 
 /**
  * Implementation of AppBar
  */
-export const Default = Template.bind({})
+export const Default = Template.bind({});
 Default.args = {
-  position: 'relative'
-}
+  position: "relative"
+};
 
 Default.parameters = {
   docs: {
     source: {
-      // language=JS
-      code: `
-        <AppBar position='relative'>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon/>
-            </IconButton>
-            <Typography variant="h6">News</Typography>
-          </Toolbar>
-          <Button>I am a button!</Button>
-        </AppBar>
-      `
+      code: defaultCode
     }
   }
-}
+};
+
+// language=JS
+const staticCode = `
+  <AppBar position='static'>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon/>
+      </IconButton>
+      <Typography variant="h6">News</Typography>
+    </Toolbar>
+    <Button>I am a button!</Button>
+  </AppBar>
+`;
 
 /**
  * Statically positioned AppBar
  */
-export const StaticPosition = Template.bind({})
+export const StaticPosition = Template.bind({});
 StaticPosition.args = {
-  position: 'static'
-}
+  position: "static"
+};
 
 StaticPosition.parameters = {
   docs: {
     source: {
-      // language=JS
-      code: `
-        <AppBar position='static'>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon/>
-            </IconButton>
-            <Typography variant="h6">News</Typography>
-          </Toolbar>
-          <Button>I am a button!</Button>
-        </AppBar>
-      `
+      code: staticCode
     }
   }
-}
+};
+
+// language=JS
+const secondaryCode = `
+  <AppBar position='static' color={'secondary'}>
+    <Toolbar>
+      <IconButton edge="start" color="inherit" aria-label="menu">
+        <MenuIcon/>
+      </IconButton>
+      <Typography variant="h6">News</Typography>
+    </Toolbar>
+    <Button>I am a button!</Button>
+  </AppBar>
+`;
 
 /**
  * A statically coloured AppBar
  */
-export const SecondaryColoured = Template.bind({})
+export const SecondaryColoured = Template.bind({});
 SecondaryColoured.args = {
-  position: 'static',
-  color: 'secondary'
-}
-
+  position: "static",
+  color: "secondary"
+};
 SecondaryColoured.parameters = {
   docs: {
     source: {
-      // language=JS
-      code: `
-        <AppBar position='static' color={'secondary'}>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <MenuIcon/>
-            </IconButton>
-            <Typography variant="h6">News</Typography>
-          </Toolbar>
-          <Button>I am a button!</Button>
-        </AppBar>
-      `
+      code: secondaryCode
     }
   }
-}
+};
