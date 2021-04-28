25c2723caf201646ad467d107f336b9c2785a05c
Maarten de Klerk <maarten@deklerk.dev>
2021-04-28 17:22:49 +0200

    add -e flag to shebang

--- scripts/prettify_components.sh
+++ scripts/prettify_components.sh
@@ -1,20 +1,20 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # Add this as a shell script to your WebStorm configurations
 # and as executable command to VS Code
 # Make sure the working directory for this command is equal to "<PROJECT_DIR>/src/components"
 
 # The path to the prettier config file.
-CONFIG_FILE="../../.prettierrc"
+CONFIG_FILE=".prettierrc"
 
 # The path to the prettier ignore file.
-IGNORE_FILE="../../.prettierignore"
+IGNORE_FILE=".prettierignore"
 
 # The regex for the exact files to be handled by prettifier.
 # ** = all (sub-)directories in the current working directory.
 # *.tsx = all files which are appended with ".tsx" in the specified directory.
 COMPONENTS_DIR_REGEX="./**/*.tsx"
 
-prettier --config $CONFIG_FILE --ignore-path $IGNORE_FILE all --write "$COMPONENTS_DIR_REGEX"
+yarn prettier --config $CONFIG_FILE --ignore-path $IGNORE_FILE all --write "$COMPONENTS_DIR_REGEX"
 
 # After this command is run, ensure to open all the components at least once for the changes to actually be applied and registered by the VC diff manager.
