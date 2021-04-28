83826401b9e3b2a53413febb00a6561daa953684
Maarten de Klerk <maarten@deklerk.dev>
2021-04-28 17:24:41 +0200

    Revert "add -e flag to shebang"
    This reverts commit 25c2723caf201646ad467d107f336b9c2785a05c.

--- scripts/prettify_stories.sh
+++ scripts/prettify_stories.sh
@@ -1,20 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
 
 # Add this as a shell script to your WebStorm configurations
 # and as executable command to VS Code
 # Make sure the working directory for this command is equal to "<PROJECT_DIR>/src/stories"
 
 # The path to the prettier config file.
-CONFIG_FILE=".prettierrc"
+CONFIG_FILE="../../.prettierrc"
 
 # The path to the prettier ignore file.
-IGNORE_FILE=".prettierignore"
+IGNORE_FILE="../../.prettierignore"
 
 # The regex for the exact files to be handled by prettifier.
 # ** = all (sub-)directories in the current working directory.
 # *.stories.tsx = all files which are appended with ".stories.tsx" in the specified directory.
 STORIES_DIR_REGEX="./**/*.stories.tsx"
 
-yarn prettier --config $CONFIG_FILE --ignore-path $IGNORE_FILE all --write "$STORIES_DIR_REGEX"
+prettier --config $CONFIG_FILE --ignore-path $IGNORE_FILE all --write "$STORIES_DIR_REGEX"
 
 # After this command is run, ensure to open all the stories at least once for the changes to actually be applied and registered by the VC diff manager.
