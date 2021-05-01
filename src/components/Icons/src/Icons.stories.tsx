db97b477ec6c299f17bd96145b528200fbc1b1f5
Maarten de Klerk <maarten@deklerk.dev>
2021-05-01 20:46:17 +0200

    display all icons in storybook

--- src/components/Icons/src/Icons.stories.tsx
+++ src/components/Icons/src/Icons.stories.tsx
@@ -3,7 +3,11 @@ import { Meta, Story } from "@storybook/react";
 import SvgIcon, { SvgIconProps } from "./SvgIcon";
 import pkg from "../package.json";
 
-import RightArrowIcon from "./svg/arrows/arrow-right.svg";
+import Grid from "../../Grid";
+
+import * as Icons from ".";
+
+console.log();
 
 export default {
   title: "Components/Data Display/Icons",
@@ -18,9 +22,31 @@ export default {
   component: SvgIcon,
 } as Meta;
 
-const Template: Story<SvgIconProps> = (props: SvgIconProps) => (
-  <SvgIcon component={RightArrowIcon} {...props}></SvgIcon>
-);
+const Template: Story<SvgIconProps> = (props: SvgIconProps) => {
+  const children = Object.keys(Icons)
+    .filter((iconName) => (iconName === "default" ? false : true))
+    .map<React.ReactElement>((iconName) => {
+      const IconElement = Icons[iconName];
+      return (
+        <Grid item xs={2} key={iconName}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <IconElement {...props} />
+          </div>
+          <p style={{ textAlign: "center", color: "#4B4B4B", fontFamily: "inherit" }}>{iconName}</p>
+        </Grid>
+      );
+    });
+
+  return (
+    <Grid container spacing={3}>
+      {children}
+    </Grid>
+  );
+};
+// (
+//   // <SvgIcon component={RightArrowIcon} {...props}></SvgIcon>
+//   <Icons.CalendarIcon></Icons.CalendarIcon>
+// );
 
 const defaultCode = `
 import RightArrowIcon from "./svg/arrow-right.svg";
