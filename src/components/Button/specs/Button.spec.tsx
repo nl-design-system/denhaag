af555e3eb5e8e17f2a450e1dfe8ec35a1eb2146f
DanielvanVliet <danielvvliet@hotmail.com>
2021-05-05 15:42:42 +0200

    added automated a11y test

--- src/components/Button/specs/Button.spec.tsx
+++ src/components/Button/specs/Button.spec.tsx
@@ -2,18 +2,27 @@ import * as React from "react";
 import { mount } from "@cypress/react";
 import Button, { ButtonProps } from "../src/index";
 
-it("Button", async () => {
-  const props: ButtonProps = {
-    onClick: () => {
-      console.log("Clicked");
-    },
-  };
+describe("Button tests", () => {
+  it("can be clicked", () => {
+    const props: ButtonProps = {
+      onClick: () => {
+        console.log("Clicked");
+      },
+    };
 
-  const spy = cy.spy(props, "onClick").as("Button click");
+    const spy = cy.spy(props, "onClick").as("Button click");
 
-  mount(<Button onClick={props.onClick}>Test button</Button>);
-  cy.get("button")
-    .contains("Test button")
-    .click()
-    .then(() => expect(spy).to.have.been.calledOnce);
+    mount(<Button onClick={props.onClick}>Test button</Button>);
+
+    cy.get("button")
+      .contains("Test button")
+      .click()
+      .then(() => expect(spy).to.have.been.calledOnce);
+  });
+
+  it("does not violate any accessibility rules", () => {
+    mount(<Button>Test button</Button>);
+    cy.injectAxe()
+    cy.checkA11y('button')
+  });
 });
