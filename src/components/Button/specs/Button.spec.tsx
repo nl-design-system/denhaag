83e5f9b01b63c6e3a98a95e12509615fa2e7fd81
DanielvanVliet <danielvvliet@hotmail.com>
2021-05-05 14:40:25 +0200

    added button click test

--- src/components/Button/specs/Button.spec.tsx
+++ src/components/Button/specs/Button.spec.tsx
@@ -1,8 +1,19 @@
-import * as React from 'react'
-import { mount } from '@cypress/react'
-import Button from '../src/index'
+import * as React from "react";
+import { mount } from "@cypress/react";
+import Button, { ButtonProps } from "../src/index";
 
-it('Button', () => {
-  mount(<Button>Test button</Button>)
-  cy.get('button').contains('Test button').click()
-})
+it("Button", async () => {
+  const props: ButtonProps = {
+    onClick: () => {
+      console.log("Clicked");
+    },
+  };
+
+  const spy = cy.spy(props, "onClick").as("Button click");
+
+  mount(<Button onClick={props.onClick}>Test button</Button>);
+  cy.get("button")
+    .contains("Test button")
+    .click()
+    .then(() => expect(spy).to.have.been.calledOnce);
+});
