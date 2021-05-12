0a3ef966bbf6e401195a3a777e04dce4dccc5100
Maarten de Klerk <maarten@deklerk.dev>
2021-05-12 11:16:15 +0200

    move stories

--- src/components/Stepper/src/stories/Stepper.stories.tsx
+++ src/components/Stepper/src/stories/Stepper.stories.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Button } from '@gemeente-denhaag/button';
-import Stepper, { Step, StepperProps } from '..';
+import Stepper, { Step, StepLabel, StepContent, StepperProps } from '..';
 import pkg from '../../package.json';
 
 export default {
@@ -10,74 +9,67 @@ export default {
     componentSubtitle: `${pkg.name} - ${pkg.version}`,
     docs: {
       source: {
-        type: 'code',
+        type: 'dynamic',
       },
     },
   },
   component: Stepper,
 } as Meta;
 
-const Template: Story<StepperProps> = () => {
-  const [activeStep, setActiveStep] = useState(0);
-  const steps = [
-    {
-      label: 'First Step in a Stepper!',
-      description: 'This is the first Step component of many in a Stepper component.',
-    },
-    {
-      label: 'Second Step in a Stepper!',
-    },
-    {
-      label: 'Third Step in a Stepper!',
-      description: 'This is the third Step component of many in a Stepper component.',
-    },
-  ];
-
-  const prev = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const next = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  };
-
-  return (
-    <React.Fragment>
-      <Stepper activeStep={activeStep}>
-        {steps.map(({ label, description }) => (
-          <Step key={label} label={label} description={description} />
-        ))}
-      </Stepper>
-      <Button onClick={prev} disabled={activeStep === 0}>
-        Prev
-      </Button>
-      <Button onClick={next} disabled={activeStep === steps.length} color="primary">
-        Next
-      </Button>
-    </React.Fragment>
-  );
-};
+const Template: Story<StepperProps> = (args: StepperProps) => (
+  <div>
+    <Stepper {...args}>
+      <Step>
+        <StepLabel>
+          <p>First Step in a Stepper!</p>
+        </StepLabel>
+        <StepContent>
+          <p>This is the first Step component of many in a Stepper component.</p>
+        </StepContent>
+      </Step>
+      <Step>
+        <StepLabel>
+          <p>Second Step in a Stepper!</p>
+        </StepLabel>
+        <StepContent>
+          <p>This is the second Step component of many in a Stepper component.</p>
+        </StepContent>
+      </Step>
+      <Step>
+        <StepLabel>
+          <p>Third Step in a Stepper!</p>
+        </StepLabel>
+        <StepContent>
+          <p>This is the third Step component of many in a Stepper component.</p>
+        </StepContent>
+      </Step>
+    </Stepper>
+  </div>
+);
 
 /**
  * Implementation of Stepper
  * "activeStep" property set to enforce step visibility.
  */
 export const Default = Template.bind({});
+Default.args = {
+  activeStep: 0,
+};
 
-// /**
-//  * Implementation of Stepper with a vertical orientation.
-//  */
-// export const VerticalOrientation = Template.bind({});
-// VerticalOrientation.args = {
-//   activeStep: 0,
-//   orientation: 'vertical',
-// };
+/**
+ * Implementation of Stepper with a vertical orientation.
+ */
+export const VerticalOrientation = Template.bind({});
+VerticalOrientation.args = {
+  activeStep: 0,
+  orientation: 'vertical',
+};
 
-// /**
-//  * Alternatively labeled Stepper.
-//  */
-// export const AlternativeLabel = Template.bind({});
-// AlternativeLabel.args = {
-//   activeStep: 0,
-//   alternativeLabel: true,
-// };
+/**
+ * Alternatively labeled Stepper.
+ */
+export const AlternativeLabel = Template.bind({});
+AlternativeLabel.args = {
+  activeStep: 0,
+  alternativeLabel: true,
+};
