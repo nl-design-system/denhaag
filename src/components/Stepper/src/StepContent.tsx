a3fbf3d69d8453442bb48aa85f4b841627162e3f
Maarten de Klerk <maarten@deklerk.dev>
2021-05-12 16:30:43 +0200

    add styling template

--- src/components/Stepper/src/StepContent.tsx
+++ src/components/Stepper/src/StepContent.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import MaterialStepContent from '@material-ui/core/StepContent';
+import { StylesProvider } from '@material-ui/core';
 import BaseProps from '@gemeente-denhaag/baseprops';
+
+// styles
+import { contentClasses } from './styles/bem-mapping';
+import '@gemeente-denhaag/denhaagbrand';
+import '@gemeente-denhaag/basestyles';
+import './styles/mui-override.module.css';
+import './styles/stepper.module.css';
 
 export interface StepContentProps extends BaseProps {
   /**
@@ -34,7 +42,13 @@ export interface StepContentProps extends BaseProps {
  * @constructor Constructs an instance of StepContent.
  */
 export const StepContent: React.FC<StepContentProps> = (props: StepContentProps) => {
-  return <MaterialStepContent {...props}>{props.children}</MaterialStepContent>;
+  return (
+    <StylesProvider injectFirst>
+      <MaterialStepContent classes={contentClasses} {...props}>
+        {props.children}
+      </MaterialStepContent>
+    </StylesProvider>
+  );
 };
 
 export default StepContent;
