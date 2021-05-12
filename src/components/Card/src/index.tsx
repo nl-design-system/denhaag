891519ad1b7d16da3e5c1643d2207ec975aed125
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-05-12 12:26:00 +0200

    Removed unused raised property from card

--- src/components/Card/src/index.tsx
+++ src/components/Card/src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MaterialCard from "@material-ui/core/Card";
 import { StylesProvider } from "@material-ui/core/styles";
-import ArrowForward from "@gemeente-denhaag/icons";
+import { ArrowRightIcon } from "@gemeente-denhaag/icons";
 import BaseProps from "@gemeente-denhaag/baseprops";
 import "./mui-override.module.css";
 import  "./card.module.css";
@@ -17,11 +17,6 @@ import { CardContent,
 import { Icon } from "@material-ui/core";
 
 export interface CardProps extends BaseProps {
-  /**
-   * If `true`, the card will use raised styling.
-   */
-  raised?: boolean;
-
   /**
    * Determines the variant of the card
    * Variant can be either `basic` or `case`
@@ -49,7 +44,6 @@ export interface CardProps extends BaseProps {
  * Primary UI component for user interaction
  */
 export const Card: React.FC<CardProps> = (props: CardProps) => {
-  const muiVariant = props.raised;
   let classes;
   const arrowClasses = cardArrowClasses;
   const titleClasses = cardTitleClasses;
@@ -66,27 +60,27 @@ export const Card: React.FC<CardProps> = (props: CardProps) => {
 
   return (
     <StylesProvider injectFirst>
-      <MaterialCard classes={classes} {...muiVariant} tabIndex={0}>
+      <MaterialCard classes={classes} tabIndex={0}>
         <div className="denhaag-card__wrapper">
           <div className="denhaag-card__background"></div>
-              <CardContent disableSpacing={true}>
-            <div className="denhaag-card--case__text__wrapper">
-                <Typography classes={titleClasses} component="p">
-                  {props.title}
+          <CardContent disableSpacing={true}>
+          <div className="denhaag-card--case__text__wrapper">
+              <Typography classes={titleClasses} component="p">
+                {props.title}
+              </Typography>
+              <Typography classes={subtitleClasses} component="p">
+                {props.subTitle}
+              </Typography>
+          </div>
+              <CardActions disableSpacing={true}>
+                <Typography component="div" classes={subtitleClasses}>
+                  {props.date.toLocaleDateString()}
                 </Typography>
-                <Typography classes={subtitleClasses} component="p">
-                  {props.subTitle}
-                </Typography>
-            </div>
-                <CardActions disableSpacing={true}>
-                  <Typography component="div" classes={subtitleClasses}>
-                    {props.date.toLocaleDateString()}
-                  </Typography>
-                  <Icon classes={arrowClasses} aria-label="ArrowRight">
-                    <ArrowForward />
-                  </Icon>
-                </CardActions>
-              </CardContent>
+                <Icon classes={arrowClasses} aria-label="ArrowRightIcon">
+                  <ArrowRightIcon />
+                </Icon>
+              </CardActions>
+            </CardContent>
         </div>
       </MaterialCard>
     </StylesProvider>
