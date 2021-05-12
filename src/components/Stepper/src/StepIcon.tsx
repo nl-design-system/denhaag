3c6eca20f090096d1151cc655db0e8e9b69bd5b8
Maarten de Klerk <maarten@deklerk.dev>
2021-05-12 15:34:45 +0200

    add custom icons to StepIcon

--- src/components/Stepper/src/StepIcon.tsx
+++ src/components/Stepper/src/StepIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import MaterialStepIcon from '@material-ui/core/StepIcon';
 import { BaseClassesProps } from '@gemeente-denhaag/baseprops';
+import SvgIcon, { CheckCircleIcon } from '@gemeente-denhaag/icons';
 
 export interface StepIconProps extends BaseClassesProps {
   /**
@@ -21,7 +21,7 @@ export interface StepIconProps extends BaseClassesProps {
   /**
    * The label displayed in the step icon.
    */
-  icon: React.ReactNode;
+  icon: boolean;
 }
 
 /**
@@ -29,8 +29,20 @@ export interface StepIconProps extends BaseClassesProps {
  * @param props The properties of a StepIcon component.
  * @constructor Constructs an instance of StepIcon.
  */
-export const StepIcon: React.FC<StepIconProps> = (props: StepIconProps) => {
-  return <MaterialStepIcon {...props} />;
+export const StepIcon: React.FC<StepIconProps> = ({ completed, icon }: StepIconProps) => {
+  if (completed) {
+    return <CheckCircleIcon />;
+  }
+
+  // Exported from https://www.figma.com/file/JpoY3waVoQGlLQzQXTL9nn/Design-System-Gemeente-Den-Haag?node-id=1908%3A9407
+  return (
+    <SvgIcon viewBox="0 0 20 20">
+      <circle cx="10" cy="10" r="10" />
+      <text x="10" y="14" textAnchor="middle">
+        {icon}
+      </text>
+    </SvgIcon>
+  );
 };
 
 export default StepIcon;
