ad80747600e3840a7aa9c082c02c98116c8d7a89
DanielvanVliet <danielvvliet@hotmail.com>
2021-05-12 15:11:01 +0200

    added margin for listsubheader

--- src/components/List/src/ListSubheader.tsx
+++ src/components/List/src/ListSubheader.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Typography } from '@gemeente-denhaag/typography';
 
+import { listsubheader_classes as classes } from './bem-mapping';
+import './mui-override.module.css';
+import { StylesProvider } from '@material-ui/core';
+
 export interface ListSubheaderProps {
   /**
    * The text shown in the List subheader
@@ -12,7 +16,11 @@ export interface ListSubheaderProps {
  * Primary UI component for user interaction
  */
 export const ListSubheader: React.FC<ListSubheaderProps> = (props: ListSubheaderProps) => {
-  return <Typography color="inherit">{props.children}</Typography>;
+  return (
+    <StylesProvider injectFirst>
+      <Typography classes={classes}>{props.children}</Typography>
+    </StylesProvider>
+  );
 };
 
 export default ListSubheader;
--- src/components/List/src/bem-mapping.ts
+++ src/components/List/src/bem-mapping.ts
@@ -1,3 +1,7 @@
 export const listitemicon_classes = {
     root: 'denhaag-list__itemicon'
 }
+
+export const listsubheader_classes = {
+    root: 'denhaag-list__subheader'
+}
