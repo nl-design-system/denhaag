51447f0ffff5877c8fa36d4ea9b7a546f262117e
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-05-12 12:23:57 +0200

    Fixed story book args to map to interface

--- src/components/Card/src/Card.stories.tsx
+++ src/components/Card/src/Card.stories.tsx
@@ -3,7 +3,6 @@ import { Meta, Story } from "@storybook/react";
 import Card, { CardProps } from ".";
 import pkg from "../package.json";
 
-const date = new Date("2020-01-21");
 
 export default {
   title: "Components/Cards/Card",
@@ -14,21 +13,27 @@ export default {
 } as Meta;
 
 
-const Template: Story<CardProps> = (args: unknown) => (
-  <Card {...args} title="Shrimp and Chorizo Paella" 
-    subTitle="This impressive paella is a perfect party dish and a fun meal to cook."
-    date={date}/>
+const Template: Story<CardProps> = (args: CardProps) => (
+  <Card {...args}/>
 );
 
 /**
  * Implementation of Card
  */
-export const Default = Template.bind({});
+export const Default: Story<CardProps> = Template.bind({});
+  Default.args = {
+    title: "Shrimp and Chorizo Paella",
+    subTitle: "This impressive paella is a perfect party dish and a fun meal to cook.",
+    date: new Date("2020-01-21")
+  };
 
 /**
  * Case variant of Card component
  */
-export const Case = Template.bind({});
+export const Case: Story<CardProps> = Template.bind({});
 Case.args = {
+  title: Default.args.title,
+  subTitle: Default.args.subTitle,
+  date: Default.args.date,
   variant: "case"
 };
