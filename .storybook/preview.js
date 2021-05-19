19ed7b820751e520556933edb4b9f3698c967978
Bryan de Jong <bryandejong99@gmail.com>
2021-05-19 15:59:55 +0200

    Add storybook design token plugin

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,16 @@ import '@gemeente-denhaag/design-tokens-proprietary';
 import '@gemeente-denhaag/design-tokens-components';
 import '@gemeente-denhaag/design-tokens-common';
 
+const tokenContext = require.context('!!raw-loader!../src', true, /.\.(css|less|scss|sass|svg)$/);
+
+const tokenFiles = tokenContext.keys().map((filename) => {
+  return { filename: filename, content: tokenContext(filename).default };
+});
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  designToken: {
+    files: tokenFiles
+  },
   format: false,
 };
