a0ac6925ab02c951113bf821491f787e3cd439ff
Maarten de Klerk <maarten@deklerk.dev>
2021-05-22 13:04:44 +0200

    update stepper icons

--- src/components/Stepper/src/StepIcon.tsx
+++ src/components/Stepper/src/StepIcon.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { StylesProvider } from '@material-ui/core';
 import { BaseClassesProps } from '@gemeente-denhaag/baseprops';
-import SvgIcon, { CheckCircleIcon } from '@gemeente-denhaag/icons';
+import SvgIcon, { SvgIconProps, CheckCircleIcon } from '@gemeente-denhaag/icons';
+// import clsx from 'clsx';
 
 export interface StepIconProps extends BaseClassesProps {
   /**
    * Whether this step is active.
    */
   active?: boolean;
+
+  inactive?: boolean;
 
   /**
    * Mark the step as completed. Is passed to child components.
@@ -29,19 +33,52 @@ export interface StepIconProps extends BaseClassesProps {
  * @param props The properties of a StepIcon component.
  * @constructor Constructs an instance of StepIcon.
  */
-export const StepIcon: React.FC<StepIconProps> = ({ completed, icon }: StepIconProps) => {
+export const StepIcon: React.FC<StepIconProps> = ({
+  active = false,
+  inactive = true,
+  completed = false,
+  icon,
+}: StepIconProps) => {
+  let color: SvgIconProps['color'];
+  if (active) {
+    color = 'primary';
+  } else if (completed) {
+    color = 'secondary';
+  } else if (inactive) {
+    color = 'disabled';
+  }
+
+  // const SvgIconclasses = clsx('denhaag-stepper__step-icon', {
+  //   'denhaag-stepper__step-icon--active': active,
+  //   'denhaag-stepper__step-icon--inactive': inactive,
+  //   'denhaag-stepper__step-icon--completed': completed,
+  // });
+
+  // const textClasses = clsx('denhaag-stepper__step-icon__text', {
+  //   'denhaag-stepper__step-icon__text--active': active,
+  //   'denhaag-stepper__step-icon__text--inactive': inactive,
+  //   'denhaag-stepper__step-icon__text--completed': completed,
+  // });
+
   if (completed) {
-    return <CheckCircleIcon />;
+    return (
+      <StylesProvider injectFirst>
+        <CheckCircleIcon classes={stepIconClasses} color={color} />
+      </StylesProvider>
+    );
   }
 
   // Exported from https://www.figma.com/file/JpoY3waVoQGlLQzQXTL9nn/Design-System-Gemeente-Den-Haag?node-id=1908%3A9407
   return (
-    <SvgIcon viewBox="0 0 20 20">
-      <circle cx="10" cy="10" r="10" />
-      <text x="10" y="14" textAnchor="middle">
-        {icon}
-      </text>
-    </SvgIcon>
+    // <span className={SvgIconclasses}>{icon}</span>
+    <StylesProvider injectFirst>
+      <SvgIcon viewBox="0 0 20 20" classes={stepIconClasses} color={color}>
+        <circle cx="10" cy="10" r="10" />
+        <text x="10" y="14" textAnchor="middle">
+          {icon}
+        </text>
+      </SvgIcon>
+    </StylesProvider>
   );
 };
 
--- src/components/Stepper/src/styles/bem-mapping.ts
+++ src/components/Stepper/src/styles/bem-mapping.ts
@@ -4,3 +4,10 @@ export const stepClasses = {
 export const contentClasses = {
   root: 'denhaag-stepper__step-content',
 };
+
+export const stepIconClasses = {
+  root: 'denhaag-stepper__step-icon',
+  colorPrimary: 'denhaag-stepper__step-icon--active',
+  colorSecondary: 'denhaag-stepper__step-icon--completed',
+  colorDisabled: 'denhaag-stepper__step-icon--inactive',
+};
