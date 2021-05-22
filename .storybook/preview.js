2b6f22cdf7a86c4425ab45ed18f278ce8fc86d6f
Bryan de Jong <bryandejong99@gmail.com>
2021-05-22 09:38:09 +0200

    Fix storybook-design-token config and presenters
    - Changed the regex filter in .storybook/preview.js to exclude css
    variables in dist and node_modules (the plugin was showing duplicate
    tokens)
    - Changed --denhaag-border token presenters

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,7 +2,7 @@ import '@gemeente-denhaag/design-tokens-proprietary';
 import '@gemeente-denhaag/design-tokens-components';
 import '@gemeente-denhaag/design-tokens-common';
 
-const tokenContext = require.context('!!raw-loader!../src', true, /.\.(css|less|scss|sass|svg)$/);
+const tokenContext = require.context('!!raw-loader!../src', true, /^((?![\\/]node_modules|dist[\\/]).)*\.(css|less|scss|sass|svg)$/);
 
 const tokenFiles = tokenContext.keys().map((filename) => {
   return { filename: filename, content: tokenContext(filename).default };
