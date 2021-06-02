022f212957b5acddbb5c1ffb78a6f8fd68769ff8
DanielvanVliet <danielvvliet@hotmail.com>
2021-06-02 11:15:37 +0200

    reworked type of subheader

--- src/components/List/src/List.tsx
+++ src/components/List/src/List.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MaterialList from '@material-ui/core/List';
 import BaseProps from '@gemeente-denhaag/baseprops';
-import ListSubheader from '@gemeente-denhaag/listsubheader';
+import { ListSubheaderProps } from '@gemeente-denhaag/listsubheader';
 
 import { StylesProvider } from '@material-ui/core';
 import { list_classes as classes } from './bem-mapping';
@@ -13,15 +13,14 @@ export interface ListProps extends BaseProps {
   /**
    * The content of the subheader, normally `ListSubheader`.
    */
-  subheader: React.ElementType<typeof ListSubheader>;
+  subheader: React.ReactElement<ListSubheaderProps>;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const List: React.FC<ListProps> = (props: ListProps) => {
-  const subheader = (props.subheader as unknown) as React.ReactElement;
-  const materialProps = { ...props, subheader };
+  const materialProps = { ...props, subheader: props.subheader };
 
   return (
     <StylesProvider injectFirst>
