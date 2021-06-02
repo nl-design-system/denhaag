accaf1d6b7f05acec369dcd4f15ff581244660d3
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-06-02 15:24:42 +0200

    Renamed CardActions file and reduced exports of Card

--- src/components/Card/src/index.ts
+++ src/components/Card/src/index.ts
@@ -2,7 +2,5 @@ import Card from './Card/Card';
 
 export default Card;
 export * from './Card/Card';
-export * from './CardHeader/CardHeader';
-export * from './CardActions/CardAction';
-export * from './CardContent/CardContent'
+
 export * from './Paper/Paper';
