a95976774a7b960ae20f152980cd6b5e03698329
Maarten de Klerk <maarten@deklerk.dev>
2021-06-05 12:01:28 +0200

    split css file into multiple modules

--- src/components/Timeline/src/index.ts
+++ src/components/Timeline/src/index.ts
@@ -3,8 +3,13 @@ import Timeline from './Timeline';
 // styles
 import '@gemeente-denhaag/design-tokens-common';
 import '@gemeente-denhaag/design-tokens-proprietary';
+
 import './styles/mui-override.module.css';
-import './styles/stepper.module.css';
+import './styles/step-content.module.css';
+import './styles/step-icon.module.css';
+import './styles/step-label.module.css';
+import './styles/step.module.css';
+import './styles/timeline.module.css';
 
 export default Timeline;
 export * from './Step';
