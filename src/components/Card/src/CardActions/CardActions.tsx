3915cf5338227791291407e7f2e70c54df2dc9db
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-06-16 16:08:53 +0200

    replaced padding inline and block properties by their start and end counterparts
    Co-authored-by: Daniel van Vliet <Danielvvliet@hotmail.com>

--- src/components/Card/src/CardActions/CardActions.tsx
+++ src/components/Card/src/CardActions/CardActions.tsx
@@ -3,7 +3,6 @@ import MaterialCardActions from "@material-ui/core/CardActions";
 import BaseProps from "@gemeente-denhaag/baseprops";
 import "./card-actions.css";
 import "./mui-override.css";
-import { StylesProvider } from "@material-ui/core/styles";
 import { cardActionClasses } from "./bem-mapping";
 
 export interface CardActionProps extends BaseProps {
@@ -18,9 +17,7 @@ export interface CardActionProps extends BaseProps {
  */
 export const CardActions: React.FC<CardActionProps> = (props: CardActionProps) => {
   return (
-    <StylesProvider injectFirst>
       <MaterialCardActions classes={cardActionClasses} disableSpacing={props.disableSpacing}>{props.children}</MaterialCardActions>
-    </StylesProvider>
   );
 }
 
--- src/components/Card/src/CardContent/CardContent.tsx
+++ src/components/Card/src/CardContent/CardContent.tsx
@@ -3,7 +3,6 @@ import MaterialCardContent from '@material-ui/core/CardContent';
 import BaseProps from '@gemeente-denhaag/baseprops';
 import './mui-override.css';
 import { CardContentClasses } from './bem-mapping';
-import { StylesProvider } from '@material-ui/core/styles';
 
 export type CardContentProps = BaseProps
 
@@ -12,9 +11,7 @@ export type CardContentProps = BaseProps
  */
 export const CardContent: React.FC<CardContentProps> = (props: CardContentProps) => {
   return (
-      <StylesProvider injectFirst>
-        <MaterialCardContent classes={CardContentClasses}>{props.children}</MaterialCardContent>
-      </StylesProvider>
+      <MaterialCardContent classes={CardContentClasses}>{props.children}</MaterialCardContent>
   )
 };
 
