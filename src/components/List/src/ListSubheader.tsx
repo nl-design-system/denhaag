78063aedca3819662947befaf4eadbca20529eb4
DanielvanVliet <danielvvliet@hotmail.com>
2021-06-19 14:26:01 +0200

    replaced typography dependency in List with MUIs

--- src/components/List/src/ListSubheader.tsx
+++ src/components/List/src/ListSubheader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Typography } from '@gemeente-denhaag/typography';
+import { Typography } from '@material-ui/core';
 
 import { listsubheader_classes as classes } from './bem-mapping';
 
