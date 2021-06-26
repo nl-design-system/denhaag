9af5981a2d7fcae43858e4a7a014ea6add4c27de
DanielvanVliet <danielvvliet@hotmail.com>
2021-06-26 16:07:36 +0200

    Alert styling for icon, buttons and typographies

--- src/components/Alerts/src/index.tsx
+++ src/components/Alerts/src/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+import './alert.css';
+import { Heading4, Paragraph } from '@gemeente-denhaag/typography';
+import { AlertTriangleIcon, CheckCircleIcon, CircleInformationIcon, CloseIcon } from '@gemeente-denhaag/Icons';
+import Button from '@gemeente-denhaag/button';
+import IconButton from '@gemeente-denhaag/iconbutton';
+
 export interface AlertsProps {
   /**
    * The title of the alert.
@@ -15,12 +21,12 @@ export interface AlertsProps {
    * The variant of the alert, determines which color and icon are displayed.
    * Default is 'base'.
    */
-  variant?: 'base' | 'alert' | 'info' | 'success' | 'warning';
+  variant: 'error' | 'info' | 'success' | 'warning';
 
   /**
    * The action of the alert, displays a clickable button that will fire this event.
    */
-  action?: () => void;
+  action?: { buttonText: string; onClick: () => void };
 
   /**
    * The action of the alert, displays a clickable CloseIcon Button that will fire this event.
@@ -31,8 +37,56 @@ export interface AlertsProps {
 /**
  * Primary UI component for user interaction
  */
-export const Alerts: React.FC<AlertsProps> = () => {
-  return <p>Alert</p>;
+export const Alerts: React.FC<AlertsProps> = ({ title, text, variant, action, close }: AlertsProps) => {
+  const customAttributes = { variant };
+  let icon;
+
+  switch (variant) {
+    case 'error':
+      icon = <AlertTriangleIcon />;
+      break;
+    case 'info':
+      icon = <CircleInformationIcon />;
+      break;
+    case 'success':
+      icon = <CheckCircleIcon />;
+      break;
+    case 'warning':
+      icon = <AlertTriangleIcon />;
+      break;
+  }
+
+  // actionButton is optional
+  let actionButton = undefined;
+  if (action !== undefined) {
+    actionButton = (
+      <Button size="medium" variant="primary-action" type="button" onClick={() => action.onClick()}>
+        {action.buttonText}
+      </Button>
+    );
+  }
+
+  // closeButton is optional
+  let closeButton = undefined;
+  if (close !== undefined) {
+    closeButton = (
+      <IconButton aria-label="Close" color="inherit" onClick={close} disableRipple disableFocusRipple>
+        <CloseIcon />
+      </IconButton>
+    );
+  }
+
+  return (
+    <div className="denhaag-alert" {...customAttributes}>
+      <div className="denhaag-alert__icon">{icon}</div>
+      <div className="denhaag-alert__content">
+        <Heading4>{title}</Heading4>
+        <Paragraph>{text}</Paragraph>
+        {actionButton}
+      </div>
+      <div>{closeButton}</div>
+    </div>
+  );
 };
 
 export default Alerts;
