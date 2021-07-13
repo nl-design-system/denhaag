f5d3006fb0e6c0e19995eecf8f185864c5a18837
Joost van Viegen <joost.vanviegen@denhaag.nl>
2021-07-13 16:44:27 +0200

    Split the menubutton component in MenuButton and MenuButtonExpandable

--- src/components/Menu/src/MenuButton.tsx
+++ src/components/Menu/src/MenuButton.tsx
@@ -3,45 +3,45 @@ import BaseProps from '@gemeente-denhaag/baseprops';
 import './menu-button.css';
 import { ChevronDownIcon } from '@gemeente-denhaag/icons';
 
+/**
+ * The properties of the normal variant of the Menu Button
+ */
 export interface MenuButtonProps extends Omit<BaseProps, 'classes' | 'tabIndex'> {
   /**
    * Determines the url the menu button refers to
    * Only applicable to the normal variant
    */
   href?: string;
+}
 
+/**
+ * The properties of the expandable variant of the Menu Button
+ */
+export interface MenuButtonExpandableProps extends Omit<BaseProps, 'classes' | 'tabIndex'> {
   /**
    * Determines the Onclick function the menu button referst to
    * Only applicable to the Expandable variant
    */
   onclick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
-
-  /**
-   * Determines if the Menu Button is expandable
-   */
-  variant?: 'normal' | 'expandable';
 }
 
 export const MenuButton: React.FC<MenuButtonProps> = (props: MenuButtonProps) => {
-  let component = null;
-  if (props.variant === 'expandable') {
-    component = (
-      <button id={props.id} onClick={props.onclick} className="denhaag-menu-button">
-        {props.children}
-        <span className="denhaag-menu-button__chevron">
-          <ChevronDownIcon aria-label="ChevronDownIcon" />
-        </span>
-      </button>
-    );
-  } else {
-    component = (
-      <a id={props.id} href={props.href} className="denhaag-menu-button">
-        {props.children}
-      </a>
-    );
-  }
+  return (
+    <a id={props.id} href={props.href} className="denhaag-menu-button">
+      {props.children}
+    </a>
+  );
+};
 
-  return component;
+export const MenuButtonExpandable: React.FC<MenuButtonProps> = (props: MenuButtonExpandableProps) => {
+  return (
+    <button id={props.id} onClick={props.onclick} className="denhaag-menu-button">
+      {props.children}
+      <span className="denhaag-menu-button__chevron">
+        <ChevronDownIcon aria-label="ChevronDownIcon" />
+      </span>
+    </button>
+  );
 };
 
 export default MenuButton;
