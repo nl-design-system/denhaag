d35036711ccbf72cf45f5b455919550aedb07b55
Bryan de Jong <bryandejong99@gmail.com>
2021-08-04 19:44:06 +0200

    fix: radio button without material ui

--- src/components/Radio/src/Radio.tsx
+++ src/components/Radio/src/Radio.tsx
@@ -1,86 +1,18 @@
 import React from 'react';
-import { Radio as MaterialRadio } from '@material-ui/core';
-import { ActiveRadioIcon, InactiveRadioIcon } from '@gemeente-denhaag/icons';
-import { StylesProvider } from '@material-ui/core';
 
-import clsx from 'clsx';
-import { classes } from './bem-mapping';
-import './mui-override.css';
 import './radio.css';
+import BaseProps from '@gemeente-denhaag/baseprops';
 
-export interface RadioProps {
-  /**
-   * Id of the input element.
-   */
-  id?: string;
-
-  /**
-   * **Signature**: `function(event: React.ChangeEvent<unknown>) => void`
-   * The event source of the callback. You can pull out the new
-   * value by accessing `event.target.value` (string). You can
-   * pull out the new checked state by accessing
-   * `event.target.checked` (boolean).
-   */
-  onChange?: (event: React.ChangeEvent<unknown>) => void;
-
-  /**
-   * Name attribute of the radio button.
-   */
-  name?: string;
-
-  /**
-   * The value of the component. The DOM casts it to `string`.
-   */
-  value?: unknown;
-
-  /**
-   * If `true`, the component is checked.
-   */
-  checked?: boolean;
-
-  /**
-   * If `true`, the radio but will be disabled.
-   */
-  disabled?: boolean;
-
-  /**
-   * If `true`, the input will be required.
-   */
-  required?: boolean;
-
-  /**
-   * The icon to display when the component is checked.
-   */
-  checkedIcon?: React.ReactNode;
-
-  /**
-   * The icon to display when the component is unchecked.
-   */
-  icon?: React.ReactNode;
-
-  /**
-   * The icon is turned red to indicate an error.
-   */
-  error?: boolean;
+export interface RadioProps extends BaseProps {
+  label: string;
 }
 
-export const Radio: React.FC<RadioProps> = (props: RadioProps) => {
-  classes.root = clsx('denhaag-radio', {
-    'denhaag-radio--error': props.error,
-  });
-
+export const Radio: React.FC<RadioProps> = ({ id, label }: RadioProps) => {
   return (
-    <StylesProvider injectFirst>
-      <MaterialRadio
-        {...props}
-        color="default"
-        classes={classes}
-        disableRipple
-        disableFocusRipple
-        checkedIcon={props.checkedIcon ?? <ActiveRadioIcon />}
-        icon={props.icon ?? <InactiveRadioIcon />}
-      />
-    </StylesProvider>
+    <div>
+      <input id={id} type={'radio'} />
+      {label && <label htmlFor={id}>{label}</label>}
+    </div>
   );
 };
 
--- src/components/Radio/src/bem-mapping.ts
+++
@@ -1,5 +0,0 @@
-export const classes = {
-  root: 'denhaag-radio',
-  checked: 'denhaag-radio--checked',
-  disabled: 'denhaag-radio--disabled',
-};
