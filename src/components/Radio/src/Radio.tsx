884ddd9353fa4373d3364e2b5b95c381cffdd066
Bryan de Jong <bryandejong99@gmail.com>
2021-08-24 16:36:06 +0200

    feat(radio): pass input props to native input

--- src/components/Radio/src/Radio.tsx
+++ src/components/Radio/src/Radio.tsx
@@ -5,12 +5,17 @@ import BaseProps from '@gemeente-denhaag/baseprops';
 
 export interface RadioProps extends BaseProps {
   label: string;
+
+  /**
+   * Attributes applied to the `input` element.
+   */
+  inputProps?: Record<string, unknown>;
 }
 
-export const Radio: React.FC<RadioProps> = ({ id, label }: RadioProps) => {
+export const Radio: React.FC<RadioProps> = ({ id, label, inputProps }: RadioProps) => {
   return (
     <div>
-      <input id={id} type={'radio'} />
+      <input id={id} type={'radio'} {...inputProps} />
       {label && <label htmlFor={id}>{label}</label>}
     </div>
   );
