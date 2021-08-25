ce4d9c3befc3f0e7e1b8b21d4540159c4f71e177
Bryan de Jong <bryandejong99@gmail.com>
2021-08-25 09:25:53 +0200

    feat(radio): add onChange and render icon

--- src/components/Radio/src/Radio.tsx
+++ src/components/Radio/src/Radio.tsx
@@ -4,18 +4,62 @@ import './radio.css';
 import BaseProps from '@gemeente-denhaag/baseprops';
 
 export interface RadioProps extends BaseProps {
+  /**
+   * If `true` the Checkbox is checked.
+   * For an example on the needed state machine to use this property.
+   */
+  checked?: boolean;
+
+  /**
+   * The label displayed next to the input
+   */
   label: string;
 
   /**
    * Attributes applied to the `input` element.
    */
   inputProps?: Record<string, unknown>;
+
+  /**
+   * Overwrite the Icon shown when in the unchecked state.
+   */
+  checkedIcon?: React.ReactNode;
+
+  /**
+   * Overwrite the Icon shown when in the checked state.
+   */
+  uncheckedIcon?: React.ReactNode;
+
+  /**
+   * Callback fired when the state is changed.
+   */
+  onChange?: (event: React.ChangeEvent) => void;
 }
 
-export const Radio: React.FC<RadioProps> = ({ id, label, inputProps }: RadioProps) => {
+export const Radio: React.FC<RadioProps> = ({
+  id,
+  label,
+  inputProps,
+  checked,
+  onChange,
+  checkedIcon,
+  uncheckedIcon,
+}: RadioProps) => {
+  const [isChecked, setChecked] = React.useState(checked);
+  const icon = isChecked ? checkedIcon : uncheckedIcon;
+
   return (
     <div>
-      <input id={id} type={'radio'} {...inputProps} />
+      <input
+        id={id}
+        type={'radio'}
+        {...inputProps}
+        onChange={(event) => {
+          setChecked(event.target.checked);
+          if (onChange) onChange(event);
+        }}
+      />
+      {icon}
       {label && <label htmlFor={id}>{label}</label>}
     </div>
   );
