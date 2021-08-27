64d36fe572e674c416a893c3d5a1a1781a1549ab
Maarten Carsjens <maarten.carsjens@gxsoftware.com>
2021-08-27 11:24:48 +0200

    feat(testing): added Card tests (a11y contrast issue..)

---
+++ src/components/Card/specs/Card.spec.jsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import Card from '../src/index';
+
+const props = {
+  title: 'Shrimp and Chorizo Paella',
+  subTitle: 'This impressive paella is a perfect party dish and a fun meal to cook.',
+  date: new Date('2020-01-21'),
+  href: '#test',
+};
+
+describe(`Card tests`, () => {
+  it('can be clicked', () => {
+    cy.mount(<Card {...props} />);
+
+    cy.get('.denhaag-card').click();
+
+    cy.location('hash').should('eq', '#test');
+  });
+
+  it('a11y and snapshots', () => {
+    cy.snapshots(
+      <Card {...props} />,
+      { variant: ['case'] },
+      {
+        selector: '.denhaag-card',
+        states: {
+          hover: 'denhaag-card--hover',
+          focus: 'denhaag-card--focus',
+        },
+      },
+    );
+  });
+});
