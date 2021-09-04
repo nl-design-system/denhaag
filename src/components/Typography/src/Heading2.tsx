64a008cbc1ba2f92c4ad6772dda748828cf1d6ac
Bryan de Jong <bryandejong99@gmail.com>
2021-09-04 17:28:40 +0200

    feat(typography): add className prop to Typography components

--- src/components/Typography/src/Heading2.tsx
+++ src/components/Typography/src/Heading2.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BaseDataDisplayProps from '@gemeente-denhaag/basedatadisplayprops';
 import './heading.css';
+import clsx from 'clsx';
 
 export type Heading2Props = BaseDataDisplayProps;
 
 export const Heading2: React.FC<Heading2Props> = (props: Heading2Props) => {
-  return <h2 className="utrecht-heading-2 utrecht-heading-2--distanced">{props.children}</h2>;
+  const rootClassNames = clsx('utrecht-heading-2 utrecht-heading-2--distanced', props.className);
+  return <h2 className={rootClassNames}>{props.children}</h2>;
 };
--- src/components/Typography/src/Heading3.tsx
+++ src/components/Typography/src/Heading3.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BaseDataDisplayProps from '@gemeente-denhaag/basedatadisplayprops';
 import './heading.css';
+import clsx from 'clsx';
 
 export type Heading3Props = BaseDataDisplayProps;
 
 export const Heading3: React.FC<Heading3Props> = (props: Heading3Props) => {
-  return <h3 className="utrecht-heading-3 utrecht-heading-3--distanced">{props.children}</h3>;
+  const rootClassNames = clsx('utrecht-heading-3 utrecht-heading-3--distanced', props.className);
+  return <h3 className={rootClassNames}>{props.children}</h3>;
 };
