b1c296627c9cff5324251ee200677a99a9354891
Bryan de Jong <bryandejong99@gmail.com>
2021-09-04 17:28:40 +0200

    feat(typography): add className prop to Typography components

--- src/components/Typography/src/Heading1.tsx
+++ src/components/Typography/src/Heading1.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import BaseDataDisplayProps from '@gemeente-denhaag/basedatadisplayprops';
 import './heading.css';
+import clsx from 'clsx';
 
 export type Heading1Props = BaseDataDisplayProps;
 
 export const Heading1: React.FC<Heading1Props> = (props: Heading1Props) => {
-  return <h1 className="utrecht-heading-1 utrecht-heading-1--distanced">{props.children}</h1>;
+  const rootClassNames = clsx('utrecht-heading-1 utrecht-heading-1--distanced', props.className);
+  return <h1 className={rootClassNames}>{props.children}</h1>;
 };
