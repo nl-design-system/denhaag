019dd350319eb57de0621ade20cab34b1fa57a07
Bryan de Jong <bryandejong99@gmail.com>
2021-09-06 12:12:00 +0200

    fix: use one class per clsx rule

--- src/components/Typography/src/Heading3.tsx
+++ src/components/Typography/src/Heading3.tsx
@@ -6,6 +6,6 @@ import clsx from 'clsx';
 export type Heading3Props = Omit<BaseDataDisplayProps, 'classes'>;
 
 export const Heading3: React.FC<Heading3Props> = (props: Heading3Props) => {
-  const rootClassNames = clsx('utrecht-heading-3 utrecht-heading-3--distanced', props.className);
+  const rootClassNames = clsx('utrecht-heading-3', 'utrecht-heading-3--distanced', props.className);
   return <h3 className={rootClassNames}>{props.children}</h3>;
 };
