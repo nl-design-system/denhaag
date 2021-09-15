98ad8eb6b977eb86eb8c02e6fbf1782e6d519c4d
Bryan de Jong <bryandejong99@gmail.com>
2021-09-15 16:20:15 +0200

    fix(link): not exported from component library

--- src/meta-packages/navigation/src/index.tsx
+++ src/meta-packages/navigation/src/index.tsx
@@ -4,3 +4,4 @@ export * from '@gemeente-denhaag/menu';
 export * from '@gemeente-denhaag/timeline';
 export * from '@gemeente-denhaag/swipeabledrawer';
 export * from '@gemeente-denhaag/tab';
+export * from '@gemeente-denhaag/link';
