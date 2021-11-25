c9c28b931e17e79d611fe0fa38cd08238bd7cac0
Maarten Carsjens <mjcarsjens@gmail.com>
2021-11-25 16:12:34 +0100

    feat: deprecated drawer

--- packages/navigation/src/index.tsx
+++ packages/navigation/src/index.tsx
@@ -1,5 +1,4 @@
 export * from '@gemeente-denhaag/baseprops';
-export * from '@gemeente-denhaag/drawer';
 export * from '@gemeente-denhaag/menu';
 export * from '@gemeente-denhaag/timeline';
 export * from '@gemeente-denhaag/swipeabledrawer';
