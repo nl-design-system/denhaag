5dbdc967afdcabf234750a2291bb6e0b4e7c0eb3
Maarten Carsjens <mjcarsjens@gmail.com>
2021-11-25 16:35:26 +0100

    feat: deprecated GridList

--- components/GridList/src/index.tsx
+++
@@ -1,6 +0,0 @@
-import GridList from './GridList';
-
-export default GridList;
-export * from './GridList';
-export * from './GridListTile';
-export * from './GridListTileBar';
--- packages/layout/src/index.tsx
+++ packages/layout/src/index.tsx
@@ -1,3 +1,2 @@
 export * from '@gemeente-denhaag/baselayoutprops';
-export * from '@gemeente-denhaag/gridlist';
 export * from '@gemeente-denhaag/hidden';
