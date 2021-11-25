9c1c4317162e14f66e6a867ec925ad51a9f92593
Maarten Carsjens <mjcarsjens@gmail.com>
2021-11-25 16:31:54 +0100

    feat: deprecated Container

--- packages/layout/src/index.tsx
+++ packages/layout/src/index.tsx
@@ -1,5 +1,4 @@
 export * from '@gemeente-denhaag/baselayoutprops';
-export * from '@gemeente-denhaag/container';
 export * from '@gemeente-denhaag/grid';
 export * from '@gemeente-denhaag/gridlist';
 export * from '@gemeente-denhaag/hidden';
