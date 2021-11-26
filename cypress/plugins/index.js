a8569ce296832e59dbe49e472c1cbc05fa743af2
Maarten Carsjens <mjcarsjens@gmail.com>
2021-11-26 11:08:30 +0100

    feat: also lint files in other folders than components

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
