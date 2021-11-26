377bf1650de8e84ccb4cb939001d7409c348d0ff
Maarten Carsjens <mjcarsjens@gmail.com>
2021-11-26 13:35:56 +0100

    #514 deprecate unused meta packages (#523)
    * feat: deprecated Griid
    
    * feat: deprecated GridList
    
    * feat: deprecated datadisplay package
    
    * feat: deprecated input meta package
    
    * feat: deprecated layout meta-package
    
    * feat: deprecated navigation meta-package
    
    * feat: deprecated surfaces meta-package
    
    * feat: also lint files in other folders than components
    
    * fix: fix order component-library package json dependencies
    
    * fix: fixed order of exports in component-library package
    
    * feat: testing testing
    
    * feat: readded stylelint everywhere (still testing)
    
    * fix: add specific folders to stylelint commands

--- packages/datadisplay/src/index.tsx
+++
@@ -1,7 +0,0 @@
-export * from '@gemeente-denhaag/alert';
-export * from '@gemeente-denhaag/avatar';
-export * from '@gemeente-denhaag/badge-counter';
-export * from '@gemeente-denhaag/baseprops';
-export * from '@gemeente-denhaag/divider';
-export * from '@gemeente-denhaag/list';
-export * from '@gemeente-denhaag/typography';
