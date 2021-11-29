622b289d1f5c8b5890996e5bb959b9bb19eec23f
Robbert Broersma <git@robbertbroersma.nl>
2021-11-29 19:01:45 +0100

    chore: tweak eslint-disable comments

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 /// <reference types="cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
@@ -16,7 +15,6 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
