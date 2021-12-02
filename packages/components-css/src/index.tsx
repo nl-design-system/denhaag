a971f3e988efdb21a9874da7d55743a0566328a7
Bryan de Jong <bryandejong99@gmail.com>
2021-12-02 16:26:38 +0100

    refactor(button): rename button files

--- packages/components-css/src/index.tsx
+++ packages/components-css/src/index.tsx
@@ -1,6 +1,6 @@
 import '../../../components/Alert/src/alert.css';
 import '../../../components/BadgeCounter/src/badge-counter.css';
-import '../../../components/Button/src/button.css';
+import '../../../components/Button/src/index.css';
 import '../../../components/Card/src/Card/card.css';
 import '../../../components/Card/src/CardActions/card-actions.css';
 import '../../../components/Checkbox/src/checkbox.css';
