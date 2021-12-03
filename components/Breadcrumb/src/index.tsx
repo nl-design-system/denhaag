6fbad7b225a986a928ae08348e26d752dbfc5e08
Paul van Impelen <paul@acato.nl>
2021-12-03 13:07:56 +0100

    feat(breadcrums): wip, added styling

---
+++ components/Breadcrumb/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import BaseProps from '@gemeente-denhaag/baseprops';
+import './breadcrumb.css';
+
+export interface BreadcrumbProps extends BaseProps {}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Breadcrumb = () => {
+  return <></>;
+};
+
+/**
+ * Default export for Breadcrumb
+ */
+export default Breadcrumb;
