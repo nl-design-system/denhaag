c5c244a3132712a24004145f5a56b5d5a3a11d16
Bryan de Jong <bryandejong99@gmail.com>
2021-12-10 14:28:14 +0100

    fixup! feat(table): add react components for all table elements

--- components/Table/src/TableHeader.tsx
+++ components/Table/src/TableHeader.tsx
@@ -7,11 +7,11 @@ export type TableHeaderProps = ThHTMLAttributes<HTMLTableHeaderCellElement>;
  * TableHeader component based on the <th> element.
  * Defines a cell as header of a group of table cells. The exact nature of this group is defined by the scope and headers attributes.
  */
-export const TableCell: React.FC<TableHeaderProps> = ({ className, ...props }: TableHeaderProps) => {
+export const TableHeader: React.FC<TableHeaderProps> = ({ className, ...props }: TableHeaderProps) => {
   const rootClassNames = clsx('denhaag-table__header', className);
   return (
-    <td className={rootClassNames} {...props}>
+    <th className={rootClassNames} {...props}>
       {props.children}
-    </td>
+    </th>
   );
 };
