060f4001c60f79e6d6b9ccae9712744cfb6f97ab
Bryan de Jong <bryandejong99@gmail.com>
2021-12-10 11:06:26 +0100

    docs(table): add jsdocs for table components

--- components/Table/src/TableCell.tsx
+++ components/Table/src/TableCell.tsx
@@ -4,7 +4,8 @@ import clsx from 'clsx';
 export type TableCellProps = TdHTMLAttributes<HTMLTableDataCellElement>;
 
 /**
- * Table component
+ * TableCell component based on the <td> element.
+ * Defines a cell of a table that contains data. It participates in the table model.
  */
 export const TableCell: React.FC<TableCellProps> = ({ className, ...props }: TableCellProps) => {
   const rootClassNames = clsx('denhaag-table__cell', className);
