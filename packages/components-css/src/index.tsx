714dd1244b70ee8d57586b1036eacde1db672f60
Paul van Impelen <paul@acato.nl>
2021-12-15 15:15:42 +0100

    feat(pagination): apply in components-css

--- packages/components-css/src/index.tsx
+++ packages/components-css/src/index.tsx
@@ -18,6 +18,7 @@ import '../../../components/List/src/list-item-text.css';
 import '../../../components/List/src/list-subheader.css';
 import '../../../components/Menu/src/menu.css';
 import '../../../components/Menu/src/menu-button.css';
+import '../../../components/Pagination/src/pagination.css';
 import '../../../components/Link/src/link.css';
 import '../../../components/Tab/src/styles/tab.css';
 import '../../../components/Tab/src/styles/tab-panel.css';
