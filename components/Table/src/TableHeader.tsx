befeb0fdb7c68f60725bfb80053f19eb695e350a
Paul van Impelen <paul@acato.nl>
2021-12-21 13:20:21 +0100

    feat(table): apply same changes to react stories

--- components/Table/src/TableHeader.tsx
+++ components/Table/src/TableHeader.tsx
@@ -7,10 +7,16 @@ export type TableHeaderProps = ThHTMLAttributes<HTMLTableHeaderCellElement>;
  * TableHeader component based on the <th> element.
  * Defines a cell as header of a group of table cells. The exact nature of this group is defined by the scope and headers attributes.
  */
-export const TableHeader: React.FC<TableHeaderProps> = ({ className, ...props }: TableHeaderProps) => {
-  const rootClassNames = clsx('denhaag-table__header', className);
+export const TableHeader: React.FC<TableHeaderProps> = ({ className, scope = 'col', ...props }: TableHeaderProps) => {
+  const rootClassNames = clsx('denhaag-table__heading', className);
+
+  // Set default sort property to none, when filtered it must be `ascending` or `descending`.
+  if (scope === 'col') {
+    props['aria-sort'] = 'none';
+  }
+
   return (
-    <th className={rootClassNames} {...props}>
+    <th className={rootClassNames} {...props} scope={scope}>
       {props.children}
     </th>
   );
