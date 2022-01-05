86f0a3be09e061e8e9a3d27f2c03f57d3ab8bfc1
Robbie Meijer <robbie@acato.nl>
2022-01-05 13:49:03 +0100

    feat(typography): remove css ref from components-css, buildfix: add index to Note

--- packages/components-css/src/index.tsx
+++ packages/components-css/src/index.tsx
@@ -29,7 +29,6 @@ import '../../../components/Timeline/src/styles/step-content.css';
 import '../../../components/Timeline/src/styles/step-icon.css';
 import '../../../components/Timeline/src/styles/step-label.css';
 import '../../../components/Timeline/src/styles/timeline.css';
-import '../../../components/Typography/src/bem.css';
 import '../../../components/Typography/src/heading.css';
 import '../../../components/Typography/src/paragraph.css';
 
