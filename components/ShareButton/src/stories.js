c36ab85cacdd3e970b6c4c7d4dd1024d5094a615
Joost Froon <joost@goedideemedia.nl>
2022-01-25 16:27:32 +0100

    chore: removed share-button from project

--- components/ShareButton/src/stories.js
+++
@@ -1,5 +0,0 @@
-import ShareButtonScripts from './shareButtonScripts';
-
-window.addEventListener('DOMContentLoaded', async () => {
-  window.onload = () => new ShareButtonScripts('denhaag-share-button');
-});
