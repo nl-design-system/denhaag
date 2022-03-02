b4e93d4d8c820b8b1fdec044785326c1fae154a5
Bryan de Jong <bryandejong99@gmail.com>
2022-03-02 11:47:07 +0100

    fix(form-field): allow custom classNames on FormField component

--- components/FormField/src/index.tsx
+++ components/FormField/src/index.tsx
@@ -1,11 +1,14 @@
 import React, { HTMLAttributes } from 'react';
 import './index.scss';
+import clsx from 'clsx';
 
 export type FormFieldProps = HTMLAttributes<HTMLDivElement>;
 
 export const FormField: React.FC<FormFieldProps> = (props: FormFieldProps) => {
+  const className = clsx('denhaag-form-field', props.className);
+
   return (
-    <div className={'denhaag-form-field'} {...props}>
+    <div {...props} className={className}>
       {props.children}
     </div>
   );
