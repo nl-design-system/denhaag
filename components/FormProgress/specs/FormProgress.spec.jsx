c7a9c807e1e9ce8a4c2aac9ccc0b2825716b9770
Maarten Carsjens <mjcarsjens@gmail.com>
2022-03-04 15:39:32 +0100

    feat(form-progress): added component test

---
+++ components/FormProgress/specs/FormProgress.spec.jsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import FormProgress from '../dist';
+
+describe('FormProgress tests', () => {
+  // Starting with functionality tests
+  it('does not show back button in first step', () => {
+    cy.mount(<FormProgress value={1} max={4} label="Stap 1 van 4" />);
+
+    cy.get('.denhaag-form-progress').should('not.contain', 'Vorige stap');
+  });
+
+  it('does not show back button in first step', () => {
+    cy.mount(<FormProgress value={2} max={4} label="Stap 2 van 4" />);
+
+    cy.get('.denhaag-form-progress').should('contain', 'Vorige stap');
+  });
+
+  it('a11y and snapshots', () => {
+    cy.snapshots(
+      <div style={{ width: '400px' }}>
+        <FormProgress value={2} max={4} label="Stap 2 van 4" />
+      </div>,
+      {},
+    );
+  });
+});
