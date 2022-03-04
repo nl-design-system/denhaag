3d3673feda3d6dbabb248ee183bd0f698b457d72
Maarten Carsjens <mjcarsjens@gmail.com>
2022-03-04 15:29:19 +0100

    feat(form-field): reworked form field
    fix: bit nicer typing
    
    fix(form-field): change input prop type

--- components/FormField/src/index.tsx
+++ components/FormField/src/index.tsx
@@ -2,14 +2,32 @@ import React, { HTMLAttributes } from 'react';
 import './index.scss';
 import clsx from 'clsx';
 
-export type FormFieldProps = HTMLAttributes<HTMLDivElement>;
+export interface FormFieldProps extends HTMLAttributes<HTMLDivElement> {
+  label: string;
+  input: React.ReactElement;
+  error?: boolean;
+  description?: string;
+}
 
-export const FormField: React.FC<FormFieldProps> = (props: FormFieldProps) => {
-  const className = clsx('denhaag-form-field', props.className);
+export const FormField: React.FC<FormFieldProps> = ({ label, input, error, description, ...props }: FormFieldProps) => {
+  const className = clsx('denhaag-form-field', { 'denhaag-form-field--error': error }, props.className);
+  const Input = input.type;
+  const id = input.props.id || `input-${Math.floor(Math.random() * 10000)}`;
+  const describedBy = input.props['aria-describedby'] || `description-${Math.floor(Math.random() * 10000)}`;
 
   return (
     <div {...props} className={className}>
-      {props.children}
+      <label htmlFor={id} className="denhaag-form-field__label">
+        {label}
+      </label>
+      <div className="denhaag-form-field__input">
+        <Input id={id} aria-describedby={describedBy} {...input.props} />
+      </div>
+      {description && (
+        <span id={describedBy} className="denhaag-form-field__description">
+          {description}
+        </span>
+      )}
     </div>
   );
 };
