04d06eb081c7ef26afc1795ced375719f607641e
paulAcato <94894596+paulAcato@users.noreply.github.com>
2022-03-24 10:47:14 +0100

    Feature/gdh 293 anchor mobile html (#842)
    * feat(anchor-links): reduce css and set ol instead of ul
    
    * style(anchor-navigation): added focus style
    
    * style(anchor-navigation): hide button when anchor-navigation is hidden
    
    * feat(anchor-collapse): anchor collapses which is related to anchor-navigation
    
    * docs(anchor-collapse): rename to correct component
    
    * style(anchor-collapse): remove css comments
    
    * style(anchor-collapse): apply border instead of box-shadow

--- components/Accordion/src/stories.js
+++ components/Accordion/src/stories.js
@@ -1,6 +1,5 @@
 import AccordionScripts from './accordionScripts';
 
 window.addEventListener('DOMContentLoaded', async () => {
-  console.clear();
   window.onload = () => new AccordionScripts('denhaag-accordion__panel');
 });
