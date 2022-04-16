6bc5cf2ae7612ae7c93a7eb36c8adb75e601e6d2
Bryan de Jong <bryandejong99@gmail.com>
2022-04-16 11:02:52 +0200

    build(components-css): improve build process

--- packages/components-css/rollup.config.js
+++ packages/components-css/rollup.config.js
@@ -1,26 +1,8 @@
-import typescript from '@rollup/plugin-typescript';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import svgr from '@svgr/rollup';
-import _ from 'lodash';
-import path from 'path';
 import postcss from 'rollup-plugin-postcss';
-
-import tsconfig from '../../tsconfig.json';
-
-const externalDependencies = [
-  '@material-ui/core',
-  '@material-ui/lab',
-  'react',
-  'react-dom',
-  '@material-ui/core/transitions',
-  '@material-ui/core/OverridableComponent',
-];
-const internalDependencies = _.keys(tsconfig.compilerOptions.paths);
-const dependencies = externalDependencies.concat(internalDependencies);
+import discardDuplicates from 'postcss-discard-duplicates';
 
 export default {
-  input: 'src/index.tsx',
+  input: 'src/index.scss',
   output: {
     dir: './dist',
     sourcemap: false,
@@ -29,17 +11,9 @@ export default {
   },
   plugins: [
     postcss({
-      extensions: ['.css'],
-      minimize: false,
+      extensions: ['.css', '.scss'],
+      plugins: [discardDuplicates()],
       extract: true,
     }),
-    nodeResolve(),
-    commonjs({ include: /node_modules/ }),
-    svgr({ icon: true, svgo: true, memo: true }),
-    typescript({
-      cacheDir: path.join(__dirname, '.rollup-cache'),
-      tsBuildInfoFile: 'dist/.tsbuildinfo',
-    }),
   ],
-  external: dependencies,
 };
