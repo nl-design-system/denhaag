fe5a266f29bd02582e97efaac2323ff627f75351
Bryan de Jong <bryandejong99@gmail.com>
2022-04-19 11:33:00 +0200

    feat: add timeline component

---
+++ components/TimelineSubstatus/src/stories/react.stories.tsx
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Button } from '@gemeente-denhaag/button';
+import { TimelineSubstatus, TimelineSubstatusProps } from '../index';
+import pkg from '../../package.json';
+
+export default {
+  title: 'React/Navigation/Timeline Substatus',
+  parameters: {
+    componentSubtitle: `${pkg.name} - ${pkg.version}`,
+    docs: {
+      source: {
+        type: 'code',
+      },
+    },
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+  component: TimelineSubstatus,
+} as Meta;
+
+const Template: Story<TimelineSubstatusProps> = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const [completed, setCompleted] = React.useState<{ [k: number]: boolean }>({});
+  const steps = [
+    {
+      label: 'First Step in a Timeline!',
+      description: 'This is the first Step component of many in a Timeline component.',
+    },
+    {
+      label: 'Second Step in a Timeline!',
+    },
+    {
+      label: 'Third Step in a Timeline!',
+      disabled: true,
+    },
+    {
+      label: 'Fourth Step in a Timeline!',
+      description: 'This is the fourth Step component of many in a Timeline component.',
+    },
+  ];
+
+  const prev = () => {
+    setActiveStep((prevActiveStep) => {
+      let nextActiveStep = prevActiveStep - 1;
+      if (steps[nextActiveStep].disabled) {
+        nextActiveStep -= 1;
+      }
+
+      const newCompleted = completed;
+      newCompleted[nextActiveStep] = false;
+      setCompleted(newCompleted);
+
+      return nextActiveStep;
+    });
+  };
+
+  const next = () => {
+    setActiveStep((prevActiveStep) => {
+      let nextActiveStep = prevActiveStep + 1;
+
+      if (nextActiveStep < steps.length && steps[nextActiveStep].disabled) {
+        nextActiveStep += 1;
+      }
+
+      const newCompleted = completed;
+      newCompleted[prevActiveStep] = true;
+      setCompleted(newCompleted);
+
+      return nextActiveStep;
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <TimelineSubstatus></TimelineSubstatus>
+      <Button onClick={prev} disabled={activeStep === 0} variant="secondary-action">
+        Prev
+      </Button>
+      <Button onClick={next} disabled={activeStep === steps.length} variant="primary-action">
+        Next
+      </Button>
+    </React.Fragment>
+  );
+};
+
+/**
+ * Implementation of Timeline
+ * "activeStep" property set to enforce step visibility.
+ */
+export const Default: Story<TimelineSubstatusProps> = Template.bind({});
