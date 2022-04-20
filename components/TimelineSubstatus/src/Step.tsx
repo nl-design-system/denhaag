dee0bf5b909f29beb9f56c5a0cc8704a806aa10d
Maarten Carsjens <mjcarsjens@gmail.com>
2022-04-20 14:04:55 +0200

    feat(steps): added expanded state context
    refactor(progress-steps): default button user-agent style
    
    fix: fix expanding by actually using state
    
    refactor(steps): added nicer typing

--- components/TimelineSubstatus/src/Step.tsx
+++ components/TimelineSubstatus/src/Step.tsx
@@ -7,16 +7,33 @@ export interface StepProps extends LiHTMLAttributes<HTMLLIElement> {
   expanded?: boolean;
 }
 
-export const Step: React.FC<StepProps> = ({ children, current, checked, expanded }) => (
-  <li
-    className={clsx(
-      'denhaag-process-steps__step',
-      checked && 'denhaag-process-steps__step--checked',
-      current && 'denhaag-process-steps__step--current',
-      expanded && 'denhaag-process-steps__step--expanded',
-    )}
-    aria-current={current ? 'step' : undefined}
-  >
-    {children}
-  </li>
-);
+interface StepContextType {
+  expanded: boolean;
+  setExpanded: (arg0: boolean) => void;
+}
+
+export const StepContext = React.createContext<StepContextType>({
+  expanded: false,
+  setExpanded: () => {},
+});
+
+export const Step: React.FC<StepProps> = ({ children, current, checked, expanded = false }) => {
+  const [state, setState] = React.useState(expanded);
+  const context: StepContextType = { expanded: state, setExpanded: setState };
+
+  return (
+    <StepContext.Provider value={context}>
+      <li
+        className={clsx(
+          'denhaag-process-steps__step',
+          checked && 'denhaag-process-steps__step--checked',
+          current && 'denhaag-process-steps__step--current',
+          state && 'denhaag-process-steps__step--expanded',
+        )}
+        aria-current={current ? 'step' : undefined}
+      >
+        {children}
+      </li>
+    </StepContext.Provider>
+  );
+};
