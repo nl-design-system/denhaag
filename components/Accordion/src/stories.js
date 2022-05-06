03df579a1aed496431148562e8af65fc6c7ae9e4
Robbie@acato <95216123+robbieatacato@users.noreply.github.com>
2022-05-06 13:36:29 +0200

    refactor(accordion): change bem, css, tokens for better accessibility (#938)
    * refactor(accordion): change bem, css, tokens for better accessibility
    
    * refactor: change bem title

--- components/Accordion/src/stories.js
+++
@@ -1,5 +0,0 @@
-import AccordionScripts from './accordionScripts';
-
-window.addEventListener('DOMContentLoaded', async () => {
-  window.onload = () => new AccordionScripts('denhaag-accordion__panel');
-});
