c05887a28c594ef6d974209738a32b8e487272bf
Bryan de Jong <bryandejong99@gmail.com>
2022-05-12 11:21:53 +0200

    feat: add posttypelabel react component

---
+++ components/PosttypeLabel/src/index.tsx
@@ -0,0 +1,10 @@
+import React, { HTMLAttributes } from 'react';
+import clsx from 'clsx';
+
+export const PostTypeLabel = (props: HTMLAttributes<HTMLParagraphElement>) => {
+  return (
+    <p {...props} className={clsx('denhaag-posttypelabel')}>
+      {props.children}
+    </p>
+  );
+};
