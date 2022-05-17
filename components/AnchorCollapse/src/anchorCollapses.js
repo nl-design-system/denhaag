87a24ab47fd4e8d152faa796e8e8572fb1f5d66a
Paul van Impelen <paul@acato.nl>
2022-05-17 15:20:10 +0200

    fix(anhcor-collapse): new JS and modify styles

--- components/AnchorCollapse/src/anchorCollapses.js
+++ components/AnchorCollapse/src/anchorCollapses.js
@@ -6,17 +6,52 @@ export default class AnchorCollapses {
       return;
     }
 
+    this.classNames = {
+      active: `${className}--active`,
+      toggle: `${className}__toggle`,
+    };
+
+    this.openOrCloseCollapses();
+    this.clickEvents();
     this.windowEvents();
   }
 
   windowEvents() {
     const self = this;
+    const ww = window.innerWidth;
     self.windowEventsLoop();
     window.addEventListener('resize', async () => {
-      self.windowEventsLoop();
+      // Check for width, for mobile devices where the url-bar overlay counts as resize.
+      if (ww !== window.innerWidth) {
+        self.openOrCloseCollapses();
+        self.windowEventsLoop();
+      }
     });
     window.addEventListener('orientationchange', async () => {
+      self.openOrCloseCollapses();
       self.windowEventsLoop();
+    });
+  }
+
+  /**
+   * Update openCollapses check.
+   */
+  openOrCloseCollapses() {
+    this.openCollapses = window.matchMedia('(min-width: 768px)').matches;
+  }
+
+  /**
+   * Fired on click events.
+   */
+  clickEvents() {
+    [...this.panels].forEach((panel) => {
+      [...panel.getElementsByClassName(this.classNames.toggle)].forEach(
+        (toggle) =>
+          (toggle.onclick = () => {
+            toggle.setAttribute('aria-expanded', toggle.getAttribute('aria-expanded') === 'false' ? 'true' : 'false');
+            panel.classList.toggle(this.classNames.active);
+          }),
+      );
     });
   }
 
@@ -24,11 +59,22 @@ export default class AnchorCollapses {
    * Loop through panels and close them based on the windowWidth.
    */
   windowEventsLoop() {
-    const openCollapses = window.matchMedia('(min-width: 768px)').matches;
     [...this.panels].forEach((panel) => {
-      [...panel.getElementsByTagName('details')].forEach((details) => {
-        details.open = openCollapses;
-      });
+      let expanded = null;
+
+      if (this.openCollapses && !panel.classList.contains(this.classNames.active)) {
+        panel.classList.add(this.classNames.active);
+        expanded = 'true';
+      } else if (panel.classList.contains(this.classNames.active)) {
+        panel.classList.remove(this.classNames.active);
+        expanded = 'false';
+      }
+
+      if (expanded) {
+        [...panel.getElementsByClassName(this.classNames.toggle)].forEach((toggle) =>
+          toggle.setAttribute('aria-expanded', expanded),
+        );
+      }
     });
   }
 }
