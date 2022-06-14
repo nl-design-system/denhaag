3b856c068791a83d5097a783979a2d1c2d2c8408
Robbie Meijer <robbie@acato.nl>
2022-06-14 18:38:56 +0200

    fix(floating contact): wip, added keyboard events

--- components/FloatingContact/src/FloatingContact.js
+++ components/FloatingContact/src/FloatingContact.js
@@ -19,8 +19,8 @@ export default class FloatingContact {
     }
 
     this.links = [...this.element.getElementsByClassName(`${id}__item-link`)];
-
     this.toggle.onclick = () => this.toggleEvents();
+    this.element.addEventListener('keydown', (event) => this.keyBoardEvents(event));
   }
 
   /**
@@ -33,5 +33,48 @@ export default class FloatingContact {
       ? this.toggle?.dataset?.labelClose || 'Close'
       : this.toggle?.dataset?.labelOpen || 'Open';
     this.links.forEach((link) => (link.tabIndex = state ? 0 : -1));
+  }
+
+  keyBoardEvents(event) {
+    const floatingContactSwitchExpanded = this.element
+      .querySelector('.denhaag-floating-contact-switch')
+      .getAttribute('aria-expanded');
+
+    // escape key
+    if (event.key === 'Escape' && floatingContactSwitchExpanded === 'true') {
+      setTimeout(() => {
+        this.toggleEvents();
+        this.toggle.focus();
+      }, 1);
+    }
+
+    // tab forwards
+    if (!event.shiftKey && event.key === 'Tab' && floatingContactSwitchExpanded === 'true') {
+      setTimeout(() => {
+        // this must be located inside timeout, because of the key event delay
+        const floatingContactItemLinkFocus = document.activeElement.classList.contains(
+          'denhaag-floating-contact__item-link',
+        );
+
+        if (!floatingContactItemLinkFocus) {
+          this.toggleEvents();
+        }
+      }, 1);
+    }
+
+    // tab backwards
+    if (event.shiftKey && event.key === 'Tab') {
+      setTimeout(() => {
+        // this must be located inside timeout, because of the key event delay
+        const floatingContactSwitchFocus = document.activeElement.classList.contains('denhaag-floating-contact-switch');
+        const floatingContactItemLinkFocus = document.activeElement.classList.contains(
+          'denhaag-floating-contact__item-link',
+        );
+
+        if (!floatingContactSwitchFocus && floatingContactSwitchExpanded === 'true' && !floatingContactItemLinkFocus) {
+          this.toggleEvents();
+        }
+      }, 1);
+    }
   }
 }
