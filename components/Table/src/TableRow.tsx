0bc8c6cfaa1877dd9f1c1c0e336104a720f77a93
Robbie Meijer <robbie@acato.nl>
2022-08-08 11:06:08 +0200

    fix(table): remove tabindex for accessibility

--- components/Table/src/TableRow.tsx
+++ components/Table/src/TableRow.tsx
@@ -7,10 +7,10 @@ export type TableRowProps = HTMLAttributes<HTMLTableRowElement>;
  * TableRow component based on the <tr> element.
  * Defines a row of cells in a table. The row's cells can then be established using a mix of <TableCell> (<td>) and <TableHeader> (<th>) elements.
  */
-export const TableRow: React.FC<TableRowProps> = ({ className, tabIndex = 0, ...props }: TableRowProps) => {
+export const TableRow: React.FC<TableRowProps> = ({ className, ...props }: TableRowProps) => {
   const rootClassNames = clsx('denhaag-table__row', className);
   return (
-    <tr className={rootClassNames} {...props} tabIndex={tabIndex}>
+    <tr className={rootClassNames} {...props}>
       {props.children}
     </tr>
   );
