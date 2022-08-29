25ae91908c5d2c0aee36c445f920eec0d4e8ed33
Robbert Broersma <git@robbertbroersma.nl>
2022-08-29 22:54:02 +0200

    fix: remove hardcoded `--distanced` class name for headings

--- components/Typography/src/Heading4.tsx
+++ components/Typography/src/Heading4.tsx
@@ -6,6 +6,6 @@ import clsx from 'clsx';
 export type Heading4Props = Omit<BaseDataDisplayProps, 'classes'>;
 
 export const Heading4: React.FC<Heading4Props> = (props: Heading4Props) => {
-  const rootClassNames = clsx('utrecht-heading-4', 'utrecht-heading-4--distanced', props.className);
+  const rootClassNames = clsx('utrecht-heading-4', props.className);
   return <h4 className={rootClassNames}>{props.children}</h4>;
 };
