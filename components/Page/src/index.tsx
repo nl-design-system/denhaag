9f0218d636e7a726429638ca552fe692da3db25a
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2022-08-30 15:01:47 +0200

    feat: added header and page components for login page

---
+++ components/Page/src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BaseProps from '@gemeente-denhaag/baseprops';
+import clsx from 'clsx';
+import './index.scss';
+
+export interface PageProps extends Omit<BaseProps, 'tabIndex' | 'classNamees'> {
+  /**
+   * The header component
+   */
+  header?: React.ReactElement;
+}
+
+export const Page: React.FC<PageProps> = (props: PageProps) => {
+  const classNames = clsx('denhaag-page', props.className);
+  return (
+    <div className={classNames}>
+      <div className="denhaag-page-header">{props.header}</div>
+      <div className="denhaag-page-content">
+        <main className="denhaag-page-content__main">{props.children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
