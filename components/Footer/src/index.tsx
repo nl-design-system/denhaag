51f1a7fe41c3d39645eb79939160c800879af012
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2022-12-13 14:15:45 +0100

    feat: wip: first working footer component, added footer to login page template

--- components/Footer/src/index.tsx
+++ components/Footer/src/index.tsx
@@ -1,19 +1,164 @@
 import React from 'react';
 import BaseProps from '@gemeente-denhaag/baseprops';
-import clsx from 'clsx';
+import ResponsiveContent from '/components/ResponsiveContent';
+
 import './index.scss';
 
-export interface FooterProps extends Omit<BaseProps, 'tabIndex' | 'classNamees' | 'children'> {
-  /**
-   * The navigation component
-   */
-  navigation?: React.ReactElement;
+export interface FooterListItemData {
+  label: string;
+  url: string;
 }
 
-export const Footer: React.FC<FooterProps> = (props: FooterProps) => {
-  const classNames = clsx('denhaag-footer', props.className);
+export interface FooterLegalProps extends Omit<BaseProps, 'tabIndex' | 'classNamees' | 'children'> {
+  legalData?: Array<FooterListItemData>;
+  copyrightLabel?: string;
+}
 
-  return <footer className={classNames}></footer>;
+export interface FooterSocialLinks {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface FooterSocialData {
+  links: Array<FooterSocialLinks>;
+  title: string;
+}
+
+export interface NewsLetterData {
+  title: string;
+  text: string;
+  buttonText: string;
+  url: string;
+}
+
+export interface ContactData {
+  title: string;
+  links: Array<FooterListItemData>;
+}
+
+export interface FooterContactProps extends Omit<BaseProps, 'tabIndex' | 'classNamees' | 'children'> {
+  socialData?: FooterSocialData;
+  newsletterData?: NewsLetterData;
+  contactData?: ContactData;
+}
+
+export interface FooterProps extends Omit<BaseProps, 'tabIndex' | 'classNamees' | 'children'> {
+  legalData?: Array<FooterListItemData>;
+  copyrightLabel?: string;
+  socialData?: FooterSocialData;
+  newsletterData?: NewsLetterData;
+  contactData?: ContactData;
+}
+
+export const FooterLegal: React.FC<FooterLegalProps> = (props: FooterLegalProps) => {
+  return (
+    <section className="denhaag-footer__legal">
+      <div className="denhaag-responsive-content">
+        <div className="denhaag-footer-legal__container">
+          {props.legalData && (
+            <div className="denhaag-footer__legal-links">
+              <ul className="denhaag-list denhaag-list--vertical">
+                {props.legalData?.map((item, key) => (
+                  <li className="denhaag-list__item" key={key}>
+                    <a href={item.url} className="denhaag-link">
+                      <span className="denhaag-link__label">{item.label}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {props.copyrightLabel && (
+            <div className="denhaag-footer__legal-copyright">
+              <p className="utrecht-paragraph">{props.copyrightLabel}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export const FooterContact: React.FC<FooterContactProps> = (props: FooterContactProps) => {
+  return (
+    <section className="denhaag-footer__contact">
+      <ResponsiveContent>
+        <div className="denhaag-footer-group">
+          {props.newsletterData && (
+            <div className="denhaag-link-group denhaag-link-group--dark">
+              <h4 className="utrecht-heading-4 denhaag-link-group__caption">{props.newsletterData.title}</h4>
+              <p className="utrecht-paragraph">{props.newsletterData.text}</p>
+              <div className="denhaag-button-group">
+                <a href={props.newsletterData.url} className="denhaag-button denhaag-button--primary-action">
+                  {props.newsletterData.buttonText}
+                </a>
+              </div>
+            </div>
+          )}
+          {props.socialData && (
+            <div className="denhaag-link-group denhaag-link-group--dark">
+              <h4 className="utrecht-heading-4 denhaag-link-group__caption">{props.socialData.title}</h4>
+              <ul className="utrecht-list-social">
+                {props.socialData.links.map((item, key) => (
+                  <li className="utrecht-list-social__item" key={key}>
+                    <a href={item.url} className="utrecht-link-social" title={item.title}>
+                      {item.icon}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+          {props.contactData && (
+            <div className="denhaag-link-group denhaag-link-group--dark">
+              <h4 className="utrecht-heading-4 denhaag-link-group__caption">{props.contactData.title}</h4>
+              <ul className="utrecht-link-list utrecht-link-list--html-ul denhaag-link-group__list">
+                {props.contactData.links.map((item, key) => (
+                  <li className="denhaag-link-group__list-item" key={key}>
+                    <a href={item.url} className="denhaag-link denhaag-link--with-icon denhaag-link--with-icon-start">
+                      <span className="denhaag-link__icon">
+                        <svg
+                          width="1em"
+                          height="1em"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="denhaag-icon"
+                          focusable="false"
+                          aria-hidden="true"
+                          shapeRendering="auto"
+                        >
+                          <path
+                            d="M12.293 5.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L16.586 13H5a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                            fill="currentColor"
+                          ></path>
+                        </svg>
+                      </span>
+                      <span className="denhaag-link__label">{item.label}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+      </ResponsiveContent>
+    </section>
+  );
+};
+
+export const Footer: React.FC<FooterProps> = (props: FooterProps) => {
+  return (
+    <footer className="denhaag-footer">
+      <FooterContact
+        socialData={props.socialData}
+        newsletterData={props.newsletterData}
+        contactData={props.contactData}
+      />
+      <FooterLegal legalData={props.legalData} copyrightLabel={props.copyrightLabel} />
+    </footer>
+  );
 };
 
 export default Footer;
