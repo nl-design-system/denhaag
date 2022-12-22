4f64781655a7538c20a1ef8e352d994627cc1c00
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2022-12-22 16:05:01 +0100

    feat: wip header sidenav changes + overview template

--- components/Header/src/index.tsx
+++ components/Header/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BaseProps from '@gemeente-denhaag/baseprops';
 import HeaderLogo from '@gemeente-denhaag/header-logo';
 import ResponsiveContent from '@gemeente-denhaag/responsive-content';
+import { MenuButtonExpandable } from '@gemeente-denhaag/menu';
 import clsx from 'clsx';
 import './index.scss';
 
@@ -39,7 +40,15 @@ export const Header: React.FC<HeaderProps> = (props: HeaderProps) => {
             </a>
           </div>
           {props.navigation && <div className="denhaag-header__navigation">{props.navigation}</div>}
-          {props.actions && <div className="denhaag-header__actions">{props.actions}</div>}
+          {/* {props.actions && <div className="denhaag-header__actions">{props.actions}</div>} */}
+          <div className="denhaag-header__actions">
+            <div className="denhaag-language-switcher">
+              <MenuButtonExpandable>NL</MenuButtonExpandable>
+            </div>
+            <div className="denhaag-user-menu">
+              <MenuButtonExpandable>Welkom Anne Klap</MenuButtonExpandable>
+            </div>
+          </div>
         </div>
       </ResponsiveContent>
       {props.breadcrumb}
