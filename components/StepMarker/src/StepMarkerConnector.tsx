e4e8760ada9f41212bbd4ba80b630b56bfa43527
Yolijn van der Kolk <yolijn@frameless.io>
2023-01-10 11:29:36 +0100

    refactor: rename StepLine to StepMarkerConnector and move to StepMarker component

---
+++ components/StepMarker/src/StepMarkerConnector.tsx
@@ -0,0 +1,34 @@
+import React, { HTMLAttributes } from 'react';
+
+import clsx from 'clsx';
+
+type MarkerType = 'main' | 'nested';
+type Appearance = 'checked' | 'not-checked' | 'current' | 'warning' | 'error';
+
+interface StepMarkerConnectorProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
+  from: MarkerType;
+  to: MarkerType;
+  appearance?: Appearance;
+}
+
+export const StepMarkerConnector: React.FC<StepMarkerConnectorProps> = ({
+  className,
+  appearance = 'not-checked',
+  from,
+  to,
+  ...props
+}: StepMarkerConnectorProps) => {
+  return (
+    <div
+      {...props}
+      className={clsx(
+        'denhaag-step-marker__connector',
+        `denhaag-step-marker__connector--${appearance}`,
+        `denhaag-step-marker__connector--${from}-to-${to}`,
+        className,
+      )}
+    ></div>
+  );
+};
+
+export default StepMarkerConnector;
