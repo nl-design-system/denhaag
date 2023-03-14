e567c911f92abe4fbae478bbf8b145e8ac23e00c
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-03-14 11:14:16 +0100

    fix: remove bat file for local development

--- build_proprietary.bat
+++
@@ -1,1 +0,0 @@
-cd C:\Git\MDH\platform\denhaag\proprietary\Common && yarn build && cd C:\Git\MDH\platform\denhaag\proprietary\Components && yarn build && cd C:\Git\MDH\platform\denhaag\proprietary\Proprietary && yarn build
