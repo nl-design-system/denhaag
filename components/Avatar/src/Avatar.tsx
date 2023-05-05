f7469a0d96524cbff1b58b1fbdfb998992a9703a
Yolijn <yolijn@frameless.io>
2023-05-05 19:04:00 +0200

    refactor: remove material-ui dependency from avatar
    Add stylesheet with current avatar styles and fallbacks and remove material dependency

--- components/Avatar/src/Avatar.tsx
+++ components/Avatar/src/Avatar.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Avatar as MaterialAvatar } from '@material-ui/core';
-import BaseProps from '@gemeente-denhaag/baseprops';
+import clsx from 'clsx';
+import React, { ElementType, HTMLAttributes } from 'react';
 
-export interface AvatarProps extends BaseProps {
+export interface AvatarProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * Used in combination with src or srcSet to provide an alt attribute for the rendered img element.
    */
@@ -11,17 +10,23 @@ export interface AvatarProps extends BaseProps {
   /**
    * The component used for the root node.
    * Either a string to use a HTML element or a component.
+   *
+   * @deprecated
    */
-  component?: React.ElementType;
+  component?: ElementType;
 
   /**
    * Attributes applied to the img element if the component is used to display an image.
    * It can be used to listen for the loading error event.
+   *
+   * @deprecated
    */
   imgProps?: React.ImgHTMLAttributes<HTMLImageElement>;
 
   /**
-   * he sizes attribute for the img element.
+   * Defines a set of media conditions (e.g. screen widths)
+   * and indicates what image size would be best to choose.
+   * Combine with `srcSet` to set different image sources for each size.
    */
   sizes?: string;
 
@@ -31,13 +36,15 @@ export interface AvatarProps extends BaseProps {
   src?: string;
 
   /**
-   * The srcSet attribute for the img element.
-   * Use this attribute for responsive image display.
+   * Defines the set of images we will allow the browser to choose between
+   * Each set of image information is separated from the previous one by a comma.
+   * Combine with `sizes` to set the screen-widths to combine these various images with.
    */
   srcSet?: string;
 
   /**
    * The shape of the avatar.
+   * @deprecated `circle` is deprecated use `circular` instead
    */
   variant?: 'circle' | 'circular' | 'rounded' | 'square';
 }
@@ -47,8 +54,36 @@ export interface AvatarProps extends BaseProps {
  * @param props The properties of an Avatar component.
  * @constructor Constructs an instance of Avatar.
  */
-export const Avatar: React.FC<AvatarProps> = (props: AvatarProps) => {
-  return <MaterialAvatar {...props}>{props.children}</MaterialAvatar>;
+export const Avatar = ({
+  variant = 'circular',
+  className,
+  children,
+  alt,
+  sizes,
+  src,
+  srcSet,
+  component = 'div',
+  imgProps,
+}: AvatarProps) => {
+  const classNames = clsx('denhaag-avatar', `denhaag-avatar--${variant}`, className);
+  const Component = component || 'div';
+
+  const imageProps = {
+    src: src || imgProps?.src,
+    sizes: sizes || imgProps?.sizes,
+    srcSet: srcSet || imgProps?.srcSet,
+    alt: alt || imgProps?.alt,
+  };
+
+  return (
+    <Component className={classNames}>
+      {imageProps.alt ? (
+        <img className={clsx('denhaag-avatar__image', imgProps?.className)} {...imageProps} alt={imageProps.alt} />
+      ) : (
+        children
+      )}
+    </Component>
+  );
 };
 
 export default Avatar;
