81a08a1756bea75dce937af6e6543cb4b40cfd2a
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-06-21 13:59:43 +0200

    feat: cleanup card component code: created smart react components which uses atomic components

---
+++ components/Card/src/Card/SubjectCard.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { ArrowRightIcon } from '@gemeente-denhaag/icons';
+import { Paragraph } from '@gemeente-denhaag/typography';
+import './card.scss';
+import Card from './Card';
+import CardActions from './CardActions';
+import CardContent from './CardContent';
+import CardTextWrapper from './CardTextWrapper';
+import CardDateWrapper from './CardDateWrapper';
+import CardDate from './CardDate';
+import CardAction from './CardAction';
+
+export interface SubjectCardProps {
+  /**
+   * Determines the title of the card
+   */
+  title: string;
+
+  /**
+   * Determines the subtitle of the card
+   */
+  subTitle?: string;
+
+  /**
+   * Determines the date shown on the card
+   */
+  date?: Date;
+
+  /**
+   * Determines the url the card points to
+   */
+  href?: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const SubjectCard: React.FC<SubjectCardProps> = ({ title, subTitle, date, href }: SubjectCardProps) => {
+  return (
+    <Card className="denhaag-subject-card">
+      <CardContent>
+        <CardTextWrapper>
+          <Paragraph className="denhaag-card__title">{title}</Paragraph>
+          <Paragraph className="denhaag-card__subtitle">{subTitle}</Paragraph>
+        </CardTextWrapper>
+        <CardActions>
+          {date && (
+            <CardDateWrapper>
+              <CardDate dateTime={date.toISOString()}>{date.toLocaleDateString()}</CardDate>
+            </CardDateWrapper>
+          )}
+          <CardAction href={href}>
+            <ArrowRightIcon className="denhaag-card__arrow-icon" />
+          </CardAction>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
+
+/**
+ * Default export for Card
+ */
+export default SubjectCard;
