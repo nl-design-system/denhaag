5d965500fb71b954b14fd9c94ac26b603d812292
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-07-05 15:37:11 +0200

    fix: fix exports for react card components

--- components/Card/src/index.tsx
+++ components/Card/src/index.tsx
@@ -1,4 +1,13 @@
-import Card from './Card/Card';
+import './Card/card.scss';
 
-export default Card;
 export * from './Card/Card';
+export * from './Card/CardAction';
+export * from './Card/CardActions';
+export * from './Card/CardBackground';
+export * from './Card/CardContent';
+export * from './Card/CardDate';
+export * from './Card/CardDateWrapper';
+export * from './Card/CardTextWrapper';
+export * from './Card/CardWrapper';
+export * from './Card/CaseCard';
+export * from './Card/SubjectCard';
