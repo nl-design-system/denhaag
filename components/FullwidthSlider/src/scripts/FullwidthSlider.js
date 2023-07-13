7a3e44d2c1ec29a102ae310cd209831cb0a10377
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2023-07-13 14:39:50 +0200

    feat: gdh-1259 | fullwidth swiper

--- components/FullwidthSlider/src/scripts/FullwidthSlider.js
+++ components/FullwidthSlider/src/scripts/FullwidthSlider.js
@@ -19,6 +19,7 @@ const ACTIVE_BULLET_CLASS = 'denhaag-fullwidth-slider__pagination-bullet--active
 
 const FullwidthSlider = () => {
   // Wrapping this in a try catch statement allows us to read errors easier in storybook
+
   try {
     const swiper = new Swiper(`.${SWIPER_CLASS}`, {
       modules: [Navigation, Pagination, Autoplay, EffectCreative],
@@ -26,7 +27,7 @@ const FullwidthSlider = () => {
       speed: 1000,
       spaceBetween: 0,
       slidesPerView: 1,
-      grabCursor: true,
+      grabCursor: false,
       slideClass: SLIDE_CLASS,
       slideActiveClass: ACTIVE_SLIDE_CLASS,
       autoplay: {
@@ -45,16 +46,15 @@ const FullwidthSlider = () => {
         bulletClass: BULLET_CLASS,
         bulletActiveClass: ACTIVE_BULLET_CLASS,
         dynamicBullets: true,
-        dynamicMainBullets: 3,
+        dynamicMainBullets: 5,
       },
       effect: 'creative',
       creativeEffect: {
         prev: {
-          shadow: false,
-          translate: ['-20%', 0, 0],
+          translate: ['-20%', 0, -1],
         },
         next: {
-          translate: ['100%', 0, 0],
+          translate: ['100%', 0, 1],
         },
       },
     });
@@ -74,6 +74,20 @@ const FullwidthSlider = () => {
         }
       });
     }
+
+    swiper.on('slidePrevTransitionStart', () => {
+      swiper.slides.forEach((slide) => slide.classList.remove('denhaag-fullwidth-slider__reverse-slide-direction'));
+      const activeSlideIndex = swiper.activeIndex;
+      const activeSlide = swiper.slides[activeSlideIndex];
+      const slideJustLeft = swiper.slides[activeSlideIndex + 1];
+      activeSlide.classList.add('denhaag-fullwidth-slider__remove-animation');
+      slideJustLeft.classList.add('denhaag-fullwidth-slider__reverse-slide-direction');
+    });
+
+    swiper.on('slideNextTransitionStart', () => {
+      const activeSlide = swiper.slidesEl.querySelector(`.${ACTIVE_SLIDE_CLASS}`);
+      activeSlide.classList.remove('denhaag-fullwidth-slider__remove-animation');
+    });
   } catch (error) {
     console.log(error);
   }
