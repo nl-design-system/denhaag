a39674708245624403fe3bd0044a114d1fd0967f
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2023-07-26 14:51:38 +0100

    feat: gdh-1258 | inline carousel variants

--- components/InlineCarousel/src/InlineCarousel.js
+++ components/InlineCarousel/src/InlineCarousel.js
@@ -13,7 +13,8 @@ const PAGINATION_CLASS = 'denhaag-inline-carousel__pagination';
 const PAGINATION_CLASS_DYNAMIC = 'denhaag-inline-carousel__pagination--dynamic';
 const BULLET_CLASS = 'denhaag-inline-carousel__pagination-bullet';
 const ACTIVE_BULLET_CLASS = 'denhaag-inline-carousel__pagination-bullet-active';
-//const PORTRAIT_IMAGE_CLASS = 'denhaag-inline-carousel__slide-image--portrait';
+const DOWNLOAD_BUTTON_CLASS = 'denhaag-inline-carousel__content-download';
+const PORTRAIT_IMAGE_CLASS = 'denhaag-inline-carousel__slide-image--portrait';
 const PREVIOUS_SLIDE_LABEL = 'Previous slide';
 const NEXT_SLIDE_LABEL = 'Next slide';
 
@@ -23,12 +24,24 @@ const InlineCarousel = () => {
     carousels.forEach((carousel) => {
       const slideCount = carousel.querySelectorAll(`.${SLIDE_CLASS}`)?.length;
 
+      let prefersReduceMotion = false;
+
+      if ('matchMedia' in window) {
+        const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+        if (mediaQuery.matches) prefersReduceMotion = true;
+      }
+
+      // Only for storybook
+      if (carousel.classList.contains('denhaag-inline-carousel--prefers-reduced-motion')) {
+        prefersReduceMotion = true;
+      }
+
       // Wrapping this in a try catch statement allows us to read errors easier in storybook
       try {
         const swiper = new Swiper(carousel, {
           modules: [Navigation, Pagination, Autoplay],
           loop: true,
-          speed: 800,
+          speed: prefersReduceMotion ? 0 : 800,
           spaceBetween: 10,
           a11y: {
             prevSlideMessage: PREVIOUS_SLIDE_LABEL,
@@ -55,6 +68,28 @@ const InlineCarousel = () => {
             const paginationEl = carousel.querySelector(`.${PAGINATION_CLASS}`);
             paginationEl.classList.add(PAGINATION_CLASS_DYNAMIC);
           }
+
+          swiper.on('slideChangeTransitionStart', () => {
+            // Remove tabindex from all inactive slides
+            const downloadButtons = carousel.querySelectorAll(`.${DOWNLOAD_BUTTON_CLASS}`);
+            if (downloadButtons) downloadButtons.forEach((button) => (button.tabIndex = -1));
+
+            // Add tabindex to current active slide
+            const activeSlideIndex = swiper.activeIndex;
+            const activeSlide = swiper.slides[activeSlideIndex];
+            const activeSlideDownloadBtn = activeSlide.querySelector(`.${DOWNLOAD_BUTTON_CLASS}`);
+            if (activeSlideDownloadBtn) {
+              activeSlideDownloadBtn.tabIndex = 0;
+            }
+          });
+
+          swiper.slides?.forEach((slide) => {
+            const portraitImage = slide.querySelector(`.${PORTRAIT_IMAGE_CLASS}`);
+            if (portraitImage) {
+              const imageSrc = portraitImage.querySelector('img').src;
+              portraitImage.style.setProperty('--denhaag-inline-carousel-background-image-url', `url("${imageSrc}")`);
+            }
+          });
         }
       } catch (error) {
         console.log(error);
