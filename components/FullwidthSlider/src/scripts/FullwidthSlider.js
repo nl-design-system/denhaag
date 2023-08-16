3ac676b621b1d670f1292025eb6440dcc5a5f0da
Bleddyn Pijpers <bleddyn.pijpers@cloud-studio.co.uk>
2023-08-16 14:48:03 +0100

    feat: gdh-1272 | JS refactor from WP site

--- components/FullwidthSlider/src/scripts/FullwidthSlider.js
+++ components/FullwidthSlider/src/scripts/FullwidthSlider.js
@@ -24,14 +24,27 @@ const ACTIVE_BULLET_CLASS = 'denhaag-fullwidth-slider__pagination-bullet--active
 const PORTRAIT_IMAGE_CLASS = 'denhaag-fullwidth-slider__slide-image--portrait';
 const PREVIOUS_SLIDE_LABEL = 'Previous slide';
 const NEXT_SLIDE_LABEL = 'Next slide';
-const PAUSE_SLIDER_LABEL = 'Pause slider';
-const PLAY_SLIDER_LABEL = 'Play slider';
+const PAUSE_SLIDER_LABEL = 'Pause image gallery slider';
+const PLAY_SLIDER_LABEL = 'Play image gallery slider';
+const A11Y_SLIDER_INDICATOR_CLASS = 'denhaag-fullwidth-slider__a11y-slide-indicator';
 
 const FullwidthSlider = () => {
   const sliders = document.querySelectorAll(`.${SWIPER_CLASS}`);
   if (sliders) {
     sliders.forEach((slider) => {
-      const slideCount = slider.querySelectorAll(`.${SLIDE_CLASS}`)?.length;
+      // Selectors
+      const getElements = (inputClass) => slider.querySelectorAll(`.${inputClass}`);
+      const getOneElement = (inputClass) => slider.querySelector(`.${inputClass}`);
+
+      const allSlides = getElements(SLIDE_CLASS);
+      const slideCount = allSlides?.length;
+      const playpause = getOneElement(PLAYPAUSE_CLASS);
+      const swiperTitle = getOneElement(SWIPER_TITLE_CLASS);
+      const a11YSliderIndicator = getOneElement(A11Y_SLIDER_INDICATOR_CLASS);
+      const paginationEl = getOneElement(PAGINATION_CLASS);
+      const portraitImages = getElements(PORTRAIT_IMAGE_CLASS);
+
+      // Check user preferences before initializing swiper
       let prefersReduceMotion = false;
 
       if ('matchMedia' in window) {
@@ -39,142 +52,174 @@ const FullwidthSlider = () => {
         if (mediaQuery.matches) prefersReduceMotion = true;
       }
 
-      // Only for storybook
       if (slider.classList.contains('denhaag-fullwidth-slider--prefers-reduced-motion')) {
         prefersReduceMotion = true;
       }
 
-      const prevEffect = !prefersReduceMotion ? ['-20%', 0, -1] : ['0%', 0, -1];
-      const nextEffect = !prefersReduceMotion ? ['100%', 0, 1] : ['0%', 0, -1];
+      const swiper = new Swiper(slider, {
+        modules: [Navigation, Pagination, Autoplay, Keyboard, EffectCreative],
+        loop: true,
+        speed: prefersReduceMotion ? 0 : 800,
+        spaceBetween: 0,
+        slidesPerView: 1,
+        grabCursor: false,
+        slideClass: SLIDE_CLASS,
+        slideActiveClass: ACTIVE_SLIDE_CLASS,
+        lazy: true,
+        keyboard: {
+          enabled: true,
+          onlyInViewport: true,
+        },
+        autoplay: {
+          delay: 7000,
+        },
+        a11y: {
+          prevSlideMessage: PREVIOUS_SLIDE_LABEL,
+          nextSlideMessage: NEXT_SLIDE_LABEL,
+        },
+        navigation: {
+          nextEl: `.${NEXT_SLIDE_CLASS}`,
+          prevEl: `.${PREV_SLIDE_CLASS}`,
+        },
+        pagination: {
+          el: `.${PAGINATION_CLASS}`,
+          bulletClass: BULLET_CLASS,
+          bulletActiveClass: ACTIVE_BULLET_CLASS,
+          dynamicBullets: slideCount > 5 ? true : false,
+          dynamicMainBullets: 3,
+        },
+        effect: 'creative',
+        creativeEffect: {
+          prev: {
+            translate: ['-20%', 0, -1],
+          },
+          next: {
+            translate: ['100%', 0, 1],
+          },
+        },
+      });
 
-      try {
-        const swiper = new Swiper(slider, {
-          modules: [Navigation, Pagination, Autoplay, Keyboard, EffectCreative],
-          loop: true,
-          speed: prefersReduceMotion ? 0 : 800,
-          spaceBetween: 0,
-          slidesPerView: 1,
-          grabCursor: false,
-          slideClass: SLIDE_CLASS,
-          slideActiveClass: ACTIVE_SLIDE_CLASS,
-          lazy: true,
-          keyboard: {
-            enabled: true,
-            onlyInViewport: true,
-          },
-          autoplay: {
-            delay: 7000,
-          },
-          a11y: {
-            prevSlideMessage: PREVIOUS_SLIDE_LABEL,
-            nextSlideMessage: NEXT_SLIDE_LABEL,
-          },
-          navigation: {
-            nextEl: `.${NEXT_SLIDE_CLASS}`,
-            prevEl: `.${PREV_SLIDE_CLASS}`,
-          },
-          pagination: {
-            el: `.${PAGINATION_CLASS}`,
-            bulletClass: BULLET_CLASS,
-            bulletActiveClass: ACTIVE_BULLET_CLASS,
-            dynamicBullets: slideCount > 5 ? true : false,
-            dynamicMainBullets: 3,
-          },
-          effect: 'creative',
-          creativeEffect: {
-            prev: {
-              translate: prevEffect,
-            },
-            next: {
-              translate: nextEffect,
-            },
-          },
+      if (swiper) {
+        swiper.autoplay?.stop();
+
+        const handlePlayPause = () => {
+          playpause.addEventListener('click', () => {
+            const { running, start, stop } = swiper.autoplay;
+
+            if (!running) {
+              start();
+              playpause.innerHTML = PAUSE_ICON_HTML;
+              playpause.setAttribute('aria-label', PAUSE_SLIDER_LABEL);
+            }
+            if (running) {
+              stop();
+              playpause.innerHTML = PLAY_ICON_HTML;
+              playpause.setAttribute('aria-label', PLAY_SLIDER_LABEL);
+            }
+          });
+        };
+
+        const handleReducedMotion = () => {
+          swiper.slides?.forEach((slide) => slide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS));
+        };
+
+        const handleImageOrientation = () => {
+          Array.from(portraitImages)?.forEach((image) => {
+            const imageSrc = image.querySelector('img').src;
+            image.style.setProperty('--denhaag-fullwidth-slider-background-image-url', `url("${imageSrc}")`);
+          });
+        };
+
+        const handleActiveSlideAnimation = () => {
+          const activeSlide = getOneElement(ACTIVE_SLIDE_CLASS);
+          if (!prefersReduceMotion) activeSlide.classList.remove(REMOVE_SLIDE_ANIMATION_CLASS);
+        };
+
+        const reverseActiveSlideAnimation = () => {
+          swiper.slides.forEach((slide) => slide.classList.remove(REVERSE_DIRECTION_CLASS));
+          const activeSlideIndex = swiper.activeIndex;
+          const activeSlide = swiper.slides[activeSlideIndex];
+          const slideJustLeft = swiper.slides[activeSlideIndex + 1];
+          activeSlide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS);
+          slideJustLeft.classList.add(REVERSE_DIRECTION_CLASS);
+        };
+
+        const a11yToggleAllSlidesVisible = () => {
+          swiper.slides.forEach((slide) => (slide.style.visibility = 'visible'));
+        };
+
+        const a11yUpdateActiveSlideVisibility = () => {
+          swiper.slides.forEach((slide) => (slide.style.visibility = 'hidden'));
+          swiper.visibleSlides.forEach((slide) => (slide.style.visibility = 'visible'));
+        };
+
+        const a11yUpdateActiveSlideTabIndex = () => {
+          const slideTitles = getElements(SLIDE_TITLE_CLASS);
+          if (slideTitles)
+            slideTitles.forEach((el) => {
+              el.tabIndex = -1;
+              const href = el.querySelector('a');
+              if (href) href.tabIndex = -1;
+            });
+
+          // Add tabindex to current active slide
+          const activeSlideIndex = swiper.realIndex;
+          const activeSlide = swiper.slides[activeSlideIndex];
+          const activeSlideHref = activeSlide.querySelector(`.${SLIDE_TITLE_CLASS}`)?.querySelector('a');
+          if (activeSlideHref) activeSlideHref.tabIndex = 0;
+        };
+
+        const a11yUpdateSlideCountScreenReader = () => {
+          const string = `Currently showing slide ${swiper.realIndex + 1} of ${slideCount}`;
+          if (paginationEl) paginationEl.setAttribute('aria-label', string);
+          if (a11YSliderIndicator) {
+            a11YSliderIndicator.textContent = string;
+            setTimeout(() => {
+              a11YSliderIndicator.textContent = '';
+            }, 1000);
+          }
+        };
+
+        // On init, remove slide animation from active slide (in view on load)
+        const activeSlide = swiper.slides[swiper.activeIndex];
+        activeSlide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS);
+
+        if (slideCount < 2) {
+          if (playpause) playpause.classList.add(`${PLAYPAUSE_CLASS}--hide`);
+          if (swiperTitle) swiperTitle.style['margin-block-end'] = 0;
+        }
+
+        if (slideCount > 5 && paginationEl) {
+          paginationEl.classList.add(PAGINATION_CLASS_DYNAMIC);
+        }
+
+        if (playpause) handlePlayPause();
+        if (prefersReduceMotion) handleReducedMotion();
+        if (portraitImages) handleImageOrientation();
+
+        // Reverse the slide animation when clicking previous slide
+        swiper.on('slidePrevTransitionStart', () => {
+          reverseActiveSlideAnimation();
         });
 
-        if (swiper) {
-          swiper.autoplay?.stop();
-          const playpause = swiper.el.querySelector(`.${PLAYPAUSE_CLASS}`);
-          const swiperTitle = slider.querySelector(`.${SWIPER_TITLE_CLASS}`);
+        // If going back to forwards navigation, reset animation
+        swiper.on('slideNextTransitionStart', () => {
+          handleActiveSlideAnimation();
+        });
 
-          if (slideCount < 2) {
-            if (playpause) playpause.classList.add(`${PLAYPAUSE_CLASS}--hide`);
-            if (swiperTitle) swiperTitle.style['margin-block-end'] = 0;
-          }
+        swiper.on('slideChangeTransitionStart', () => {
+          a11yToggleAllSlidesVisible();
+          a11yUpdateActiveSlideTabIndex();
+          a11yUpdateSlideCountScreenReader();
+        });
 
-          if (slideCount > 5) {
-            const paginationEl = slider.querySelector(`.${PAGINATION_CLASS}`);
-            paginationEl.classList.add(PAGINATION_CLASS_DYNAMIC);
-          }
+        swiper.on('slideChangeTransitionEnd', () => {
+          a11yUpdateActiveSlideVisibility();
+        });
 
-          swiper.slides?.forEach((slide) => {
-            if (prefersReduceMotion) slide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS);
-            const portraitImage = slide.querySelector(`.${PORTRAIT_IMAGE_CLASS}`);
-            if (portraitImage) {
-              const imageSrc = portraitImage.querySelector('img').src;
-              portraitImage.style.setProperty('--denhaag-fullwidth-slider-background-image-url', `url("${imageSrc}")`);
-            }
-          });
-
-          if (playpause) {
-            playpause.addEventListener('click', () => {
-              const { running, start, stop } = swiper.autoplay;
-
-              if (!running) {
-                start();
-                playpause.innerHTML = PAUSE_ICON_HTML;
-                playpause.setAttribute('aria-label', PAUSE_SLIDER_LABEL);
-              }
-              if (running) {
-                stop();
-                playpause.innerHTML = PLAY_ICON_HTML;
-                playpause.setAttribute('aria-label', PLAY_SLIDER_LABEL);
-              }
-            });
-          }
-
-          // On init, remove slide animation from active slide (in view on load)
-          const activeSlide = swiper.slides[swiper.activeIndex];
-          activeSlide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS);
-
-          // Reverse the slide animation when clicking previous slide
-          swiper.on('slidePrevTransitionStart', () => {
-            swiper.slides.forEach((slide) => slide.classList.remove(REVERSE_DIRECTION_CLASS));
-
-            const activeSlideIndex = swiper.activeIndex;
-            const activeSlide = swiper.slides[activeSlideIndex];
-            const slideJustLeft = swiper.slides[activeSlideIndex + 1];
-            activeSlide.classList.add(REMOVE_SLIDE_ANIMATION_CLASS);
-            slideJustLeft.classList.add(REVERSE_DIRECTION_CLASS);
-          });
-
-          // If going back to forwards navigation, reset animation
-          swiper.on('slideNextTransitionStart', () => {
-            const activeSlide = swiper.slidesEl.querySelector(`.${ACTIVE_SLIDE_CLASS}`);
-            if (!prefersReduceMotion) activeSlide.classList.remove(REMOVE_SLIDE_ANIMATION_CLASS);
-          });
-
-          swiper.on('slideChangeTransitionStart', () => {
-            // Remove tabindex from all inactive slides
-            const slideTitles = document.querySelectorAll(`.${SLIDE_TITLE_CLASS}`);
-
-            if (slideTitles)
-              slideTitles.forEach((el) => {
-                const href = el.querySelector('a');
-                el.tabIndex = -1;
-                if (href) href.tabIndex = -1;
-              });
-
-            // Add tabindex to current active slide
-            const activeSlideIndex = swiper.activeIndex;
-            const activeSlide = swiper.slides[activeSlideIndex];
-            const activeSlideHref = activeSlide.querySelector(`.${SLIDE_TITLE_CLASS}`)?.querySelector('a');
-            if (activeSlideHref) {
-              activeSlideHref.tabIndex = 0;
-            }
-          });
-        }
-      } catch (error) {
-        console.log(error);
+        // On init
+        a11yUpdateActiveSlideVisibility();
+        a11yUpdateActiveSlideTabIndex();
       }
     });
   }
--- components/InlineCarousel/src/InlineCarousel.js
+++ components/InlineCarousel/src/InlineCarousel.js
@@ -15,81 +15,124 @@ const BULLET_CLASS = 'denhaag-inline-carousel__pagination-bullet';
 const ACTIVE_BULLET_CLASS = 'denhaag-inline-carousel__pagination-bullet-active';
 const DOWNLOAD_BUTTON_CLASS = 'denhaag-inline-carousel__content-download';
 const PORTRAIT_IMAGE_CLASS = 'denhaag-inline-carousel__slide-image--portrait';
-const PREVIOUS_SLIDE_LABEL = 'Previous slide';
-const NEXT_SLIDE_LABEL = 'Next slide';
+const PREVIOUS_SLIDE_LABEL = 'Previous image';
+const NEXT_SLIDE_LABEL = 'Next image';
+const A11Y_SLIDER_INDICATOR_CLASS = 'denhaag-inline-carousel__a11y-slide-indicator';
 
 const InlineCarousel = () => {
   const carousels = document.querySelectorAll(`.${SWIPER_CLASS}`);
   if (carousels) {
     carousels.forEach((carousel) => {
-      const slideCount = carousel.querySelectorAll(`.${SLIDE_CLASS}`)?.length;
+      // Selectors
+      const getElements = (inputClass) => carousel.querySelectorAll(`.${inputClass}`);
+      const allSlides = getElements(SLIDE_CLASS);
+      const slideCount = allSlides?.length;
+      const a11YSliderIndicator = carousel.querySelector(`.${A11Y_SLIDER_INDICATOR_CLASS}`);
+      const downloadButtons = getElements(DOWNLOAD_BUTTON_CLASS);
+      const portraitImages = getElements(PORTRAIT_IMAGE_CLASS);
+      const paginationEl = carousel.querySelector(`.${PAGINATION_CLASS}`);
 
+      // Check portrait images
+      if (portraitImages)
+        portraitImages.forEach((image) => {
+          const imageSrc = image.querySelector('img').src;
+          image.style.setProperty('--denhaag-inline-carousel-background-image-url', `url("${imageSrc}")`);
+        });
+
+      // Check user preferences before initializing swiper
       let prefersReduceMotion = false;
-
       if ('matchMedia' in window) {
         const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
         if (mediaQuery.matches) prefersReduceMotion = true;
       }
 
-      // Only for storybook
       if (carousel.classList.contains('denhaag-inline-carousel--prefers-reduced-motion')) {
         prefersReduceMotion = true;
       }
 
-      // Wrapping this in a try catch statement allows us to read errors easier in storybook
-      try {
-        const swiper = new Swiper(carousel, {
-          modules: [Navigation, Pagination],
-          loop: true,
-          speed: prefersReduceMotion ? 0 : 800,
-          spaceBetween: 10,
-          a11y: {
-            prevSlideMessage: PREVIOUS_SLIDE_LABEL,
-            nextSlideMessage: NEXT_SLIDE_LABEL,
-          },
-          navigation: {
-            nextEl: `.${NEXT_SLIDE_CLASS}`,
-            prevEl: `.${PREV_SLIDE_CLASS}`,
-          },
-          pagination: {
-            el: `.${PAGINATION_CLASS}`,
-            bulletClass: BULLET_CLASS,
-            bulletActiveClass: ACTIVE_BULLET_CLASS,
-            dynamicBullets: slideCount > 5 ? true : false,
-            dynamicMainBullets: 3,
-          },
+      // Init swiper
+      const swiper = new Swiper(carousel, {
+        modules: [Navigation, Pagination],
+        loop: true,
+        lazy: true,
+        speed: prefersReduceMotion ? 0 : 800,
+        spaceBetween: 10,
+        a11y: {
+          prevSlideMessage: PREVIOUS_SLIDE_LABEL,
+          nextSlideMessage: NEXT_SLIDE_LABEL,
+        },
+        navigation: {
+          nextEl: `.${NEXT_SLIDE_CLASS}`,
+          prevEl: `.${PREV_SLIDE_CLASS}`,
+        },
+        pagination: {
+          el: `.${PAGINATION_CLASS}`,
+          bulletClass: BULLET_CLASS,
+          bulletActiveClass: ACTIVE_BULLET_CLASS,
+          dynamicBullets: slideCount > 5 ? true : false,
+          dynamicMainBullets: 3,
+        },
+      });
+
+      if (swiper) {
+        if (slideCount > 5 && paginationEl) {
+          paginationEl.classList.add(PAGINATION_CLASS_DYNAMIC);
+        }
+
+        const a11yHideDownloadBtns = () => {
+          if (downloadButtons)
+            downloadButtons.forEach((button) => {
+              button.tabIndex = -1;
+              button.setAttribute('aria-hidden', 'true');
+            });
+        };
+
+        const a11yShowActiveDownloadBtn = () => {
+          const activeSlide = swiper.slides.find((slide) => slide.classList.contains('swiper-slide-active'));
+          const activeSlideDownloadBtn = activeSlide.querySelector(`.${DOWNLOAD_BUTTON_CLASS}`);
+          if (activeSlideDownloadBtn) {
+            activeSlideDownloadBtn.tabIndex = 0;
+            activeSlideDownloadBtn.setAttribute('aria-hidden', 'false');
+          }
+        };
+
+        const a11yHideSlideVisibility = () => {
+          swiper.slides.forEach((slide) => (slide.style.visibility = 'visible'));
+        };
+
+        const a11yShowActiveSlideVisible = () => {
+          swiper.slides.forEach((slide) => (slide.style.visibility = 'hidden'));
+          const active = swiper.slides.find((slide) => slide.classList.contains('swiper-slide-active'));
+          active.style.visibility = 'visible';
+        };
+
+        const a11yUpdateSlideCountScreenReader = () => {
+          const string = `Currently showing image ${swiper.realIndex + 1} of ${slideCount}.`;
+          if (paginationEl) paginationEl.setAttribute('aria-label', string);
+          if (a11YSliderIndicator) {
+            a11YSliderIndicator.textContent = string;
+            setTimeout(() => {
+              a11YSliderIndicator.textContent = '';
+            }, 1000);
+          }
+        };
+
+        // Swiper events
+        swiper.on('slideChangeTransitionStart', () => {
+          a11yHideSlideVisibility();
+          a11yHideDownloadBtns();
+          a11yUpdateSlideCountScreenReader();
         });
 
-        if (swiper) {
-          if (slideCount > 5) {
-            const paginationEl = carousel.querySelector(`.${PAGINATION_CLASS}`);
-            paginationEl.classList.add(PAGINATION_CLASS_DYNAMIC);
-          }
+        swiper.on('slideChangeTransitionEnd', () => {
+          a11yShowActiveSlideVisible();
+          a11yShowActiveDownloadBtn();
+        });
 
-          swiper.on('slideChangeTransitionStart', () => {
-            // Remove tabindex from all inactive slides
-            const downloadButtons = carousel.querySelectorAll(`.${DOWNLOAD_BUTTON_CLASS}`);
-            if (downloadButtons) downloadButtons.forEach((button) => (button.tabIndex = -1));
-
-            // Add tabindex to current active slide
-            const activeSlideIndex = swiper.activeIndex;
-            const activeSlide = swiper.slides[activeSlideIndex];
-            const activeSlideDownloadBtn = activeSlide.querySelector(`.${DOWNLOAD_BUTTON_CLASS}`);
-            if (activeSlideDownloadBtn) {
-              activeSlideDownloadBtn.tabIndex = 0;
-            }
-          });
-
-          swiper.slides?.forEach((slide) => {
-            const portraitImage = slide.querySelector(`.${PORTRAIT_IMAGE_CLASS}`);
-            if (portraitImage) {
-              const imageSrc = portraitImage.querySelector('img').src;
-              portraitImage.style.setProperty('--denhaag-inline-carousel-background-image-url', `url("${imageSrc}")`);
-            }
-          });
-        }
-      } catch (error) {
-        console.log(error);
+        // On init
+        a11yShowActiveSlideVisible();
+        a11yHideDownloadBtns();
+        a11yShowActiveDownloadBtn();
       }
     });
   }
