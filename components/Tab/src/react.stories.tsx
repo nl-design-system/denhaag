e51531ba7927771f016b81140ac964c308221199
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-08-25 11:14:44 +0200

    fix: cleanup tab react stories: disabled will be added later

--- components/Tab/src/react.stories.tsx
+++ components/Tab/src/react.stories.tsx
@@ -26,24 +26,7 @@ const Template: Story<TabProps> = () => {
     { label: 'Yet another tab', panelContent: 'Item Three' },
   ];
 
-  return (
-    <Tabs tabData={tabData} />
-    /*     <Fragment>
-      <Tabs
-        value={value}
-        onChange={(_event: React.ChangeEvent<unknown>, newValue: number) => {
-          setValue(newValue);
-        }}
-      >
-        <Tab {...args} value={0} />
-        <Tab label="Another tab" value={1} />
-        <Tab label="Yet another tab" value={2} />
-      </Tabs>
-      <TabPanel value="0">Item One</TabPanel>
-      <TabPanel value="1">Item Two</TabPanel>
-      <TabPanel value="2">Item Three</TabPanel>
-    </Fragment> */
-  );
+  return <Tabs tabData={tabData} />;
 };
 
 // language=JS
@@ -78,43 +61,6 @@ Default.parameters = {
   docs: {
     source: {
       code: defaultCode,
-    },
-  },
-};
-
-// language=JS
-const disabledCode = `
-const [value, setValue] = React.useState(0);
-
-return (
-  <TabContext value={value.toString()}>
-    <Tabs value={value} onChange={(event: React.ChangeEvent<unknown>, newValue: number) => {
-      setValue(newValue);
-    }}>
-      <Tab label="A disabled tab" disabled value={0}/>
-      <Tab label="Another tab" value={1}/>
-      <Tab label="Yet another tab" value={2}/>
-    </Tabs>
-    <TabPanel value="1">Item One</TabPanel>
-    <TabPanel value="2">Item Two</TabPanel>
-    <TabPanel value="3">Item Three</TabPanel>
-  </TabContext>
-);
-`;
-
-/**
- * Implementation of Tab which is disabled
- */
-export const Disabled = Template.bind({});
-Disabled.args = {
-  label: 'A disabled tab',
-  disabled: true,
-};
-
-Disabled.parameters = {
-  docs: {
-    source: {
-      code: disabledCode,
     },
   },
 };
