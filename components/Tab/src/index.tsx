3a1c450bc967f3069744ac22099624d8c8b16ed4
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-09-19 13:55:48 +0200

    fix: change in design related to focus/hover state + cleanup some code

--- components/Tab/src/index.tsx
+++ components/Tab/src/index.tsx
@@ -9,8 +9,11 @@ import { TabText } from './TabText';
 import { v4 as uuidv4 } from 'uuid';
 
 export * from './Tab';
-export * from './TabPanel';
+export * from './TabIndicator';
 export * from './TabList';
+export * from './TabPanel';
+export * from './TabsContainer';
+export * from './TabText';
 
 export interface TabsProps extends HTMLAttributes<HTMLDivElement> {
   tabData: Array<{ label: string; panelContent: React.ReactNode }>;
