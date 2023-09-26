6d493280f8d1666c8af9c8745e18c7ed228bc59c
Robbert Broersma <git@robbertbroersma.nl>
2023-09-26 19:13:26 +0200

    feat: add Storybook 7 package

---
+++ packages/storybook/config/manager.ts
@@ -0,0 +1,9 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
+import theme from './theme';
+
+addons.setConfig({
+  theme: create({
+    ...theme,
+  }),
+});
