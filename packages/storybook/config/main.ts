3d2d5a1af6c68f6a2eea205149e34085b8ab22a0
Robbert Broersma <git@robbertbroersma.nl>
2023-09-26 19:13:26 +0200

    feat: add Storybook 7 package

---
+++ packages/storybook/config/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+const config: StorybookConfig = {
+  core: {
+    disableTelemetry: true,
+  },
+  stories: [
+    '../../../.storybook/stories/**/*.stories.mdx',
+    '../../../components/*/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../components/*/src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|mdx|ts|tsx)',
+  ],
+  features: {
+    buildStoriesJson: true,
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    '@storybook/preset-scss',
+    '@etchteam/storybook-addon-status/register',
+    '@whitespace/storybook-addon-html',
+    '@storybook/addon-links',
+    'storybook-addon-pseudo-states',
+    'storybook-addon-themes',
+  ],
+  staticDirs: ['../../../.storybook/stories/assets/'],
+  docs: {
+    autodocs: 'tag',
+  },
+};
+
+export default config;
---
+++ packages/storybook/config/preview.tsx
@@ -0,0 +1,147 @@
+/* eslint-env node */
+import type { Preview } from '@storybook/react';
+import type { StoryContext } from '@storybook/types';
+import clsx from 'clsx';
+import prettierBabel from 'prettier/parser-babel';
+import prettier from 'prettier/standalone';
+import React, { ReactElement } from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+
+import '@utrecht/component-library-css/dist/index.css';
+import '@gemeente-denhaag/design-tokens-components/dist/theme/index.css';
+
+const preview: Preview = {
+  decorators: [
+    // Enable `utrecht-document` component as backdrop
+    // Enable `utrecht-theme` to configure the design tokens
+    // Ensure old html templates will be rendered as react component
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (Story: any, storyContext: any): ReactElement => {
+      // Hack to make current args for a story available in the transformSource of the docs addon
+      storyContext.parameters['args'] = storyContext.args;
+
+      return (
+        <div
+          className={clsx('utrecht-document', 'utrecht-document--surface', 'denhaag-theme', {
+            'utrecht-reduced-motion': ['1', 'true'].includes(process.env['STORYBOOK_REDUCED_MOTION'] ?? ''),
+          })}
+        >
+          {Story()}
+        </div>
+      );
+    },
+    // (Story) => (
+    //   <ThemeDecorator config={parameters.themes}>
+    //     <StylesProvider>{<Story />}</StylesProvider>
+    //   </ThemeDecorator>
+    // ),
+  ],
+  parameters: {
+    // Make the "Docs" tab the default, instead of the "Canvas" tab
+    viewMode: 'docs',
+
+    previewTabs: {
+      // Move the "Docs" tab to the front
+      'storybook/docs/panel': { index: -1, title: 'Documentation' },
+      canvas: { title: 'Demo' },
+    },
+
+    options: {
+      storySort: {
+        order: [
+          'Utrecht',
+          [
+            'README',
+            'Wat is het Utrecht Design System?',
+            'Toestemming voor gebruik',
+            'Design Tokens',
+            ['Design Tokens'],
+            'Gebruikersonderzoek',
+            ['Gebruikersonderzoek'],
+          ],
+          'NL Design System',
+          'Onderzoek',
+          'CSS Component',
+          ['README'],
+          'HTML Component',
+          ['README'],
+          'Web Component',
+          ['README'],
+          'Angular Web Component',
+          ['README'],
+          'React Component',
+          ['README'],
+          'React Web Component',
+          ['README'],
+          'Vue.js Web Component',
+          ['README'],
+          'Form.io Component',
+          ['README'],
+        ],
+      },
+    },
+
+    // Configure @storybook/addon-docs
+    docs: {
+      // Show code by default.
+      // Stories without concise code snippets can hide the code at Story level.
+      source: {
+        state: 'open',
+      },
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      transformSource: (src: string, storyContext: StoryContext<any>): string => {
+        // Ensure valid HTML in the Preview source
+        const render =
+          typeof storyContext.component === 'function'
+            ? storyContext.component
+            : typeof storyContext.component?.render === 'function'
+            ? storyContext.component?.render
+            : null;
+
+        if (render) {
+          return prettier.format(ReactDOMServer.renderToStaticMarkup(render(storyContext.args)), {
+            parser: 'babel',
+            plugins: [prettierBabel],
+          });
+        }
+        return src;
+      },
+    },
+    status: {
+      statuses: {
+        PRODUCTION: {
+          background: '#006400',
+          color: '#ffffff',
+          description:
+            'Used in production in a variety of situations, well tested, stable APIs, mostly patches and minor releases.',
+        },
+        BETA: {
+          background: '#cca300',
+          color: '#ffffff',
+          description:
+            'Used in production in a specific situation, evolving APIs based on feedback, breaking changes are still likely.',
+        },
+        ALPHA: {
+          background: '#cc0000',
+          color: '#ffffff',
+          description:
+            'Used in prototypes and in projects that are still in development, breaking changes occur frequently and are not communicated.',
+        },
+        'WORK IN PROGRESS': {
+          background: '#cc0000',
+          color: '#ffffff',
+          description:
+            'Do not use in production. Does not follow semantic versioning and any published packages are for internal use only.',
+        },
+        DEPRECATED: {
+          background: '#9e1616',
+          color: '#ffffff',
+          description:
+            'This component is no longer supported. If used in production, please replace with the recommended alternative.',
+        },
+      },
+    },
+  },
+};
+
+export default preview;
