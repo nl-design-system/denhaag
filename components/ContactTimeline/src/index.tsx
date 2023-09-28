370d73d9dbc39de8303388fb9c172baac74d9d68
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-09-28 13:08:24 +0200

    WIP Contact timeline mobile

--- components/ContactTimeline/src/index.tsx
+++ components/ContactTimeline/src/index.tsx
@@ -1,6 +1,8 @@
 import React, { Key, OlHTMLAttributes, ReactNode } from 'react';
 import { StepMarker, StepMarkerConnector } from '@gemeente-denhaag/step-marker';
-import { Step, StepHeader, StepHeading, StepBody, StepMeta } from '@gemeente-denhaag/process-steps';
+import { Step, StepHeader, StepHeading, StepBody } from '@gemeente-denhaag/process-steps';
+import { format, differenceInDays } from 'date-fns';
+import { nl } from 'date-fns/locale';
 import clsx from 'clsx';
 import './index.scss';
 
@@ -9,20 +11,23 @@ export interface ContactTimelineItemProps {
   title: ReactNode;
   marker?: ReactNode;
   date: ReactNode;
+  isoDate: string;
+  todayLabel: string;
   channel: ReactNode;
   nextItem?: boolean;
 }
 
 export interface ContactTimelineProps {
   items: ContactTimelineItemProps[];
+  todayLabel: string;
 }
 
-export const ContactTimelineMobile: React.FC<ContactTimelineProps> = ({ items }: ContactTimelineProps) => {
+export const ContactTimelineMobile: React.FC<ContactTimelineProps> = ({ items, todayLabel }: ContactTimelineProps) => {
   return (
     <ContactTimelineList mobile className="denhaag-process-steps">
       {items.map((item, index) => {
         const nextItem = index < items.length - 1;
-        return <ContactTimelineListItem {...item} key={item.id} nextItem={nextItem} />;
+        return <ContactTimelineListItem {...item} key={item.id} nextItem={nextItem} todayLabel={todayLabel} />;
       })}
     </ContactTimelineList>
   );
@@ -43,7 +48,33 @@ export const ContactTimelineList: React.FC<ContactTimelineListProps> = ({
   </ol>
 );
 
-const ContactTimelineListItem: React.FC<ContactTimelineItemProps> = ({ title, date, channel, nextItem, ...props }) => {
+interface Props {
+  dateTime: string;
+  todayLabel: string;
+  now?: string;
+  locale?: Locale;
+}
+
+// Partially copied version of the Action date. We should make a generic component.
+export const ContactTimelineDate = ({ dateTime, now = new Date().toISOString(), locale = nl, todayLabel }: Props) => {
+  const date = new Date(dateTime);
+  const daysDifference = differenceInDays(date, new Date(now));
+
+  if (daysDifference === 0) {
+    return <>{todayLabel}</>;
+  }
+
+  return <>{format(date, 'd-M-yyyy', { locale: locale })}</>;
+};
+
+const ContactTimelineListItem: React.FC<ContactTimelineItemProps> = ({
+  title,
+  date,
+  isoDate,
+  todayLabel,
+  channel,
+  nextItem,
+}) => {
   return (
     <Step appearance="default">
       <StepHeader className="denhaag-contact-timeline__step-header">
@@ -51,7 +82,13 @@ const ContactTimelineListItem: React.FC<ContactTimelineItemProps> = ({ title, da
         <div>
           <StepHeading>{title}</StepHeading>
           <div className="denhaag-contact-timeline__meta">
-            <span className="denhaag-contact-timeline__meta-date">{date}</span>
+            {date ? (
+              date
+            ) : (
+              <time dateTime={isoDate} className="denhaag-contact-timeline__meta-date">
+                <ContactTimelineDate dateTime={isoDate} todayLabel={todayLabel} />
+              </time>
+            )}
             <span className="denhaag-contact-timeline__meta-marker" />
             <span className="denhaag-contact-timeline__meta-channel">{channel}</span>
           </div>
