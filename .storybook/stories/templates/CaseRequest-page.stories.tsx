6e8e0575c28be26c5629fb983f3857ab69489e69
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2023-09-29 10:31:27 +0200

    docs: add case request template

---
+++ .storybook/stories/templates/CaseRequest-page.stories.tsx
@@ -0,0 +1,114 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading2 } from '../../../components/Typography/src';
+import { Sidenav, SidenavList, SidenavItem, SidenavLink } from '../../../components/Sidenav/src';
+import { BadgeCounter } from '../../../components/BadgeCounter/src';
+import { GridIcon, InboxIcon, ArchiveIcon } from '../../../components/Icons/src';
+import { Divider } from '../../../components/Divider/src';
+import { Link } from '../../../components/Link/src';
+import Alert from '../../../components/Alert/src';
+import React from 'react';
+import { Page, PageHeader, PageFooter } from '../../../components/Page/src';
+import { HeaderLogic, HeaderLogicProps } from '../../../components/Header/src';
+import Footer from '../../../components/Footer/src';
+import ResponsiveContent from '../../../components/ResponsiveContent/src';
+import { AuthenticationCardGroup } from '../../../components/CardGroup/src/story-template';
+import {
+  headerProps,
+  footerLegalData,
+  footerSocialData,
+  newsletterData,
+  contactData,
+  copyright,
+} from '../templates/util';
+
+import './CaseRequest-page.scss';
+
+const meta = {
+  title: 'Templates/Case Request Page',
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+  },
+} as Meta<typeof Page>;
+
+export default meta;
+
+const loginHeaderProps: HeaderLogicProps = {
+  ...headerProps,
+  breadcrumbs: {
+    navigationPath: [{ label: 'Home', href: 'https://denhaag.nl/' }, { label: 'MijnDenHaag' }],
+  },
+  userprofileMenu: {
+    label: 'Welkom Anne Klap',
+    authorisedLoginLabel: 'Ingelogd namens Jason Verploeg',
+    navigationGroups: [
+      {
+        label: 'MijnDenHaag',
+        navigation: [
+          { label: 'Overzicht', href: '' },
+          { label: 'Lopende zaken', href: '' },
+          { label: "Thema's", href: '' },
+          { label: 'Mijn account', href: '' },
+        ],
+      },
+    ],
+  },
+};
+
+// Login page
+
+type Story = StoryObj<typeof meta>;
+
+export const Request: Story = {
+  render: (args) => (
+    <Page {...args}>
+      <PageHeader>
+        <HeaderLogic {...loginHeaderProps} />
+      </PageHeader>
+      <ResponsiveContent className="denhaag-page-content">
+        <Sidenav>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink current href="/#">
+                <InboxIcon />
+                Label
+              </SidenavLink>
+            </SidenavItem>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <ArchiveIcon />
+                Mijn lopende zaken
+                <BadgeCounter>2</BadgeCounter>
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <ArchiveIcon />
+                Belastingzaken
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+          <SidenavList>
+            <SidenavItem>
+              <SidenavLink href="/#">
+                <ArchiveIcon />
+                Lopende zaken
+              </SidenavLink>
+            </SidenavItem>
+          </SidenavList>
+        </Sidenav>
+        <Heading2>Aanvraag subsidie geluidsisolatie</Heading2>
+      </ResponsiveContent>
+      <PageFooter>
+        <Footer
+          newsletterData={newsletterData}
+          contactData={contactData}
+          legalData={footerLegalData}
+          copyrightLabel={copyright}
+          socialData={footerSocialData}
+        />
+      </PageFooter>
+    </Page>
+  ),
+};
