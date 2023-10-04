986a45ea54b8bdd8e29a573621556e798fad1486
Robbert Broersma <git@robbertbroersma.nl>
2023-10-04 14:12:56 +0200

    build: add SCSS and custom CSS to denhaag-theme

---
+++ proprietary/Components/rollup.config.mjs
@@ -0,0 +1,19 @@
+import postcss from 'rollup-plugin-postcss';
+import discardDuplicates from 'postcss-discard-duplicates';
+
+export default {
+  input: 'src/theme/index.scss',
+  output: {
+    dir: './dist/theme/',
+    sourcemap: false,
+    format: 'esm',
+    compact: true,
+  },
+  plugins: [
+    postcss({
+      extensions: ['.css', '.scss'],
+      plugins: [discardDuplicates()],
+      extract: true,
+    }),
+  ],
+};
