063a95b6e654c444ed20b5429341bbad0a7bdb80
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-10-06 10:47:34 +0200

    WIP first working contact timeline desktop before refactor

--- components/ContactTimeline/src/index.tsx
+++ components/ContactTimeline/src/index.tsx
@@ -20,14 +20,26 @@ export interface ContactTimelineItemProps {
 export interface ContactTimelineProps {
   items: ContactTimelineItemProps[];
   todayLabel: string;
+  mobile?: boolean;
 }
 
-export const ContactTimelineMobile: React.FC<ContactTimelineProps> = ({ items, todayLabel }: ContactTimelineProps) => {
+export const ContactTimelineMobile: React.FC<ContactTimelineProps> = ({
+  items,
+  todayLabel,
+  mobile = false,
+}: ContactTimelineProps) => {
+  let ListItemComponent: React.FC<ContactTimelineItemProps>;
+  if (mobile) {
+    ListItemComponent = ContactTimelineListItem;
+  } else {
+    ListItemComponent = ContactTimelineListItemDesktop;
+  }
+
   return (
-    <ContactTimelineList mobile className="denhaag-process-steps">
+    <ContactTimelineList mobile={mobile} className="denhaag-process-steps">
       {items.map((item, index) => {
         const nextItem = index < items.length - 1;
-        return <ContactTimelineListItem {...item} key={item.id} nextItem={nextItem} todayLabel={todayLabel} />;
+        return <ListItemComponent {...item} key={item.id} nextItem={nextItem} todayLabel={todayLabel} />;
       })}
     </ContactTimelineList>
   );
@@ -92,6 +104,35 @@ const ContactTimelineListItem: React.FC<ContactTimelineItemProps> = ({
             <span className="denhaag-contact-timeline__meta-marker" />
             <span className="denhaag-contact-timeline__meta-channel">{channel}</span>
           </div>
+        </div>
+      </StepHeader>
+      <StepBody>{nextItem && <StepMarkerConnector appearance="default" from="main" to="main" />}</StepBody>
+    </Step>
+  );
+};
+
+const ContactTimelineListItemDesktop: React.FC<ContactTimelineItemProps> = ({
+  title,
+  date,
+  isoDate,
+  todayLabel,
+  channel,
+  nextItem,
+}) => {
+  return (
+    <Step appearance="default">
+      <StepHeader className="denhaag-contact-timeline__step-header">
+        <div className="denhaag-contact-timeline__step-header__date">
+          <time dateTime={isoDate} className="denhaag-contact-timeline__meta-date">
+            <ContactTimelineDate dateTime={isoDate} todayLabel={todayLabel} />
+          </time>
+        </div>
+        <StepMarker appearance="default" nested />
+        <div className="denhaag-contact-timeline__step-header__channel">
+          <span className="denhaag-contact-timeline__meta-channel">{channel}</span>
+        </div>
+        <div>
+          <StepHeading>{title}</StepHeading>
         </div>
       </StepHeader>
       <StepBody>{nextItem && <StepMarkerConnector appearance="default" from="main" to="main" />}</StepBody>
