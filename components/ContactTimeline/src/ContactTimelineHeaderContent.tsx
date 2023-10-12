51df51782aa666a60a495d69efd68143d882c9e9
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-10-12 16:26:42 +0200

    WIP contact timeline: transformed to one responsive component + collapsible

--- components/ContactTimeline/src/ContactTimelineHeaderContent.tsx
+++ components/ContactTimeline/src/ContactTimelineHeaderContent.tsx
@@ -4,5 +4,5 @@ import React, { HTMLAttributes } from 'react';
 interface ContactTimelineHeaderContentProps extends HTMLAttributes<HTMLSpanElement> {}
 
 export const ContactTimelineHeaderContent: React.FC<ContactTimelineHeaderContentProps> = ({ children }) => {
-  return <div className={clsx('denhaag-contact-timeline__step-header-content')}>{children}</div>;
+  return <div className={clsx('denhaag-contact-timeline__step-header__content')}>{children}</div>;
 };
