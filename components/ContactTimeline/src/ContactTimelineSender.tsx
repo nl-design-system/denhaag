95839e4b65bb81096816c380ca1d67fc85509273
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-10-20 15:56:48 +0200

    WIP refactor contact timeline

--- components/ContactTimeline/src/ContactTimelineSender.tsx
+++
@@ -1,8 +0,0 @@
-import clsx from 'clsx';
-import React, { HTMLAttributes } from 'react';
-
-interface ContactTimelineSenderProps extends HTMLAttributes<HTMLSpanElement> {}
-
-export const ContactTimelineSender: React.FC<ContactTimelineSenderProps> = ({ children }) => {
-  return <div className={clsx('denhaag-contact-timeline__sender')}>{children}</div>;
-};
