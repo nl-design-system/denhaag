6b8359084392d4aafe6003ae9127b2d76b3ef6f4
Borai van Uden <info@borai.nl>
2023-11-06 16:20:12 +0100

    feat: removed DotIndicator

--- components/BadgeCounter/src/index.tsx
+++ components/BadgeCounter/src/index.tsx
@@ -1,5 +1,4 @@
 import React, { HTMLAttributes } from 'react';
-import { DotIndicator } from '@gemeente-denhaag/dotindicator';
 import './index.scss';
 import clsx from 'clsx';
 
@@ -14,9 +13,7 @@ export const BadgeCounter: React.FC<BadgeCounterProps> = (props: BadgeCounterPro
   const rootClassNames = clsx('denhaag-badge-counter', props.className);
   return (
     <div id={props.id} className={rootClassNames}>
-      <DotIndicator overlap={'rectangle'}>
-        <div className={'denhaag-badge-counter__counter'}>{props.children}</div>
-      </DotIndicator>
+      <div className={'denhaag-badge-counter__counter'}>{props.children}</div>
     </div>
   );
 };
