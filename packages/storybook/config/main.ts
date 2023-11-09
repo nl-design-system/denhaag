c5416c2b80d3044d79fafd84b0639784837e9a50
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2023-11-09 15:52:47 +0100

    fix: svg errors

--- packages/storybook/config/main.ts
+++ packages/storybook/config/main.ts
@@ -5,7 +5,7 @@ const config: StorybookConfig = {
     disableTelemetry: true,
   },
   stories: [
-    '../../../.storybook/stories/**/*.stories.mdx',
+    '../../../.storybook/stories/**/*.stories.@(js|jsx|ts|tsx|mdx)',
     '../../../components/*/src/**/*.stories.@(js|jsx|ts|tsx)',
     '../../../components/*/src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|mdx|ts|tsx)',
@@ -31,6 +31,19 @@ const config: StorybookConfig = {
   staticDirs: ['../../../.storybook/stories/assets/'],
   docs: {
     autodocs: 'tag',
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  webpackFinal: async (config: any) => {
+    const rules = config.module.rules;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const fileLoaderRule = rules.find((rule: any) => rule.test && rule.test.test('.svg'));
+    fileLoaderRule.exclude = /\.svg$/;
+
+    rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
   },
 };
 
