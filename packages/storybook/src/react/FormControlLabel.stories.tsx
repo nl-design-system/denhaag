59bffe2c59b79a7c2cc56de73d2bc2797d28cd2d
Borai van Uden <info@borai.nl>
2023-11-23 11:55:33 +0100

    feat: form control label tsx

---
+++ packages/storybook/src/react/FormControlLabel.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { FormControlLabel } from '@gemeente-denhaag/formcontrollabel';
+import Checkbox from '@gemeente-denhaag/checkbox';
+import { Radio as RadioComponent } from '@gemeente-denhaag/radio';
+import readme from '../../../../components/FormControlLabel/README.md';
+
+const exampleArgs = {
+  label: 'Label',
+  input: <Checkbox />,
+};
+
+const meta = {
+  id: 'react-input-form-control-label',
+  title: 'React/Input/Form Control Label',
+  component: FormControlLabel,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof FormControlLabel>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Radio: Story = {
+  args: { ...Default.args, input: <RadioComponent /> },
+};
