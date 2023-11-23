fdbdfbd2d26fb0bac82995553fb62978092d08a0
Borai van Uden <info@borai.nl>
2023-11-23 11:38:45 +0100

    feat: dot indicator tsx

---
+++ packages/storybook/src/react/DotIndicator.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@gemeente-denhaag/button';
+import { DotIndicator } from '@gemeente-denhaag/dotindicator';
+import readme from '../../../../components/DotIndicator/README.md';
+
+const exampleArgs = {
+  overlap: 'rectangle',
+};
+
+const meta = {
+  id: 'react-data-display-dot-indicator',
+  title: 'React/Data Display/Dot indicator',
+  component: DotIndicator,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof DotIndicator>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <DotIndicator {...args}>
+      <Button>Read messages</Button>
+    </DotIndicator>
+  ),
+};
