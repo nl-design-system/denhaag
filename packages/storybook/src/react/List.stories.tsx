70e2e076badc9707e769d57e5ed88a8b21cd0421
Borai van Uden <info@borai.nl>
2023-11-27 09:50:20 +0100

    feat: list tsx

---
+++ packages/storybook/src/react/List.stories.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { HouseIcon, TrashIcon } from '@gemeente-denhaag/icons';
+import { List, ListItem, ListSubheader } from '@gemeente-denhaag/list';
+import readme from '../../../../components/List/README.md';
+
+const exampleArgs = {
+  children: (
+    <>
+      <ListItem primaryText="ListItem" />
+      <ListItem primaryText="ListItem" />
+      <ListItem primaryText="ListItem" />
+    </>
+  ),
+};
+
+const meta = {
+  id: 'react-data-display-list',
+  title: 'React/Data Display/List',
+  component: List,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof List>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Subheader: Story = {
+  args: {
+    subheader: <ListSubheader>Subheader</ListSubheader>,
+  },
+};
+
+export const Listitem: Story = {
+  args: {
+    children: (
+      <>
+        <ListItem primaryText="ListItem" />
+        <ListItem
+          primaryText="Single-line item"
+          actionType="action"
+          leftIcon={<HouseIcon color="inherit"></HouseIcon>}
+          rightIcon={<TrashIcon />}
+          onClick={action('Secondary action click')}
+        />
+        <ListItem
+          primaryText="Single-line item"
+          secondaryText="Secondary text"
+          leftIcon={<HouseIcon color="inherit" />}
+          actionType="nav"
+          onClick={action('ListItem Nav click')}
+        />
+      </>
+    ),
+  },
+};
