71aa25e8fed74a8cdb81d114d94eaf0cee2dcc0d
Borai van Uden <info@borai.nl>
2023-11-27 09:22:36 +0100

    feat: link tsx

---
+++ packages/storybook/src/react/Link.stories.tsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { ArrowLeftIcon, ArrowRightIcon } from '@gemeente-denhaag/icons';
+import { Paragraph } from '@gemeente-denhaag/typography';
+import { Link } from '@gemeente-denhaag/link';
+
+import readme from '../../../../components/Link/README.md';
+
+const exampleArgs = {
+  href: '#',
+};
+
+const meta = {
+  id: 'react-navigation-link',
+  title: 'React/Navigation/Link',
+  component: Link,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof Link>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <Paragraph>
+      It is possible to put <Link {...args}>a link</Link> in a Paragraph element.
+    </Paragraph>
+  ),
+};
+
+export const disabled: Story = {
+  render: (args) => (
+    <Paragraph>
+      It is possible to put{' '}
+      <Link disabled {...args}>
+        a link
+      </Link>{' '}
+      in a Paragraph element.
+    </Paragraph>
+  ),
+};
+
+export const iconStart: Story = {
+  render: (args) => (
+    <Paragraph>
+      It is possible to put{' '}
+      <Link disabled icon={<ArrowLeftIcon />} iconAlign="start" {...args}>
+        a link
+      </Link>{' '}
+      in a Paragraph element.
+    </Paragraph>
+  ),
+};
+
+export const iconEnd: Story = {
+  render: (args) => (
+    <Paragraph>
+      It is possible to put{' '}
+      <Link disabled icon={<ArrowRightIcon />} {...args}>
+        a link
+      </Link>{' '}
+      in a Paragraph element.
+    </Paragraph>
+  ),
+};
