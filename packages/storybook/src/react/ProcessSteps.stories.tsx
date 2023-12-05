ae243b3a581070c101d2a297d53fc2bc173d93c5
Borai van Uden <info@borai.nl>
2023-12-05 09:59:36 +0100

    feat: process steps tsx

---
+++ packages/storybook/src/react/ProcessSteps.stories.tsx
@@ -0,0 +1,272 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Status } from '@gemeente-denhaag/process-steps';
+
+import readme from '../../../../components/ProcessSteps/README.md';
+
+const exampleArgs = {
+  steps: [
+    {
+      id: 'deelname',
+      marker: 1,
+      title: 'Deelname aan geluidsonderzoek',
+      status: 'checked',
+      steps: [{ title: 'Aanmelding ontvangen', status: 'checked' }],
+    },
+    {
+      id: 'onderzoek',
+      marker: 2,
+      title: 'Onderzoek naar geluidsoverlast',
+      status: 'current',
+      steps: [
+        { title: 'Afspraak meten geluidsoverlast gemaakt', status: 'checked' },
+        { title: 'Geluidsoverlast gemeten' },
+        { title: 'Onderzoek resultaten verwerkt' },
+      ],
+    },
+    { id: 'uitvoeren', marker: 3, title: 'Uitvoeren van maatregelen' },
+    { id: 'klaar', marker: 4, title: 'Maatregelen zijn uitgevoerd' },
+  ],
+};
+
+const meta = {
+  id: 'react-process-steps-status',
+  title: 'React/Process Steps/Status',
+  component: Status,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    chromatic: { viewports: [1768, 1280, 768, 360] },
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof Status>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Collapsible: Story = {
+  args: { ...Default.args, collapsible: true },
+};
+
+export const CollapsibleWithExpandedStep: Story = {
+  args: {
+    ...Default.args,
+    collapsible: true,
+    expandedSteps: ['deelname'],
+    steps: [
+      {
+        id: 'deelname',
+        marker: 1,
+        title: 'Deelname aan geluidsonderzoek',
+        status: 'checked',
+        steps: [{ title: 'Aanmelding ontvangen', status: 'checked' }],
+      },
+      {
+        id: 'onderzoek',
+        marker: 2,
+        title: 'Onderzoek naar geluidsoverlast',
+        status: 'current',
+        steps: [
+          { title: 'Afspraak meten geluidsoverlast gemaakt', status: 'checked' },
+          { title: 'Geluidsoverlast gemeten' },
+          { title: 'Onderzoek resultaten verwerkt' },
+        ],
+      },
+      { id: 'uitvoeren', marker: 3, title: 'Uitvoeren van maatregelen' },
+      { id: 'klaar', marker: 4, title: 'Maatregelen zijn uitgevoerd' },
+    ],
+  },
+};
+
+export const WithoutSubsteps: Story = {
+  args: {
+    ...Default.args,
+    collapsible: true,
+    steps: [
+      {
+        id: 'getuigen-wijzigen',
+        marker: 1,
+        title: 'Getuigen wijzigen of meer getuigen uitnodigen',
+        status: 'checked',
+      },
+      {
+        id: 'getuigen-definitief',
+        marker: 2,
+        title: 'Getuigen zijn definitief en bevestigingen van getuigen ontvangen',
+        status: 'current',
+      },
+      { id: 'extras', marker: 3, title: 'Eventuele extras bestellen' },
+      { id: 'trouwen', marker: 4, title: 'Trouwen' },
+    ],
+  },
+};
+
+export const WithMetadata: Story = {
+  args: {
+    ...Default.args,
+    collapsible: true,
+    steps: [
+      {
+        id: 'getuigen-wijzigen',
+        marker: 1,
+        title: 'Getuigen wijzigen of meer getuigen uitnodigen',
+        status: 'checked',
+        meta: (
+          <a className="utrecht-link" href="https://example.com">
+            Getuigen wijzigen of meer getuigen uitnodigen
+          </a>
+        ),
+        date: 'Tussen vandaag en 24 februari 2023',
+      },
+      {
+        id: 'getuigen-definitief',
+        marker: 2,
+        title: 'Getuigen zijn definitief en bevestigingen van getuigen ontvangen',
+        status: 'current',
+        steps: [{ title: 'Getuigen aangemeld', status: 'checked' }],
+        meta: 'Getuigen zijn definitief en bevestigingen van getuigen ontvangen',
+        date: 'Regel het vóór 24 februari 2023',
+      },
+      {
+        id: 'trouwen',
+        marker: 4,
+        title: 'Trouwen',
+        meta: (
+          <p className="utrecht-paragraph">
+            Jullie gaan trouwen in het{' '}
+            <a className="utrecht-link" href="https://example.com">
+              Stadhuis Spui
+            </a>
+          </p>
+        ),
+        date: 'Op 1 april 2023',
+      },
+    ],
+  },
+};
+
+export const WithExpandedMetadata: Story = {
+  args: {
+    expandedSteps: ['ontwerpen'],
+    collapsible: true,
+    steps: [
+      {
+        id: 'verkennen',
+        marker: 1,
+        title: 'Verkennen',
+        status: 'checked',
+        date: 'juli 2023',
+      },
+      {
+        id: 'ontwerpen',
+        marker: 2,
+        title: 'Ontwerpen',
+        status: 'current',
+        meta: 'Eindelijk tijd om concreet te worden! we schetsen de plannen en leggen het voor om samen tot een beslissing te komen.',
+        date: 'aug - sept 2023',
+        steps: [
+          { title: 'Voorlopige schetsontwerp afgerond en gepresenteerd', status: 'checked' },
+          { title: 'Stemronde nummer 01', status: 'checked' },
+          { title: 'Verwerken feedback it stemronde', status: 'current' },
+          { title: 'We organiseren een informatieavond voor bewoners' },
+        ],
+      },
+      {
+        id: 'uitvoeren',
+        marker: 4,
+        title: 'Uitvoeren',
+        date: 'okt - nov 2023',
+      },
+      {
+        id: 'afronden',
+        marker: 4,
+        title: 'Afronden',
+        date: 'dec 2023',
+      },
+    ],
+  },
+};
+
+export const WithWarning: Story = {
+  args: {
+    steps: [
+      {
+        id: 'deelname',
+        marker: 1,
+        title: 'Deelname aan geluidsonderzoek',
+        status: 'checked',
+        steps: [{ title: 'Aanmelding ontvangen', status: 'checked' }],
+      },
+      {
+        id: 'onderzoek',
+        marker: 2,
+        title: 'Onderzoek naar geluidsoverlast',
+        status: 'warning',
+        steps: [
+          { title: 'Afspraak meten geluidsoverlast gemaakt', status: 'warning' },
+          { title: 'Geluidsoverlast gemeten', status: 'warning' },
+          { title: 'Onderzoek resultaten verwerkt' },
+        ],
+      },
+      { id: 'uitvoeren', marker: 3, title: 'Uitvoeren van maatregelen' },
+      { id: 'klaar', marker: 4, title: 'Maatregelen zijn uitgevoerd' },
+    ],
+  },
+};
+
+export const WithError: Story = {
+  args: {
+    steps: [
+      {
+        id: 'deelname',
+        marker: 1,
+        title: 'Deelname aan geluidsonderzoek',
+        status: 'checked',
+        steps: [{ title: 'Aanmelding ontvangen', status: 'checked' }],
+      },
+      {
+        id: 'onderzoek',
+        marker: 2,
+        title: 'Onderzoek naar geluidsoverlast',
+        status: 'error',
+        meta: 'Er is geen afspraak gemaakt. De aanvraag is stopgezet. Wilt u toch graag in aanmerking komen voor subsidie? Neem dan contact op met de gemeente via 14070.',
+        steps: [{ title: 'Afspraak meten geluidsoverlast gemaakt', status: 'error' }],
+      },
+      { id: 'uitvoeren', marker: 3, title: 'Uitvoeren van maatregelen' },
+      { id: 'klaar', marker: 4, title: 'Maatregelen zijn uitgevoerd' },
+    ],
+  },
+};
+
+export const NonCollapsibleStep: Story = {
+  args: {
+    expandedSteps: ['onderzoek'],
+    collapsible: true,
+    steps: [
+      {
+        id: 'deelname',
+        marker: 1,
+        title: 'Deelname aan geluidsonderzoek',
+        status: 'checked',
+        steps: [{ title: 'Aanmelding ontvangen', status: 'checked' }],
+      },
+      {
+        id: 'onderzoek',
+        marker: 2,
+        title: 'Onderzoek naar geluidsoverlast',
+        status: 'error',
+        meta: 'Er is geen afspraak gemaakt. De aanvraag is stopgezet. Wilt u toch graag in aanmerking komen voor subsidie? Neem dan contact op met de gemeente via 14070.',
+        steps: [{ title: 'Afspraak meten geluidsoverlast gemaakt', status: 'error' }],
+        collapsible: false,
+      },
+      { id: 'uitvoeren', marker: 3, title: 'Uitvoeren van maatregelen' },
+      { id: 'klaar', marker: 4, title: 'Maatregelen zijn uitgevoerd' },
+    ],
+  },
+};
