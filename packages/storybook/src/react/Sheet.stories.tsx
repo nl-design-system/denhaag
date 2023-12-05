c84c7fe8482462887b2d7bebd4a72068eb4ca090
Borai van Uden <info@borai.nl>
2023-12-05 10:22:45 +0100

    feat: sheet tsx

--- packages/storybook/src/react/Sheet.stories.tsx
+++ packages/storybook/src/react/Sheet.stories.tsx
@@ -1,29 +1,37 @@
 import React from 'react';
-import { Meta, Story } from '@storybook/react';
+import { Meta, StoryObj } from '@storybook/react';
 import { CloseIcon } from '@gemeente-denhaag/icons';
 import { IconButton } from '@gemeente-denhaag/iconbutton';
 import { Sheet, SheetContainer, SheetDialog, SheetProps } from '@gemeente-denhaag/sheet';
 import { ResponsiveContent } from '@gemeente-denhaag/responsive-content';
-import pkg from '../../../../components/Sheet/package.json';
 
-export default {
+import readme from '../../../../components/Sheet/README.md';
+
+const exampleArgs: SheetProps = {
+  children: 'Sheet',
+};
+
+const meta = {
+  id: 'react-page-elements-sheet',
   title: 'React/Page Elements/Sheet',
+  component: Sheet,
+  args: exampleArgs,
+  tags: ['autodocs'],
   parameters: {
-    componentSubtitle: `${pkg.name} - ${pkg.version}`,
+    chromatic: { viewports: [1768, 1280, 768, 360] },
     docs: {
-      source: {
-        type: 'code',
+      description: {
+        component: readme,
       },
     },
-    status: {
-      type: 'WORK IN PROGRESS',
-    },
   },
-  component: Sheet,
-} as Meta;
+} as Meta<typeof Sheet>;
 
-const Template: Story<SheetProps> = () => {
-  return (
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => (
     <Sheet>
       <ResponsiveContent>
         <IconButton className="denhaag-sheet__close-button">
@@ -32,17 +40,13 @@ const Template: Story<SheetProps> = () => {
         <SheetContainer>Content</SheetContainer>
       </ResponsiveContent>
     </Sheet>
-  );
+  ),
 };
 
-const DialogTemplate: Story<SheetProps> = () => {
-  return (
+export const MobileDialog: Story = {
+  render: () => (
     <SheetDialog open>
       <ResponsiveContent>Content</ResponsiveContent>
     </SheetDialog>
-  );
+  ),
 };
-
-export const Default: Story<SheetProps> = Template.bind({});
-
-export const MobileDialog: Story<SheetProps> = DialogTemplate.bind({});
