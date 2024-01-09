3687453c8b28609f99ea816646b8cfabe9883d4f
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-01-09 17:17:05 +0100

    feat: replaced old textfield with utrecht textbox

--- packages/storybook/src/react/TextField.stories.tsx
+++ packages/storybook/src/react/TextField.stories.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { TextField } from '@gemeente-denhaag/textfield';
-import { CallIcon } from '@gemeente-denhaag/icons';
+import Textbox from '@gemeente-denhaag/textfield';
 import readme from '../../../../components/TextField/README.md';
 
-const exampleArgs = {
-  placeholder: 'Placeholder',
-};
-
 const meta = {
-  id: 'react-input-textfield',
-  title: 'React/Input/Textfield',
-  component: TextField,
-  args: exampleArgs,
+  title: 'React/Input/TextBox',
+  id: 'react-textbox',
+  component: Textbox,
   tags: ['autodocs'],
+  args: { placeholder: 'Placeholder' },
   parameters: {
     docs: {
       description: {
@@ -21,21 +15,16 @@ const meta = {
       },
     },
   },
-} as Meta<typeof TextField>;
+} as Meta<typeof Textbox>;
 
 export default meta;
-type Story = StoryObj<typeof meta>;
 
+type Story = StoryObj<typeof meta>;
 export const Default: Story = {};
 
 export const Disabled: Story = {
   args: { ...Default.args, disabled: true },
 };
-
 export const Invalid: Story = {
   args: { ...Default.args, invalid: true },
-};
-
-export const WithIcon: Story = {
-  args: { ...Default.args, placeholder: 'Phone number', icon: <CallIcon /> },
 };
