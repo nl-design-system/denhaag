9e23a15bc118f5f967abd6b8fc562ae68d8df715
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-01-09 17:15:48 +0100

    feat: added formfieldset

---
+++ components/FormFieldSet/src/index.tsx
@@ -0,0 +1,5 @@
+import { Fieldset, FieldsetProps, FieldsetLegend, FieldsetLegendProps } from '@utrecht/component-library-react';
+import './index.scss';
+
+export { FieldsetProps, FieldsetLegend, FieldsetLegendProps };
+export default Fieldset;
