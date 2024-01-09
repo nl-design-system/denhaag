d28f772c66d05013dfd9ddd12887c3d7fb3161e0
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-01-09 17:17:05 +0100

    feat: replaced old textfield with utrecht textbox

--- components/TextField/src/index.tsx
+++ components/TextField/src/index.tsx
@@ -1,52 +1,5 @@
-import React, { ForwardedRef, forwardRef, InputHTMLAttributes } from 'react';
+import { Textbox, TextboxProps } from '@utrecht/component-library-react';
 import './index.scss';
-import clsx from 'clsx';
-import { CircleWarningIcon, SvgIconProps } from '@gemeente-denhaag/icons';
 
-export interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
-  /**
-   * Whether the input should be disabled
-   */
-  disabled?: boolean;
-
-  /**
-   * Whether the input field is in a error state.
-   */
-  invalid?: boolean;
-
-  /**
-   * Icon shown at the end of the input field
-   */
-  icon?: React.ReactElement<SvgIconProps>;
-
-  /**
-   * Icon to replace the default invalid icon
-   */
-  invalidIcon?: React.ReactElement<SvgIconProps>;
-}
-
-export const TextField: React.FC<TextFieldProps> = forwardRef(function DenHaagTextField(
-  props: TextFieldProps,
-  ref: ForwardedRef<HTMLInputElement>,
-) {
-  const inputClasses = clsx('denhaag-textfield__input', {
-    'denhaag-textfield__input--invalid': props.invalid,
-    'denhaag-textfield__input--disabled': props.disabled,
-  });
-
-  const invalidIcon = React.cloneElement(props.invalidIcon || <CircleWarningIcon />, {
-    className: 'denhaag-textfield__invalid-icon',
-  });
-
-  return (
-    <div className={'denhaag-textfield'}>
-      <input className={inputClasses} ref={ref} aria-invalid={props.invalid || undefined} {...props} />
-      <div className={'denhaag-textfield__icons'}>
-        {props.invalid && invalidIcon}
-        {props.icon}
-      </div>
-    </div>
-  );
-});
-
-export default TextField;
+export { TextboxProps };
+export default Textbox;
--- components/Textbox/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import { Textbox, TextboxProps } from '@utrecht/component-library-react';
-import './index.scss';
-
-export { TextboxProps };
-export default Textbox;
