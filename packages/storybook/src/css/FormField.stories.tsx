c1d074ba87b1231695bf2b3e90991aee0398de3f
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-01-12 11:12:06 +0100

    fix: use different uuid for each formfield

--- packages/storybook/src/css/FormField.stories.tsx
+++ packages/storybook/src/css/FormField.stories.tsx
@@ -124,21 +124,21 @@ export const CheckboxGroup: Story = {
 
 export const CheckboxGroupWithDescription: Story = {
   render: () => (
-    <Fieldset aria-describedby="d7218515-bf54-45c1-ba57-a72af5d7fd42-description">
+    <Fieldset aria-describedby="c7218515-bf54-45c1-ba57-a72af5d7fd42-description">
       <FieldsetLegend>Hoe wil je dat wij contact opnemen?</FieldsetLegend>
       <FormFieldDescription
         className="utrecht-form-field-description--distanced"
-        id="d7218515-bf54-45c1-ba57-a72af5d7fd42-description"
+        id="c7218515-bf54-45c1-ba57-a72af5d7fd42-description"
       >
         Kies één of meerdere opties.
       </FormFieldDescription>
       <FormField type="checkbox">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
-          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e9" type="checkbox">
+          <FormLabel htmlFor="3e054ee1-799e-4608-9055-19d3fc0b88e9" type="checkbox">
             <Checkbox
-              aria-describedby="4e054ee1-799e-4608-9055-19d3fc0b88e9-description"
+              aria-describedby="3e054ee1-799e-4608-9055-19d3fc0b88e9-description"
               className="utrecht-form-field__input"
-              id="4e054ee1-799e-4608-9055-19d3fc0b88e9"
+              id="3e054ee1-799e-4608-9055-19d3fc0b88e9"
               name="phone"
               value="true"
             />
@@ -148,11 +148,11 @@ export const CheckboxGroupWithDescription: Story = {
       </FormField>
       <FormField type="checkbox">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
-          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b820" type="checkbox">
+          <FormLabel htmlFor="138b2692-2460-4bad-93b5-b6def8c3b820" type="checkbox">
             <Checkbox
-              aria-describedby="238b2692-2460-4bad-93b5-b6def8c3b820-description"
+              aria-describedby="138b2692-2460-4bad-93b5-b6def8c3b820-description"
               className="utrecht-form-field__input"
-              id="238b2692-2460-4bad-93b5-b6def8c3b820"
+              id="138b2692-2460-4bad-93b5-b6def8c3b820"
               name="email"
               value="true"
             />
@@ -162,11 +162,11 @@ export const CheckboxGroupWithDescription: Story = {
       </FormField>
       <FormField type="checkbox">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
-          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="checkbox">
+          <FormLabel htmlFor="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="checkbox">
             <Checkbox
-              aria-describedby="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description"
+              aria-describedby="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description"
               className="utrecht-form-field__input"
-              id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2"
+              id="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2"
               name="letter"
               value="true"
             />
@@ -186,11 +186,11 @@ export const RadioButtonGroup: Story = {
       </FieldsetLegend>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e9" type="radio">
+          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e0" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="4e054ee1-799e-4608-9055-19d3fc0b88e9"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="4e054ee1-799e-4608-9055-19d3fc0b88e0"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2"
               value="phone"
             />
             Telefoon
@@ -199,11 +199,11 @@ export const RadioButtonGroup: Story = {
       </FormField>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b820" type="radio">
+          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b821" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="238b2692-2460-4bad-93b5-b6def8c3b820"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="238b2692-2460-4bad-93b5-b6def8c3b821"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2"
               value="email"
             />
             E-mail
@@ -212,11 +212,11 @@ export const RadioButtonGroup: Story = {
       </FormField>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="radio">
+          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2"
               value="letter"
             />
             Brief
@@ -239,11 +239,11 @@ export const RadioButtonGroupWithDescription: Story = {
       </FormFieldDescription>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e9" type="radio">
+          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e8" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="4e054ee1-799e-4608-9055-19d3fc0b88e9"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="4e054ee1-799e-4608-9055-19d3fc0b88e8"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4"
               value="phone"
             />
             Telefoon
@@ -252,11 +252,11 @@ export const RadioButtonGroupWithDescription: Story = {
       </FormField>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b820" type="radio">
+          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b829" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="238b2692-2460-4bad-93b5-b6def8c3b820"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="238b2692-2460-4bad-93b5-b6def8c3b829"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4"
               value="email"
             />
             E-mail
@@ -265,11 +265,11 @@ export const RadioButtonGroupWithDescription: Story = {
       </FormField>
       <FormField type="radio">
         <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
-          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="radio">
+          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5" type="radio">
             <RadioButton
               className="utrecht-form-field__input"
-              id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2"
-              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b3"
+              id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5"
+              name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4"
               value="letter"
             />
             Brief
