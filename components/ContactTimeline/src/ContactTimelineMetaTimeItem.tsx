871271fa29f995fda749e9189c637763184ab3dd
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-01-15 16:19:31 +0100

    fix: contact timeline meta item text overflow

--- components/ContactTimeline/src/ContactTimelineMetaTimeItem.tsx
+++ components/ContactTimeline/src/ContactTimelineMetaTimeItem.tsx
@@ -3,9 +3,20 @@ import React, { TimeHTMLAttributes } from 'react';
 
 interface ContactTimelineMetaTimeItemProps extends TimeHTMLAttributes<HTMLTimeElement> {}
 
-export const ContactTimelineMetaTimeItem: React.FC<ContactTimelineMetaTimeItemProps> = ({ children, ...props }) => {
+export const ContactTimelineMetaTimeItem: React.FC<ContactTimelineMetaTimeItemProps> = ({
+  children,
+  className,
+  ...props
+}) => {
   return (
-    <time className={clsx('denhaag-contact-timeline__meta-item')} {...props}>
+    <time
+      className={clsx(
+        'denhaag-contact-timeline__meta-item',
+        'denhaag-contact-timeline__meta-item--time-item',
+        className,
+      )}
+      {...props}
+    >
       {children}
     </time>
   );
