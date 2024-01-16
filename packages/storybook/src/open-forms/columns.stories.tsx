9d68e1b0962b5660cba831f5e508aca0d633e773
Bryan de Jong <bryandejong99@gmail.com>
2024-01-16 23:49:02 +0000

    feat: add open-forms-cosign and -columns stories

---
+++ packages/storybook/src/open-forms/columns.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { SingleFormioComponent } from './story-util';
+
+const meta: Meta = {
+  id: 'open-forms-columns',
+  title: 'Open Formulieren/Columns',
+  component: SingleFormioComponent,
+  args: {
+    key: 'cosign',
+    type: 'columns',
+    extraComponentProperties: {
+      columns: [
+        {
+          size: 3,
+          sizeMobile: 6,
+          components: [
+            {
+              key: 'postcode',
+              type: 'postcode',
+              label: 'Postcode',
+            },
+          ],
+        },
+        {
+          size: 3,
+          sizeMobile: 6,
+          components: [
+            {
+              key: 'number',
+              type: 'textfield',
+              label: 'Huisnummer',
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof SingleFormioComponent>;
+
+export const Default: Story = {
+  render: (args) => <SingleFormioComponent {...args} />,
+};
