1a51529d64e5d1d93eb179c4f33cf51f6818a40c
Borai van Uden <info@borai.nl>
2024-02-27 11:33:34 +0100

    feat: add anchor to table cell/header

--- components/Table/src/TableCell.tsx
+++ components/Table/src/TableCell.tsx
@@ -1,14 +1,27 @@
 import React, { TdHTMLAttributes } from 'react';
 import clsx from 'clsx';
 
-export type TableCellProps = TdHTMLAttributes<HTMLTableDataCellElement>;
+export interface TableCellProps extends TdHTMLAttributes<HTMLTableCellElement> {
+  href?: string;
+}
 
 /**
  * TableCell component based on the <td> element.
  * Defines a cell of a table that contains data. It participates in the table model.
  */
-export const TableCell: React.FC<TableCellProps> = ({ className, ...props }: TableCellProps) => {
-  const rootClassNames = clsx('denhaag-table__cell', className);
+export const TableCell: React.FC<TableCellProps> = ({ className, href, ...props }: TableCellProps) => {
+  const rootClassNames = clsx('denhaag-table__cell', className, {
+    'denhaag-table__cell--link': href,
+  });
+
+  if (href) {
+    return (
+      <td className={rootClassNames} {...props}>
+        <a href={href}>{props.children}</a>
+      </td>
+    );
+  }
+
   return (
     <td className={rootClassNames} {...props}>
       {props.children}
