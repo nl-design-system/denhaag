09beac2e57dbbd060a833868d76dc935afb78b8e
Borai van Uden <info@borai.nl>
2024-03-07 11:23:25 +0100

    feat: convert css story to tsx

---
+++ packages/storybook/src/css/Sidenav.stories.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { BadgeCounter } from '@gemeente-denhaag/badge-counter';
+import { Sidenav, SidenavItem, SidenavLink, SidenavList } from '@gemeente-denhaag/sidenav';
+import { ArchiveIcon, CheckCircleIcon, GridIcon, InboxIcon, UserIcon } from '@gemeente-denhaag/icons';
+import readme from '../../../../components/Button/README.md';
+
+const exampleArgs = {
+  children: (
+    <>
+      <SidenavList>
+        <SidenavItem>
+          <SidenavLink href={'/#'} current>
+            <GridIcon />
+            Overzicht
+          </SidenavLink>
+        </SidenavItem>
+      </SidenavList>
+      <SidenavList>
+        <SidenavItem>
+          <SidenavLink href={'/#'}>
+            <CheckCircleIcon />
+            Mijn taken
+          </SidenavLink>
+        </SidenavItem>
+        <SidenavItem>
+          <SidenavLink href={'/#'}>
+            <InboxIcon />
+            Mijn berichten
+            <BadgeCounter>2</BadgeCounter>
+          </SidenavLink>
+        </SidenavItem>
+        <SidenavItem>
+          <SidenavLink href={'/#'}>
+            <ArchiveIcon />
+            Mijn lopende zaken
+          </SidenavLink>
+        </SidenavItem>
+      </SidenavList>
+      <SidenavList>
+        <SidenavItem>
+          <SidenavLink href={'/#'}>
+            <UserIcon />
+            Mijn gegevens
+          </SidenavLink>
+        </SidenavItem>
+      </SidenavList>
+    </>
+  ),
+};
+
+const meta = {
+  id: 'css-navigation-sidenav',
+  title: 'CSS/Navigation/Sidenav',
+  component: Sidenav,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof Sidenav>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
