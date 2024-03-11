8fe50da153a519b1e7908e6c77dce458947368ae
Borai van Uden <info@borai.nl>
2024-03-11 15:16:57 +0100

    Update tabs when data change (#1507)
    Wanneer een tab child geüpdatet wordt vind er op dit moment geen
    rerender plaats omdat de tabData opgeslagen wordt in de state van de het
    tab component. Door een useEffect toe te voegen als de tabData veranderd
    is dit nu opgelost.

--- packages/storybook/src/react/Tabs.stories.tsx
+++ packages/storybook/src/react/Tabs.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Tabs, TabsProps } from '@gemeente-denhaag/tab';
 import readme from '../../../../components/Tab/README.md';
+import React, { useEffect, useState } from 'react';
 
 const exampleArgs: TabsProps = {
   tabData: [
@@ -29,3 +30,27 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {};
+
+export const UpdateTabContent: Story = {
+  render: () => {
+    const [seconds, setSeconds] = useState(1);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setSeconds((seconds: number) => seconds + 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }, []);
+
+    return (
+      <Tabs
+        tabData={[
+          { label: 'A tab with timer', panelContent: <>Update timer: {seconds}</> },
+          { label: 'Another tab', panelContent: 'Item Two' },
+          { label: 'Yet another tab', panelContent: 'Item Three' },
+        ]}
+      />
+    );
+  },
+};
