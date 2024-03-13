d4976acd33fa45e12b4f07392e85dff280ee56a3
Borai van Uden <info@borai.nl>
2024-03-13 09:50:01 +0100

    Utrecht Button Link (#1509)
    Heb een externe link nodig die er uit ziet als een button voor de button
    in de taken. Weet niet helemaal of dit het juiste is. Misschien goed om
    hier nog even over te sparren.

---
+++ packages/storybook/src/react/ButtonLink.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta, StoryObj } from '@storybook/react';
+import readme from '../../../../components/ButtonLink/README.md';
+import { ButtonLink } from '@gemeente-denhaag/button-link';
+
+const exampleArgs = {
+  href: '#',
+  children: 'Button',
+};
+
+const meta = {
+  title: 'React/Input/Button Link',
+  id: 'react-input-button-link',
+  component: ButtonLink,
+  tags: ['autodocs'],
+  args: exampleArgs,
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof ButtonLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {};
+
+export const Secondary: Story = {
+  args: { ...Default.args, appearance: 'secondary-action-button' },
+};
