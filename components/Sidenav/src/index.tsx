8b30e415ff3c44f6b26ebe8149927b3dadeacd59
Marwaxhello <112861555+Marwaxhello@users.noreply.github.com>
2024-04-18 11:16:05 +0200

    fix: sidenav components export bug

--- components/Sidenav/src/index.tsx
+++ components/Sidenav/src/index.tsx
@@ -1,6 +1,11 @@
 import './index.scss';
 
-export * from './Sidenav';
-export * from './SidenavItem';
-export * from './SidenavList';
-export * from './SidenavLink';
+// export * from './Sidenav';
+// export * from './SidenavItem';
+// export * from './SidenavList';
+// export * from './SidenavLink';
+
+export { default as Sidenav } from './Sidenav';
+export { default as SidenavItem } from './SidenavItem';
+export { default as SidenavList } from './SidenavList';
+export { default as SidenavLink } from './SidenavLink';
