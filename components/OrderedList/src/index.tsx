0c4fe2509163ffd4446e5b9ca4abd366b03a673f
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-06-04 14:43:36 +0200

    feat: also use the utrecht ordered list

--- components/OrderedList/src/index.tsx
+++ components/OrderedList/src/index.tsx
@@ -1,1 +1,4 @@
+import { OrderedList, OrderedListProps, OrderedListItem, OrderedListItemProps } from '@utrecht/component-library-react';
 import './index.scss';
+
+export { OrderedList, OrderedListProps, OrderedListItem, OrderedListItemProps };
