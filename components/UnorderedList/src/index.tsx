dbb13e1d1c0bf744c8af47b647b84e30e9ccf9e2
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-06-04 14:11:43 +0200

    feat: replaced den haag unordered list with utrecht unordered list

--- components/UnorderedList/src/index.tsx
+++ components/UnorderedList/src/index.tsx
@@ -1,1 +1,20 @@
+import React from 'react';
+import {
+  UnorderedList as UtrechtUnorderedList,
+  UnorderedListProps as UtrechtUnorderedListProps,
+  UnorderedListItem,
+  UnorderedListItemProps,
+} from '@utrecht/component-library-react';
 import './index.scss';
+import clsx from 'clsx';
+
+interface UnorderedListProps extends UtrechtUnorderedListProps {
+  nested?: boolean;
+}
+
+const UnorderedList = ({ className, nested, ...props }: UnorderedListProps) => {
+  const classNames = clsx(className, nested ? 'denhaag-unordered-list--nested' : '');
+  return <UtrechtUnorderedList className={classNames} {...props} />;
+};
+
+export { UnorderedList, UnorderedListProps, UnorderedListItem, UnorderedListItemProps };
---
+++ packages/storybook/src/react/UnorderedList.stories.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import readme from '../../../../components/FormField/README.md';
+import { UnorderedList, UnorderedListItem } from '@gemeente-denhaag/unorderedlist';
+
+const meta = {
+  id: 'react-data-display-unordered-list',
+  title: 'React/Data Display/Unordered List',
+  component: UnorderedList,
+  args: {},
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof UnorderedList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    children: [
+      <UnorderedListItem key={1}>List item 1</UnorderedListItem>,
+      <UnorderedListItem key={2}>List item 2</UnorderedListItem>,
+      <UnorderedListItem key={3}>List item 3</UnorderedListItem>,
+    ],
+  },
+};
+
+export const Nested: Story = {
+  args: {
+    children: [
+      <UnorderedListItem key={1}>List item 1</UnorderedListItem>,
+      <UnorderedListItem key={2}>List item 2</UnorderedListItem>,
+      <UnorderedList nested key={3}>
+        <UnorderedListItem>List item 1</UnorderedListItem>
+        <UnorderedListItem>List item 2</UnorderedListItem>
+        <UnorderedListItem>List item 3</UnorderedListItem>
+      </UnorderedList>,
+      <UnorderedListItem key={4}>List item 3</UnorderedListItem>,
+    ],
+  },
+};
