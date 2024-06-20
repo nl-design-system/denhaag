c05a19b10cd5fe3368786093512f340950f58157
Borai van Uden <info@borai.nl>
2024-06-20 17:14:49 +0200

    feat: copy utrecht comp

---
+++ packages/storybook/src/react/StatusBadge.stories.tsx
@@ -0,0 +1,52 @@
+import { Meta, StoryObj } from '@storybook/react';
+import readme from '../../../../components/StatusBadge/README.md';
+import { StatusBadge } from '@utrecht/component-library-react';
+
+const exampleArgs = {
+  children: 'Label',
+  status: 'default',
+};
+
+const meta = {
+  id: 'react-data-display-status-badge',
+  title: 'React/Data Display/Status Badge',
+  component: StatusBadge,
+  args: exampleArgs,
+  tags: ['autodocs'],
+  parameters: {
+    docs: {
+      description: {
+        component: readme,
+      },
+    },
+  },
+} as Meta<typeof StatusBadge>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const Neutral: Story = {
+  args: {
+    status: 'neutral',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    status: 'success',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    status: 'error',
+  },
+};
+
+export const Warning: Story = {
+  args: {
+    status: 'warning',
+  },
+};
