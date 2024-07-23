32839e592fecb676b25b08b1838af27632717014
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-23 15:18:30 +0200

    chore: updated prettier, cleanup ncurc

--- .ncurc.minor.js
+++ .ncurc.minor.js
@@ -2,14 +2,6 @@ const patchConfig = require('./.ncurc.patch');
 
 module.exports = {
   ...patchConfig,
-  reject: [
-    ...patchConfig.reject,
-    'react',
-    'react-dom',
-    '@types/react',
-    '@types/react-dom',
-    'react-is',
-    'react-test-renderer',
-  ],
+  reject: [...patchConfig.reject],
   target: 'minor',
 };
