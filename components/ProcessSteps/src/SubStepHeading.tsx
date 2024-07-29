dd5a64a1528e3ff41b41c82800f76c70552227b3
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2024-07-29 11:42:42 +0200

    fix: use simpler type for substepheading
    Co-authored-by: Robbert <git@robbertbroersma.nl>

--- components/ProcessSteps/src/SubStepHeading.tsx
+++ components/ProcessSteps/src/SubStepHeading.tsx
@@ -1,6 +1,6 @@
 import React, { PropsWithChildren } from 'react';
 
-export const SubStepHeading = ({ children }: PropsWithChildren<NonNullable<unknown>>) => (
+export const SubStepHeading = ({ children }: PropsWithChildren) => (
   <p className={'denhaag-process-steps__sub-step-heading'}>{children}</p>
 );
 
