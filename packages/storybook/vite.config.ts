31820d77f8c8b3ed5cbf5b91056a3002301adaf7
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-29 15:48:45 +0200

    chore: cleanup dependency

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -1,10 +1,9 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { plugin as mdPlugin, Mode } from 'vite-plugin-markdown';
 
 export default defineConfig({
   assetsInclude: ['**/*.md'],
-  plugins: [react(), mdPlugin({ mode: [Mode.MARKDOWN] })],
+  plugins: [react()],
   build: {
     rollupOptions: {
       external: ['react', 'react-dom', 'lodash'],
