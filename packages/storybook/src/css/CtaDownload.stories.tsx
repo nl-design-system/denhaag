f0d2e3b0bd663a8140152969f4123f194ad9911b
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-30 09:39:29 +0200

    fix: remove export of helper function in story

--- packages/storybook/src/css/CtaDownload.stories.tsx
+++ packages/storybook/src/css/CtaDownload.stories.tsx
@@ -5,7 +5,7 @@ import '@gemeente-denhaag/ctadownload';
 import pkg from '../../../../components/CtaDownload/package.json';
 import readme from '../../../../components/CtaDownload/README.md?raw';
 
-export const fileImage = (h: number, w: number) =>
+const fileImage = (h: number, w: number) =>
   `https://images.unsplash.com/photo-1569235186275-626cb53b83ce?fit=crop&w=${w}&h=${h}`;
 
 const meta = {
