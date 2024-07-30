c38c301e881b41ba556d43b1b51561b8c4b7083e
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-07-30 12:37:14 +0200

    chore: cleanup

--- packages/storybook/vite.config.ts
+++ packages/storybook/vite.config.ts
@@ -2,7 +2,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  assetsInclude: ['**/*.md'],
   plugins: [react()],
   build: {
     rollupOptions: {
