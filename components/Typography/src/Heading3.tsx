b834bfdbeb1ae594f0768b4acea906ee69508412
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-08-27 16:49:40 +0200

    refactor: use the utrecht typography react components, instead of only the css classes

--- components/Typography/src/Heading3.tsx
+++ components/Typography/src/Heading3.tsx
@@ -1,13 +1,5 @@
-import React from 'react';
+import { Heading3, Heading3Props } from '@utrecht/component-library-react';
 import './heading.scss';
-import clsx from 'clsx';
 
-export interface Heading3Props {
-  className?: string;
-  children?: React.ReactNode;
-}
-
-export const Heading3: React.FC<Heading3Props> = (props: Heading3Props) => {
-  const rootClassNames = clsx('utrecht-heading-3', props.className);
-  return <h3 className={rootClassNames}>{props.children}</h3>;
-};
+export { Heading3, Heading3Props };
+export default Heading3;
