4d933c3a9e16c3e7d6ae05be04a31c6114e79d1b
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-08-27 13:42:20 +0200

    fix: remove style property, replaced with classname

--- components/FormField/src/index.tsx
+++ components/FormField/src/index.tsx
@@ -1,22 +1,5 @@
-import React, { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
-import {
-  FormField as UtrechtFormField,
-  FormFieldProps as UtrechtFormFieldProps,
-} from '@utrecht/component-library-react';
+import { FormField, FormFieldProps } from '@utrecht/component-library-react';
 import './index.scss';
-import clsx from 'clsx';
-
-interface FormFieldProps extends PropsWithChildren<UtrechtFormFieldProps> {
-  flex?: boolean;
-}
-
-const FormField = forwardRef(function FormField(
-  { className, flex, ...restProps }: FormFieldProps,
-  ref: ForwardedRef<HTMLDivElement>,
-) {
-  const classNames = clsx(className, { 'denhaag-form-field--flex': flex });
-  return <UtrechtFormField ref={ref} className={classNames} {...restProps} />;
-});
 
 export { FormField, FormFieldProps };
 export default FormField;
