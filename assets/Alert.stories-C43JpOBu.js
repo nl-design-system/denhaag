324b105b28b059676cacffb81e4ad2e4d7c36429
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2024-09-05 09:53:40 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@57eee127d6d43599fc68c41206a18b4eb7fa34e8 🚀

---
+++ assets/Alert.stories-C43JpOBu.js
@@ -0,0 +1,43 @@
+import{j as R}from"./jsx-runtime-BlAj40OV.js";import{a as i}from"./chunk-454WOBUV-CM0pFb8Z.js";import{A as _}from"./index-BUOVzzQ0.js";import{M as H}from"./index-DZxrVG7T.js";import{r as O}from"./README-BteT48cG.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";import"./v4-CQkTLCs1.js";const q={title:"Title",text:"Here comes text. This text provides additional details and actionable steps the user can take.",variant:"info"},P={id:"react-data-display-alert",title:"React/Data Display/Alert",component:_,args:q,tags:["autodocs"],parameters:{docs:{description:{component:O}}}},r={},a={args:{...r.args,variant:"success"}},e={args:{...r.args,variant:"warning"}},s={args:{...r.args,variant:"error"}},o={args:{...r.args,action:{buttonText:"Action",onClick:i("Action clicked")}}},t={args:{...r.args,close:i("Close clicked")}},c={args:{...r.args,action:{buttonText:"Action",onClick:i("Action clicked")},close:i("Close clicked")}},n={args:{...r.args,supportIcon:R.jsx(H,{})}};var p,m,l;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:"{}",...(l=(m=r.parameters)==null?void 0:m.docs)==null?void 0:l.source}}};var d,u,g;a.parameters={...a.parameters,docs:{...(d=a.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'success'
+  }
+}`,...(g=(u=a.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var A,f,C;e.parameters={...e.parameters,docs:{...(A=e.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'warning'
+  }
+}`,...(C=(f=e.parameters)==null?void 0:f.docs)==null?void 0:C.source}}};var k,x,h;s.parameters={...s.parameters,docs:{...(k=s.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    variant: 'error'
+  }
+}`,...(h=(x=s.parameters)==null?void 0:x.docs)==null?void 0:h.source}}};var D,S,W;o.parameters={...o.parameters,docs:{...(D=o.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    action: {
+      buttonText: 'Action',
+      onClick: action('Action clicked')
+    }
+  }
+}`,...(W=(S=o.parameters)==null?void 0:S.docs)==null?void 0:W.source}}};var v,I,T;t.parameters={...t.parameters,docs:{...(v=t.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    close: action('Close clicked')
+  }
+}`,...(T=(I=t.parameters)==null?void 0:I.docs)==null?void 0:T.source}}};var b,E,j;c.parameters={...c.parameters,docs:{...(b=c.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    action: {
+      buttonText: 'Action',
+      onClick: action('Action clicked')
+    },
+    close: action('Close clicked')
+  }
+}`,...(j=(E=c.parameters)==null?void 0:E.docs)==null?void 0:j.source}}};var M,w,y;n.parameters={...n.parameters,docs:{...(M=n.parameters)==null?void 0:M.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    supportIcon: <MegaphoneIcon />
+  }
+}`,...(y=(w=n.parameters)==null?void 0:w.docs)==null?void 0:y.source}}};const Q=["Default","Success","Warning","Error","WithAction","WithClose","WithActionAndClose","WithCustomIcon"];export{r as Default,s as Error,a as Success,e as Warning,o as WithAction,c as WithActionAndClose,t as WithClose,n as WithCustomIcon,Q as __namedExportsOrder,P as default};
---
+++ assets/FormField.stories-nr40AoRc.js
@@ -0,0 +1,161 @@
+import{j as e}from"./jsx-runtime-BlAj40OV.js";import{C as l}from"./index-DvfBLRTt.js";import{F as r,a as u,b as F}from"./index-CjNULpLh.js";import{F as _}from"./index-Xs28d-b-.js";import{F as A}from"./index-L5_0eVMK.js";import{F as a}from"./index-DAO7DxnO.js";import{T as x}from"./index-4n77_aQv.js";import{P as d}from"./index-Z0sHnJT-.js";import{R as c}from"./index-Bs7D5YDf.js";import{r as J}from"./README-B_v4QdWb.js";import{S as Q,a as o}from"./index-BEzTkA2Q.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";const ne={id:"react-input-form-field",title:"React/Input/Form Field",component:r,args:{},tags:["autodocs"],parameters:{docs:{description:{component:J}}}},i={render:()=>e.jsxs(r,{children:[e.jsx(a,{children:"Username"}),e.jsx(x,{placeholder:"Placeholder"})]})},t={render:()=>e.jsxs(r,{children:[e.jsx(a,{children:"Username"}),e.jsx(_,{children:"Vul hier uw username in"}),e.jsx(x,{placeholder:"Placeholder"})]})},n={render:()=>e.jsxs(r,{invalid:!0,children:[e.jsx(a,{children:"Username"}),e.jsx(_,{children:"Description"}),e.jsx(x,{placeholder:"Placeholder",invalid:!0}),e.jsx(A,{children:"Error message"})]})},s={render:()=>e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{type:"checkbox",children:[e.jsx(l,{name:"consent",className:"utrecht-form-field__input"}),"Ik ga akkoord met de algemene voorwaarden"]})})})},m={render:()=>e.jsxs(u,{children:[e.jsx(F,{className:"utrecht-form-fieldset__legend--distanced",children:"Hoe wil je dat wij contact opnemen?"}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"4e054ee1-799e-4608-9055-19d3fc0b88e9",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"4e054ee1-799e-4608-9055-19d3fc0b88e9-description",className:"utrecht-form-field__input",id:"4e054ee1-799e-4608-9055-19d3fc0b88e9",name:"phone",value:"true"}),"Telefoon"]})})}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"238b2692-2460-4bad-93b5-b6def8c3b820",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"238b2692-2460-4bad-93b5-b6def8c3b820-description",className:"utrecht-form-field__input",id:"238b2692-2460-4bad-93b5-b6def8c3b820",name:"email",value:"true"}),"E-mail"]})})}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description",className:"utrecht-form-field__input",id:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2",name:"letter",value:"true"}),"Brief"]})})})]})},b={render:()=>e.jsxs(u,{"aria-describedby":"c7218515-bf54-45c1-ba57-a72af5d7fd42-description",children:[e.jsx(F,{children:"Hoe wil je dat wij contact opnemen?"}),e.jsx(_,{className:"utrecht-form-field-description--distanced",id:"c7218515-bf54-45c1-ba57-a72af5d7fd42-description",children:"Kies één of meerdere opties."}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"3e054ee1-799e-4608-9055-19d3fc0b88e9",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"3e054ee1-799e-4608-9055-19d3fc0b88e9-description",className:"utrecht-form-field__input",id:"3e054ee1-799e-4608-9055-19d3fc0b88e9",name:"phone",value:"true"}),"Telefoon"]})})}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"138b2692-2460-4bad-93b5-b6def8c3b820",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"138b2692-2460-4bad-93b5-b6def8c3b820-description",className:"utrecht-form-field__input",id:"138b2692-2460-4bad-93b5-b6def8c3b820",name:"email",value:"true"}),"E-mail"]})})}),e.jsx(r,{type:"checkbox",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--checkbox",children:e.jsxs(a,{htmlFor:"5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2",type:"checkbox",children:[e.jsx(l,{"aria-describedby":"5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description",className:"utrecht-form-field__input",id:"5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2",name:"letter",value:"true"}),"Brief"]})})})]})},f={render:()=>e.jsxs(u,{children:[e.jsx(F,{className:"utrecht-form-fieldset__legend--distanced",children:"Hoe wil je dat wij contact opnemen?"}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"4e054ee1-799e-4608-9055-19d3fc0b88e0",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"4e054ee1-799e-4608-9055-19d3fc0b88e0",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b2",value:"phone"}),"Telefoon"]})})}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"238b2692-2460-4bad-93b5-b6def8c3b821",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"238b2692-2460-4bad-93b5-b6def8c3b821",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b2",value:"email"}),"E-mail"]})})}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b2",value:"letter"}),"Brief"]})})})]})},h={render:()=>e.jsxs(u,{"aria-describedby":"d7218515-bf54-45c1-ba57-a72af5d7fd42-description",children:[e.jsx(F,{children:"Hoe wil je dat wij contact opnemen?"}),e.jsx(_,{className:"utrecht-form-field-description--distanced",id:"d7218515-bf54-45c1-ba57-a72af5d7fd42-description",children:"Kies één opties."}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"4e054ee1-799e-4608-9055-19d3fc0b88e8",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"4e054ee1-799e-4608-9055-19d3fc0b88e8",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b4",value:"phone"}),"Telefoon"]})})}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"238b2692-2460-4bad-93b5-b6def8c3b829",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"238b2692-2460-4bad-93b5-b6def8c3b829",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b4",value:"email"}),"E-mail"]})})}),e.jsx(r,{type:"radio",children:e.jsx(d,{className:"utrecht-form-field__label utrecht-form-field__label--radio",children:e.jsxs(a,{htmlFor:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5",type:"radio",children:[e.jsx(c,{className:"utrecht-form-field__input",id:"6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5",name:"828aa90e-0e99-4db9-a5e5-d39f689cf7b4",value:"letter"}),"Brief"]})})})]})},p={render:()=>e.jsxs(r,{className:"denhaag-form-field--flex",children:[e.jsx(a,{children:"Options"}),e.jsxs(Q,{children:[e.jsx(o,{value:"",children:"Select an option"}),e.jsx(o,{value:"1",children:"Option #1"}),e.jsx(o,{value:"2",children:"Option #2"}),e.jsx(o,{value:"3",children:"Option #3"}),e.jsx(o,{value:"4",children:"Option #4"})]})]})};var j,y,g;i.parameters={...i.parameters,docs:{...(j=i.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  render: () => <FormField>
+      <FormLabel>Username</FormLabel>
+      <TextInput placeholder="Placeholder" />
+    </FormField>
+}`,...(g=(y=i.parameters)==null?void 0:y.docs)==null?void 0:g.source}}};var k,N,L;t.parameters={...t.parameters,docs:{...(k=t.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  render: () => <FormField>
+      <FormLabel>Username</FormLabel>
+      <FormFieldDescription>Vul hier uw username in</FormFieldDescription>
+      <TextInput placeholder="Placeholder" />
+    </FormField>
+}`,...(L=(N=t.parameters)==null?void 0:N.docs)==null?void 0:L.source}}};var v,P,S;n.parameters={...n.parameters,docs:{...(v=n.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  render: () => <FormField invalid>
+      <FormLabel>Username</FormLabel>
+      <FormFieldDescription>Description</FormFieldDescription>
+      <TextInput placeholder="Placeholder" invalid />
+      <FormFieldErrorMessage>Error message</FormFieldErrorMessage>
+    </FormField>
+}`,...(S=(P=n.parameters)==null?void 0:P.docs)==null?void 0:S.source}}};var O,w,B;s.parameters={...s.parameters,docs:{...(O=s.parameters)==null?void 0:O.docs,source:{originalSource:`{
+  render: () => <FormField type="checkbox">
+      <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+        <FormLabel type="checkbox">
+          <Checkbox name="consent" className="utrecht-form-field__input" />
+          Ik ga akkoord met de algemene voorwaarden
+        </FormLabel>
+      </Paragraph>
+    </FormField>
+}`,...(B=(w=s.parameters)==null?void 0:w.docs)==null?void 0:B.source}}};var D,C,E;m.parameters={...m.parameters,docs:{...(D=m.parameters)==null?void 0:D.docs,source:{originalSource:`{
+  render: () => <Fieldset>
+      <FieldsetLegend className="utrecht-form-fieldset__legend--distanced">
+        Hoe wil je dat wij contact opnemen?
+      </FieldsetLegend>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e9" type="checkbox">
+            <Checkbox aria-describedby="4e054ee1-799e-4608-9055-19d3fc0b88e9-description" className="utrecht-form-field__input" id="4e054ee1-799e-4608-9055-19d3fc0b88e9" name="phone" value="true" />
+            Telefoon
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b820" type="checkbox">
+            <Checkbox aria-describedby="238b2692-2460-4bad-93b5-b6def8c3b820-description" className="utrecht-form-field__input" id="238b2692-2460-4bad-93b5-b6def8c3b820" name="email" value="true" />
+            E-mail
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="checkbox">
+            <Checkbox aria-describedby="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description" className="utrecht-form-field__input" id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" name="letter" value="true" />
+            Brief
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+    </Fieldset>
+}`,...(E=(C=m.parameters)==null?void 0:C.docs)==null?void 0:E.source}}};var R,T,I;b.parameters={...b.parameters,docs:{...(R=b.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  render: () => <Fieldset aria-describedby="c7218515-bf54-45c1-ba57-a72af5d7fd42-description">
+      <FieldsetLegend>Hoe wil je dat wij contact opnemen?</FieldsetLegend>
+      <FormFieldDescription className="utrecht-form-field-description--distanced" id="c7218515-bf54-45c1-ba57-a72af5d7fd42-description">
+        Kies één of meerdere opties.
+      </FormFieldDescription>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="3e054ee1-799e-4608-9055-19d3fc0b88e9" type="checkbox">
+            <Checkbox aria-describedby="3e054ee1-799e-4608-9055-19d3fc0b88e9-description" className="utrecht-form-field__input" id="3e054ee1-799e-4608-9055-19d3fc0b88e9" name="phone" value="true" />
+            Telefoon
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="138b2692-2460-4bad-93b5-b6def8c3b820" type="checkbox">
+            <Checkbox aria-describedby="138b2692-2460-4bad-93b5-b6def8c3b820-description" className="utrecht-form-field__input" id="138b2692-2460-4bad-93b5-b6def8c3b820" name="email" value="true" />
+            E-mail
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="checkbox">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--checkbox">
+          <FormLabel htmlFor="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" type="checkbox">
+            <Checkbox aria-describedby="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2-description" className="utrecht-form-field__input" id="5a1b86d8-f5d2-4852-9f95-b6690ac3d0e2" name="letter" value="true" />
+            Brief
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+    </Fieldset>
+}`,...(I=(T=b.parameters)==null?void 0:T.docs)==null?void 0:I.source}}};var G,H,U;f.parameters={...f.parameters,docs:{...(G=f.parameters)==null?void 0:G.docs,source:{originalSource:`{
+  render: () => <Fieldset>
+      <FieldsetLegend className="utrecht-form-fieldset__legend--distanced">
+        Hoe wil je dat wij contact opnemen?
+      </FieldsetLegend>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e0" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="4e054ee1-799e-4608-9055-19d3fc0b88e0" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2" value="phone" />
+            Telefoon
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b821" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="238b2692-2460-4bad-93b5-b6def8c3b821" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2" value="email" />
+            E-mail
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e1" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b2" value="letter" />
+            Brief
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+    </Fieldset>
+}`,...(U=(H=f.parameters)==null?void 0:H.docs)==null?void 0:U.source}}};var W,K,M;h.parameters={...h.parameters,docs:{...(W=h.parameters)==null?void 0:W.docs,source:{originalSource:`{
+  render: () => <Fieldset aria-describedby="d7218515-bf54-45c1-ba57-a72af5d7fd42-description">
+      <FieldsetLegend>Hoe wil je dat wij contact opnemen?</FieldsetLegend>
+      <FormFieldDescription className="utrecht-form-field-description--distanced" id="d7218515-bf54-45c1-ba57-a72af5d7fd42-description">
+        Kies één opties.
+      </FormFieldDescription>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="4e054ee1-799e-4608-9055-19d3fc0b88e8" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="4e054ee1-799e-4608-9055-19d3fc0b88e8" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4" value="phone" />
+            Telefoon
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="238b2692-2460-4bad-93b5-b6def8c3b829" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="238b2692-2460-4bad-93b5-b6def8c3b829" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4" value="email" />
+            E-mail
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+      <FormField type="radio">
+        <Paragraph className="utrecht-form-field__label utrecht-form-field__label--radio">
+          <FormLabel htmlFor="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5" type="radio">
+            <RadioButton className="utrecht-form-field__input" id="6a1b86d8-f5d2-4852-9f95-b6690ac3d0e5" name="828aa90e-0e99-4db9-a5e5-d39f689cf7b4" value="letter" />
+            Brief
+          </FormLabel>
+        </Paragraph>
+      </FormField>
+    </Fieldset>
+}`,...(M=(K=h.parameters)==null?void 0:K.docs)==null?void 0:M.source}}};var V,q,z;p.parameters={...p.parameters,docs:{...(V=p.parameters)==null?void 0:V.docs,source:{originalSource:`{
+  render: () => <FormField className="denhaag-form-field--flex">
+      <FormLabel>Options</FormLabel>
+      <Select>
+        <SelectOption value="">Select an option</SelectOption>
+        <SelectOption value="1">Option #1</SelectOption>
+        <SelectOption value="2">Option #2</SelectOption>
+        <SelectOption value="3">Option #3</SelectOption>
+        <SelectOption value="4">Option #4</SelectOption>
+      </Select>
+    </FormField>
+}`,...(z=(q=p.parameters)==null?void 0:q.docs)==null?void 0:z.source}}};const se=["Default","WithDescription","Invalid","SingleCheckbox","CheckboxGroup","CheckboxGroupWithDescription","RadioButtonGroup","RadioButtonGroupWithDescription","SelectInput"];export{m as CheckboxGroup,b as CheckboxGroupWithDescription,i as Default,n as Invalid,f as RadioButtonGroup,h as RadioButtonGroupWithDescription,p as SelectInput,s as SingleCheckbox,t as WithDescription,se as __namedExportsOrder,ne as default};
