4a97d821c89a2df92a759399a08e231b98bb4682
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2024-09-24 15:13:46 +0200

    fix: added index.scss

--- components/TextInput/src/index.tsx
+++ components/TextInput/src/index.tsx
@@ -1,6 +1,7 @@
 import { Textbox as TextInput } from '@persoonlijke-regelingen-assistent/components-react';
 import { TextboxProps as UtrechtTextboxProps } from '@utrecht/component-library-react';
 import { InputHTMLAttributes } from 'react';
+import './index.scss';
 
 //PRA doesn't export the props yet, so for now i made a copy.
 interface TextboxSpecificProps {
