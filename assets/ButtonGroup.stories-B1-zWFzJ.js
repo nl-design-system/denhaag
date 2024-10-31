abea817ae81ddc489ea897719d6e59673afd260c
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2024-10-31 13:30:54 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@9d87b939835887f11658b1247823cb22a2e8e72a 🚀

---
+++ assets/ButtonGroup.stories-B1-zWFzJ.js
@@ -0,0 +1,44 @@
+import{j as a}from"./jsx-runtime-BlAj40OV.js";import"./index-Bvl4hp_Y.js";import"./index-Mas5QbYc.js";import"./index-3zeTQWRx.js";import"./index-Cs7sjTYM.js";import"./_commonjsHelpers-BosuxZz1.js";const c=`# Button Group
+
+Button groups are used to group buttons.
+
+## When to use
+
+Mainly used to wrap buttons in the Gutenberg Editor of WordPress.
+
+## Anatomy
+
+The button group consists of:
+
+1. Container
+`,b={title:"CSS/Input/Button Group",tags:["autodocs"],parameters:{docs:{description:{component:c}},status:{type:"WORK IN PROGRESS"}}},t={render:()=>a.jsx("div",{className:"denhaag-button-group",children:a.jsxs("button",{className:"denhaag-button denhaag-button--secondary-action denhaag-button--start-icon",children:[a.jsx("span",{className:"denhaag-button__icon",children:a.jsx("svg",{width:"1em",height:"1em",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",className:"denhaag-icon",focusable:"false","aria-hidden":"true",children:a.jsx("path",{d:"M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"})})}),"Button"]})})},n={render:()=>a.jsxs("div",{className:"denhaag-button-group",children:[a.jsxs("button",{className:"denhaag-button denhaag-button--secondary-action denhaag-button--start-icon",children:[a.jsx("span",{className:"denhaag-button__icon",children:a.jsx("svg",{width:"1em",height:"1em",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",className:"denhaag-icon",focusable:"false","aria-hidden":"true",children:a.jsx("path",{d:"M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"})})}),"Button"]}),a.jsxs("button",{className:"denhaag-button denhaag-button--secondary-action denhaag-button--start-icon",children:[a.jsx("span",{className:"denhaag-button__icon",children:a.jsx("svg",{width:"1em",height:"1em",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",className:"denhaag-icon",focusable:"false","aria-hidden":"true",children:a.jsx("path",{d:"M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"})})}),"Button"]})]})};var e,s,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  render: () => <div className="denhaag-button-group">
+      <button className={'denhaag-button denhaag-button--secondary-action denhaag-button--start-icon'}>
+        <span className={'denhaag-button__icon'}>
+          <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="denhaag-icon" focusable="false" aria-hidden="true">
+            <path d="M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"></path>
+          </svg>
+        </span>
+        Button
+      </button>
+    </div>
+}`,...(o=(s=t.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};var r,d,i;n.parameters={...n.parameters,docs:{...(r=n.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  render: () => <div className="denhaag-button-group">
+      <button className={'denhaag-button denhaag-button--secondary-action denhaag-button--start-icon'}>
+        <span className={'denhaag-button__icon'}>
+          <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="denhaag-icon" focusable="false" aria-hidden="true">
+            <path d="M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"></path>
+          </svg>
+        </span>
+        Button
+      </button>
+      <button className={'denhaag-button denhaag-button--secondary-action denhaag-button--start-icon'}>
+        <span className={'denhaag-button__icon'}>
+          <svg width="1em" height="1em" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="denhaag-icon" focusable="false" aria-hidden="true">
+            <path d="M11.707 18.707a1 1 0 01-1.414 0l-6-6a1 1 0 010-1.414l6-6a1 1 0 111.414 1.414L7.414 11H19a1 1 0 110 2H7.414l4.293 4.293a1 1 0 010 1.414z"></path>
+          </svg>
+        </span>
+        Button
+      </button>
+    </div>
+}`,...(i=(d=n.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const w=["Single","Multiple"];export{n as Multiple,t as Single,w as __namedExportsOrder,b as default};
