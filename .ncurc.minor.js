81946d9524344951e005fd97035d6fb5928d3025
Borai van Uden <info@borai.nl>
2024-12-30 09:10:39 +0100

    docs: add comment

--- .ncurc.minor.js
+++ .ncurc.minor.js
@@ -1,5 +1,6 @@
 const patchConfig = require('./.ncurc.patch');
 
+// TODO: kan update niet uitvoeren omdat functie in laatste versie is disabled: https://github.com/nl-design-system/themes/commit/637fcb0ff11e3edcb0f61f967a45dcc6c7af8669#r150807599
 module.exports = {
   ...patchConfig,
   reject: [...patchConfig.reject, '@nl-design-system-unstable/theme-toolkit'],
