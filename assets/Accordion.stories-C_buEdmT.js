7439154bf66d0af969bfd9880cf71c472c704554
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2025-03-06 11:15:13 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@2c033a0ca8386bbe73f165a63d9b173fc30f2834 🚀

---
+++ assets/Accordion.stories-C_buEdmT.js
@@ -0,0 +1,14 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as i,a as o}from"./index-DXRhoqAG.js";import{t as p}from"./list-D4N81Wxu.js";import{P as a}from"./index-CNu5sYmo.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";const g={children:e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Contact",description:"Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.",children:e.jsx(a,{children:"Contactgegevens"})}),e.jsx(o,{title:"Meldingen",description:"Stel in waarvoor u meldingen wilt ontvangen en op welke manier",children:e.jsx(a,{children:"Meldingen"})})]})},A={id:"react-surfaces-accordion",title:"React/Surfaces/Accordion",component:i,args:g,tags:["autodocs"],parameters:{tokensPrefix:"denhaag-accordion",tokens:p}},n={},r={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(i,{children:e.jsx(o,{title:"Contact",description:"Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.",children:e.jsx(a,{children:"Contactgegevens"})})}),e.jsx(i,{children:e.jsx(o,{title:"Meldingen",description:"Stel in waarvoor u meldingen wilt ontvangen en op welke manier",children:e.jsx(a,{children:"Meldingen"})})})]})};var t,c,s;n.parameters={...n.parameters,docs:{...(t=n.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(c=n.parameters)==null?void 0:c.docs)==null?void 0:s.source}}};var d,l,m;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <>
+      <Accordion>
+        <AccordionSection title="Contact" description="Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.">
+          <Paragraph>Contactgegevens</Paragraph>
+        </AccordionSection>
+      </Accordion>
+      <Accordion>
+        <AccordionSection title="Meldingen" description="Stel in waarvoor u meldingen wilt ontvangen en op welke manier">
+          <Paragraph>Meldingen</Paragraph>
+        </AccordionSection>
+      </Accordion>
+    </>
+}`,...(m=(l=r.parameters)==null?void 0:l.docs)==null?void 0:m.source}}};const S=["Default","Standalone"];export{n as Default,r as Standalone,S as __namedExportsOrder,A as default};
