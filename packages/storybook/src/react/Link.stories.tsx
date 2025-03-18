f9954601ec646a2825d106e79c7fd00daed095c1
Borai van Uden <info@borai.nl>
2025-03-18 14:02:23 +0100

    feat: link candidate component

--- packages/storybook/src/react/Link.stories.tsx
+++ packages/storybook/src/react/Link.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Meta, StoryObj } from '@storybook/react';
-import { ArrowLeftIcon, ArrowRightIcon } from '@gemeente-denhaag/icons';
-import { Paragraph } from '@gemeente-denhaag/typography';
+import * as Icons from '@gemeente-denhaag/icons';
+import { ArrowRightIcon, ArrowLeftIcon } from '@gemeente-denhaag/icons';
+import { Paragraph } from '@gemeente-denhaag/paragraph';
 import { Link } from '@gemeente-denhaag/link';
 
 import readme from '../../../../components/Link/README.md?raw';
@@ -10,12 +11,36 @@ const exampleArgs = {
   href: '#',
 };
 
-const meta = {
-  id: 'react-navigation-link',
-  title: 'React/Navigation/Link',
+const meta: Meta<typeof Link> = {
   component: Link,
-  args: exampleArgs,
   tags: ['autodocs'],
+  args: exampleArgs,
+  argTypes: {
+    disabled: {
+      order: 1,
+      type: 'boolean',
+      options: [true, false],
+      control: {
+        type: 'select',
+      },
+    },
+    icon: {
+      order: 2,
+      type: 'string',
+      options: [...Object.keys(Icons)],
+      control: {
+        type: 'select',
+      },
+    },
+    iconAlign: {
+      order: 3,
+      type: 'string',
+      options: ['start', 'end'],
+      control: {
+        type: 'select',
+      },
+    },
+  },
   parameters: {
     docs: {
       description: {
@@ -23,51 +48,46 @@ const meta = {
       },
     },
   },
-} as Meta<typeof Link>;
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: (args) => (
-    <Paragraph>
-      It is possible to put <Link {...args}>a link</Link> in a Paragraph element.
-    </Paragraph>
-  ),
+  render: (args) => {
+    const Icon = Icons[args.icon as unknown as keyof typeof Icons];
+    args.icon = Icon ? <Icon /> : args.icon || undefined;
+
+    return (
+      <Paragraph>
+        It is possible to put <Link {...args}>a link</Link> in a Paragraph element.
+      </Paragraph>
+    );
+  },
 };
 
-export const disabled: Story = {
-  render: (args) => (
-    <Paragraph>
-      It is possible to put{' '}
-      <Link disabled {...args}>
-        a link
-      </Link>{' '}
-      in a Paragraph element.
-    </Paragraph>
-  ),
+export const Disabled: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
 };
 
-export const iconStart: Story = {
-  render: (args) => (
-    <Paragraph>
-      It is possible to put{' '}
-      <Link icon={<ArrowLeftIcon />} iconAlign="start" {...args}>
-        a link
-      </Link>{' '}
-      in a Paragraph element.
-    </Paragraph>
-  ),
+export const IconStart: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    icon: <ArrowLeftIcon />,
+    iconAlign: 'start',
+  },
 };
 
-export const iconEnd: Story = {
-  render: (args) => (
-    <Paragraph>
-      It is possible to put{' '}
-      <Link icon={<ArrowRightIcon />} {...args}>
-        a link
-      </Link>{' '}
-      in a Paragraph element.
-    </Paragraph>
-  ),
+export const IconEnd: Story = {
+  ...Default,
+  args: {
+    ...Default.args,
+    icon: <ArrowRightIcon />,
+    iconAlign: 'end',
+  },
 };
