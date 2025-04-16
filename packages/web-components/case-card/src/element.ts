658eeae1ad7d639fdb1772461914bdc129c82a76
Bryan de Jong <bryan@levitade.io>
2025-04-16 11:34:39 +0200

    chore: refactor web component card code

--- packages/web-components/case-card/src/element.ts
+++ packages/web-components/case-card/src/element.ts
@@ -4,77 +4,14 @@ It should be taken from node_modules/@utrecht/paragraph-css/dist/index.mjs,
 but the module resolution is not working yet
  */
 import css, { utrechtParagraphCss, denhaagIconCss } from './css.js';
+import { escapeXML } from './util.js';
 
 export interface CardData {
-  linkAriaLabel: string;
-  lang: string;
-  dateTime: string;
+  lang?: string;
+  linkLabel: string;
+  dateTime?: string;
   href: string;
-  heading: string;
 }
-
-export const QUOTATION_MARK = 0x0022;
-export const AMPERSAND = 0x0026;
-export const LESS_THAN_SIGN = 0x003c;
-export const GREATER_THAN_SIGN = 0x003e;
-export const SPACE = 0x0020;
-export const NONCHARACTER_FFFE = 0xfffe;
-export const NONCHARACTER_FFFF = 0xffff;
-
-/**
- * Escaping of the following characters using entities: & < > "
- * Also replace non-character ranges with the replacement character (U+FFFD)
- *
- * @param {string} xml
- * @param {boolean=} attr When false, double quotes will not be escaped
- * @return {string}
- */
-export const escapeXML = function escapeXML(xml: string) {
-  /**
-   * Escape the Unicode 'non-characters'
-   * Escape characters below 0xC0 except tab and the newlines
-   * Escape < to prevent starting an element
-   * Escape > to prevent ending a CDATA section
-   * Escape " to prevent ending an attribute
-   * Escape & to prevent starting an entity reference
-   * Don't escape surrogate blocks, because they are essential for Unicode support in JavaScript
-   *
-   * @see http://www.w3.org/TR/REC-xml/#charsets
-   * @param {string} m One character.
-   * @return {string} The same character, or its escaped version.
-   */
-  function escapeChar(char: string) {
-    let m = char;
-    const charCode = m.charCodeAt(0);
-
-    if (charCode === QUOTATION_MARK) {
-      // && attr !== false)
-      // One byte shorter than than &quot;
-      m = '&#34;';
-    } else if (charCode === AMPERSAND) {
-      // &
-      m = '&amp;';
-    } else if (charCode === GREATER_THAN_SIGN) {
-      /*
-       * In a text node the > needn't be escaped,
-       * but in CDATA sections it must be.
-       */
-      // <
-      m = '&gt;';
-    } else if (charCode === LESS_THAN_SIGN) {
-      m = '&lt;';
-    } else if (charCode < SPACE || charCode === NONCHARACTER_FFFE || charCode === NONCHARACTER_FFFF) {
-      m = '\uFFFD';
-    }
-
-    return m;
-  }
-
-  // TODO: Move RegExp to constant
-
-  // eslint-disable-next-line no-control-regex
-  return xml.replace(/[\0-\t\u000B\u000C\u000E-\u00C0"&<>\uFFFE\uFFFF]/g, escapeChar);
-};
 
 const sheet = new CSSStyleSheet();
 sheet.replaceSync(css);
@@ -88,18 +25,16 @@ denhaagIconSheet.replaceSync(denhaagIconCss);
 export class DenhaagCaseCardElement extends HTMLElement implements CardData {
   dateTime: string;
   href: string;
-  linkAriaLabel: string;
-  heading: string;
+  linkLabel: string;
   _shadow: ShadowRoot;
   _div: HTMLElement;
 
   constructor() {
     super();
-    this.linkAriaLabel = this.getAttribute('linkAriaLabel') || '';
-    this.lang = this.getAttribute('lang') || 'nl-NL'; // maybe get the lang from the document as fallback
     this.dateTime = this.getAttribute('datetime') || '';
     this.href = this.getAttribute('href') || '';
-    this.heading = this.getAttribute('heading') || '';
+    this.lang = this.getAttribute('lang') || 'nl-NL'; // maybe get the lang from the document as fallback
+    this.linkLabel = this.getAttribute('linkLabel') || '';
 
     this._shadow = this.attachShadow({
       mode: 'closed',
@@ -114,8 +49,24 @@ export class DenhaagCaseCardElement extends HTMLElement implements CardData {
     this.render();
   }
 
+  getDateTimeElement() {
+    if (!this.dateTime) {
+      return '';
+    }
+
+    const formattedDate = this.dateTime && new Intl.DateTimeFormat(this.lang).format(new Date(this.dateTime));
+
+    return `<div class="denhaag-card__date-wrapper">
+              <date datetime="${escapeXML(this.dateTime)}">${escapeXML(formattedDate)}</date>
+            </div>`;
+  }
+
   render() {
-    const formattedDate = new Intl.DateTimeFormat(this.lang).format(new Date(this.dateTime));
+    const subtitleSlot = this.querySelector('[slot="subtitle"]')
+      ? `<p class="utrecht-paragraph denhaag-card__subtitle">
+          <slot name="subtitle"></slot>
+        </p>`
+      : '';
 
     this._div.innerHTML = `
       <div class="denhaag-card denhaag-case-card">
@@ -126,13 +77,11 @@ export class DenhaagCaseCardElement extends HTMLElement implements CardData {
               <p class="utrecht-paragraph denhaag-card__title">
                 <slot name="heading"></slot>
               </p>
-              <p class="utrecht-paragraph denhaag-card__subtitle">
-                <slot name="subtitle"></slot>
-              </p>
+              ${subtitleSlot}
             </div>
             <div class="denhaag-card__actions">
-              ${this.dateTime && `<div class="denhaag-card__date-wrapper"><date datetime="${escapeXML(this.dateTime)}">${escapeXML(formattedDate)}</date></div>`}
-              <a aria-label=${this.linkAriaLabel} href="${escapeXML(this.href)}" class="denhaag-card__action-link"
+              ${this.getDateTimeElement()}
+              <a aria-label=${this.linkLabel} href="${escapeXML(this.href)}" class="denhaag-card__action-link"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
