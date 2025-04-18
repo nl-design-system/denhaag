f297feeec64041154785f9e219e2fcccd9eadaf1
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2025-04-18 14:16:20 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@f9e8e9caaaac2b3145520572865953e9f6e8a238 🚀

---
+++ assets/SubjectCard.stories-Ch31Nv1R.js
@@ -0,0 +1,7 @@
+import{S as p}from"./index-BDEYAJn8.js";import{r as i}from"./README-CQHOMdg4.js";import{t as d}from"./util-DBKc7DZo.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";import"./jsx-runtime-D_zvdyIk.js";import"./index-DgcIFbrm.js";const l={title:"Shrimp and Chorizo Paella",subTitle:"This impressive paella is a perfect party dish and a fun meal to cook.",href:"#"},b={id:"react-cards-subject-card",title:"React/Cards/Subject Card",component:p,args:l,tags:["autodocs"],parameters:{docs:{description:{component:i}}}},e={},a={args:{...e.args,date:"2020-01-21",locale:d}};var r,t,o;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(o=(t=e.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};var s,c,m;a.parameters={...a.parameters,docs:{...(s=a.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    date: '2020-01-21',
+    locale: templateLocale
+  }
+}`,...(m=(c=a.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const C=["Default","WithDate"];export{e as Default,a as WithDate,C as __namedExportsOrder,b as default};
