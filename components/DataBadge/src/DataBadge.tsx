2e983d5e68a09c8476d3d1864cd639b5bda7a19b
Borai van Uden <info@borai.nl>
2025-04-22 16:06:49 +0200

    feat: moved dataBadge to other file

---
+++ components/DataBadge/src/DataBadge.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import clsx from 'clsx';
+import {
+  DataBadge as NLDataBadge,
+  DataBadgeProps as NLDataBadgeProps,
+} from '@nl-design-system-candidate/data-badge-react';
+export * from '@nl-design-system-candidate/data-badge-react';
+import '@nl-design-system-candidate/data-badge-css/data-badge.css';
+
+export type DataBadgeProps = NLDataBadgeProps & {
+  status?: 'neutral' | 'success' | 'warning' | 'error';
+};
+
+export const DataBadge = ({ status, ...props }: DataBadgeProps) => {
+  const classes = clsx({
+    'nl-data-badge--neutral': status === 'neutral',
+    'nl-data-badge--success': status === 'success',
+    'nl-data-badge--warning': status === 'warning',
+    'nl-data-badge--error': status === 'error',
+  });
+
+  return <NLDataBadge className={classes} {...props} />;
+};
+
+export default DataBadge;
