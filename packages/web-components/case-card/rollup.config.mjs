291ff4a8993ae94ade0e3542bbd7b0435c06f7ab
Bryan de Jong <bryan@levitade.io>
2025-04-29 11:18:29 +0200

    feat: build separate index and element js files

--- packages/web-components/case-card/rollup.config.mjs
+++ packages/web-components/case-card/rollup.config.mjs
@@ -1,15 +1,30 @@
-import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
-const config = {
-  input: 'src/index.ts',
-  output: {
-    dir: './dist',
-    sourcemap: false,
-    format: 'esm',
-    compact: true,
+const configs = [
+  {
+    input: 'src/element.ts',
+    output: {
+      dir: './dist',
+      entryFileNames: 'element.js',
+      sourcemap: false,
+      format: 'esm',
+      compact: true,
+    },
+    plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.build.json' })],
   },
-  plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.build.json' })],
-};
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: './dist',
+      entryFileNames: 'index.js',
+      sourcemap: false,
+      format: 'esm',
+      compact: true,
+    },
+    plugins: [nodeResolve(), typescript({ tsconfig: './tsconfig.build.json' })],
+    external: ['./element.js'],
+  },
+];
 
-export default config;
+export default configs;
