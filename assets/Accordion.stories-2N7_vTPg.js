99259c056fd8b559a6a064eda993f636c25e4657
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2025-04-29 10:18:09 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@401fece5df7813c1122bc9026f9f37b203c10965 🚀

---
+++ assets/Accordion.stories-2N7_vTPg.js
@@ -0,0 +1,50 @@
+import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{A as n,a}from"./index-BS1RqJCu.js";import{t as w}from"./list-Cnrvab_m.js";import{P as m}from"./index-CNu5sYmo.js";import{D as g}from"./index-B1Y3eFJt.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";const A={children:e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Contact",description:"Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.",children:e.jsx(m,{children:"Contactgegevens"})}),e.jsx(a,{title:"Meldingen",description:"Stel in waarvoor u meldingen wilt ontvangen en op welke manier",children:e.jsx(g,{items:[{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."}]})})]})},x={id:"css-surfaces-accordion",title:"CSS/Surfaces/Accordion",component:n,args:A,tags:["autodocs"],parameters:{tokensPrefix:"denhaag-accordion",tokens:w}},i={},t={render:()=>e.jsxs(e.Fragment,{children:[e.jsx(n,{children:e.jsx(a,{title:"Contact",description:"Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.",children:e.jsx(m,{children:"Contactgegevens"})})}),e.jsx(n,{children:e.jsx(a,{title:"Meldingen",description:"Stel in waarvoor u meldingen wilt ontvangen en op welke manier",children:e.jsx(g,{items:[{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."},{title:"Beast of Bodmin",detail:"A large feline inhabiting Bodmin Moor."},{title:"Morgawr",detail:"A sea serpent."},{title:"Owlman",detail:"A giant owl-like creature."}]})})})]})};var r,o,l;i.parameters={...i.parameters,docs:{...(r=i.parameters)==null?void 0:r.docs,source:{originalSource:"{}",...(l=(o=i.parameters)==null?void 0:o.docs)==null?void 0:l.source}}};var d,s,c;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <>
+      <Accordion>
+        <AccordionSection title="Contact" description="Hier vindt u uw e-mailadres en telefoonnummer waarmee we u kunnen bereiken.">
+          <Paragraph>Contactgegevens</Paragraph>
+        </AccordionSection>
+      </Accordion>
+      <Accordion>
+        <AccordionSection title="Meldingen" description="Stel in waarvoor u meldingen wilt ontvangen en op welke manier">
+          <DescriptionList items={[{
+          title: 'Beast of Bodmin',
+          detail: 'A large feline inhabiting Bodmin Moor.'
+        }, {
+          title: 'Morgawr',
+          detail: 'A sea serpent.'
+        }, {
+          title: 'Owlman',
+          detail: 'A giant owl-like creature.'
+        }, {
+          title: 'Beast of Bodmin',
+          detail: 'A large feline inhabiting Bodmin Moor.'
+        }, {
+          title: 'Morgawr',
+          detail: 'A sea serpent.'
+        }, {
+          title: 'Owlman',
+          detail: 'A giant owl-like creature.'
+        }, {
+          title: 'Beast of Bodmin',
+          detail: 'A large feline inhabiting Bodmin Moor.'
+        }, {
+          title: 'Morgawr',
+          detail: 'A sea serpent.'
+        }, {
+          title: 'Owlman',
+          detail: 'A giant owl-like creature.'
+        }, {
+          title: 'Beast of Bodmin',
+          detail: 'A large feline inhabiting Bodmin Moor.'
+        }, {
+          title: 'Morgawr',
+          detail: 'A sea serpent.'
+        }, {
+          title: 'Owlman',
+          detail: 'A giant owl-like creature.'
+        }]} />
+        </AccordionSection>
+      </Accordion>
+    </>
+}`,...(c=(s=t.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const b=["Default","Standalone"];export{i as Default,t as Standalone,b as __namedExportsOrder,x as default};
