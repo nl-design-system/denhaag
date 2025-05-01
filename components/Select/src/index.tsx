97e61b232a63d328894fd2f69c2ff4a8b4a91f82
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2025-05-01 15:47:31 +0200

    fix: accept ref prop in select

--- components/Select/src/index.tsx
+++ components/Select/src/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { RefAttributes } from 'react';
 import {
   Select as UtrechtSelect,
   SelectOption,
-  SelectProps,
+  SelectProps as UtrechtSelectProps,
   SelectOptionProps,
 } from '@utrecht/component-library-react';
 import './index.scss';
 import clsx from 'clsx';
+
+type SelectProps = UtrechtSelectProps & RefAttributes<HTMLSelectElement>;
 
 const Select = ({ className, ...props }: SelectProps) => {
   const classNames = clsx(className, 'denhaag-select');
