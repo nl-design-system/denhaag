fa8777995bb100020d194ca7760e58e5ac9b55a6
Borai van Uden <info@borai.nl>
2025-05-12 16:05:56 +0200

    fix: media query

--- components/Card/src/CaseCard.tsx
+++ components/Card/src/CaseCard.tsx
@@ -2,13 +2,12 @@ import React, { AnchorHTMLAttributes, ComponentType } from 'react';
 import { ArrowRightIcon } from '@gemeente-denhaag/icons';
 import { BasicLink } from '@gemeente-denhaag/link';
 import { Paragraph } from '@gemeente-denhaag/typography';
-import Card from './CaseCardBase';
+import Base from './CaseCardBase';
 import Footer from './CaseCardFooter';
 import Action from './CaseCardAction';
 import Wrapper from './CaseCardWrapper';
 import Background from './CaseCardBackground';
 import Context from './CaseCardContext';
-import Container from './CaseCardContainer';
 
 export type CaseCardAppearance = 'default' | 'archived' | 'list';
 
@@ -30,23 +29,21 @@ export const CaseCard = ({
   Link = BasicLink,
 }: CaseCardProps) => {
   return (
-    <Container>
-      <Card appearance={appearance}>
-        <Wrapper>
-          <Background />
-          <div>
-            <Paragraph className="denhaag-case-card__title">{title}</Paragraph>
-            <Paragraph className="denhaag-case-card__subtitle">{subTitle}</Paragraph>
-          </div>
-          <Footer>
-            <Context>{context}</Context>
-            <Action aria-label={title} href={href} Action={Link}>
-              <ArrowRightIcon className="denhaag-case-card__arrow" />
-            </Action>
-          </Footer>
-        </Wrapper>
-      </Card>
-    </Container>
+    <Base appearance={appearance}>
+      <Wrapper>
+        <Background />
+        <div>
+          <Paragraph className="denhaag-case-card__title">{title}</Paragraph>
+          <Paragraph className="denhaag-case-card__subtitle">{subTitle}</Paragraph>
+        </div>
+        <Footer>
+          <Context>{context}</Context>
+          <Action aria-label={title} href={href} Action={Link}>
+            <ArrowRightIcon className="denhaag-case-card__arrow" />
+          </Action>
+        </Footer>
+      </Wrapper>
+    </Base>
   );
 };
 
