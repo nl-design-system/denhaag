99238ebda47670ed5cc48255991d22f2511cad27
Borai van Uden <info@borai.nl>
2025-05-12 11:31:21 +0200

    feat: split up css files

--- components/Card/src/Card/Card.tsx
+++ components/Card/src/Card/Card.tsx
@@ -2,10 +2,20 @@ import React, { HTMLAttributes } from 'react';
 import '../index.scss';
 import clsx from 'clsx';
 
-export type CardProps = HTMLAttributes<HTMLDivElement>;
+export type CardProps = HTMLAttributes<HTMLDivElement> & {
+  appearance?: 'archived' | 'list';
+};
 
-export const Card = ({ className, ...props }: CardProps) => {
-  const classNames = clsx('denhaag-case-card', className);
+export const Card = ({ appearance, className, ...props }: CardProps) => {
+  const classNames = clsx(
+    'denhaag-case-card',
+    {
+      'denhaag-case-card--archived': appearance === 'archived',
+      'denhaag-case-card--list': appearance === 'list',
+    },
+    className,
+  );
+
   return <div className={classNames} {...props}></div>;
 };
 
--- components/Card/src/Card/CaseCard.tsx
+++ components/Card/src/Card/CaseCard.tsx
@@ -2,7 +2,6 @@ import React, { AnchorHTMLAttributes, ComponentType } from 'react';
 import { ArrowRightIcon } from '@gemeente-denhaag/icons';
 import { BasicLink } from '@gemeente-denhaag/link';
 import { Paragraph } from '@gemeente-denhaag/typography';
-import clsx from 'clsx';
 import '../index.scss';
 import Card from './Card';
 import Footer from './Footer';
@@ -10,6 +9,7 @@ import Action from './Action';
 import Wrapper from './Wrapper';
 import Background from './Background';
 import Context from './Context';
+import Container from './Container';
 
 export interface CaseCardProps {
   title: string;
@@ -21,27 +21,24 @@ export interface CaseCardProps {
 }
 
 export const CaseCard = ({ title, subTitle, context, href, appearance, Link = BasicLink }: CaseCardProps) => {
-  const classNames = clsx({
-    'denhaag-case-card--archived': appearance === 'archived',
-    'denhaag-case-card--list': appearance === 'list',
-  });
-
   return (
-    <Card className={classNames}>
-      <Wrapper>
-        <Background />
-        <div>
-          <Paragraph className="denhaag-case-card__title">{title}</Paragraph>
-          <Paragraph className="denhaag-case-card__subtitle">{subTitle}</Paragraph>
-        </div>
-        <Footer>
-          <Context>{context}</Context>
-          <Action aria-label={title} href={href} Action={Link}>
-            <ArrowRightIcon className="denhaag-case-card__arrow" />
-          </Action>
-        </Footer>
-      </Wrapper>
-    </Card>
+    <Container>
+      <Card appearance={appearance}>
+        <Wrapper>
+          <Background />
+          <div>
+            <Paragraph className="denhaag-case-card__title">{title}</Paragraph>
+            <Paragraph className="denhaag-case-card__subtitle">{subTitle}</Paragraph>
+          </div>
+          <Footer>
+            <Context>{context}</Context>
+            <Action aria-label={title} href={href} Action={Link}>
+              <ArrowRightIcon className="denhaag-case-card__arrow" />
+            </Action>
+          </Footer>
+        </Wrapper>
+      </Card>
+    </Container>
   );
 };
 
