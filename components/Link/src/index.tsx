ae5cacc87be1a2429aec9fd51a8c99b8ee6b6103
Borai van Uden <info@borai.nl>
2025-06-23 12:08:40 +0200

    Revert "feat: export link from index file"
    This reverts commit 76f4c3601db5f4c1837b44bbbab839801e08242c.

--- components/Link/src/index.tsx
+++ components/Link/src/index.tsx
@@ -1,6 +1,36 @@
+import React, { ComponentType } from 'react';
+import { SvgIconProps } from '@gemeente-denhaag/icons';
+import BasicLink, { LinkProps as CandidateLinkProps } from './BasicLink';
+import clsx from 'clsx';
 import './index.scss';
-import Link from './Link';
+
+export interface LinkProps extends CandidateLinkProps {
+  icon?: React.ReactElement<SvgIconProps>;
+  iconAlign?: 'start' | 'end';
+  Link?: ComponentType<CandidateLinkProps>;
+}
+
+export const Link = ({ icon = undefined, iconAlign = 'end', children, Link = BasicLink, ...props }: LinkProps) => {
+  const rootClassNames = clsx(
+    'denhaag-link',
+    {
+      'denhaag-link--disabled': props.disabled,
+      'denhaag-link--with-icon': icon !== undefined,
+      'denhaag-link--with-icon-start': icon !== undefined && iconAlign === 'start',
+      'denhaag-link--with-icon-end': icon !== undefined && iconAlign === 'end',
+    },
+    props.className,
+  );
+  const iconClassName = clsx('denhaag-link__icon');
+  const iconWrapped = <span className={iconClassName}>{icon}</span>;
+
+  return (
+    <Link className={rootClassNames} {...props}>
+      {icon !== undefined && iconAlign === 'start' ? iconWrapped : ''}
+      {children}
+      {icon !== undefined && iconAlign === 'end' ? iconWrapped : ''}
+    </Link>
+  );
+};
 
 export default Link;
-
-export * from './Link';
