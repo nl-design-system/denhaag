e121c064ebc0967c3b70478435ac2b9b566b8353
Charlotte <42849307+eslook@users.noreply.github.com>
2025-07-11 07:42:28 +0000

    docs: community stappenplan - EUPL-1.2 licentie toegepast

---
+++ packages/storybook/src/denhaag/License.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+import md from '../../../LICENSE.md?raw';
+
+const meta = {
+  title: 'Den Haag/License',
+  tags: ['autodocs', '!dev'],
+  parameters: {
+    chromatic: { disableSnapshot: true },
+    docs: {
+      page: md,
+    },
+  },
+} as Meta;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
