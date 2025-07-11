bc3b74a4d67aff78175c3bad176ac4f20c47aac9
eslook <42849307+eslook@users.noreply.github.com>
2025-07-11 13:57:52 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@9fd7b1a25145bbb88838400bb065012f5791ab67 🚀

---
+++ assets/CaseCard.stories-Cm3gtIcV.js
@@ -0,0 +1,85 @@
+import{j as t}from"./jsx-runtime-D_zvdyIk.js";import{C as B}from"./index-Bczgnj5r.js";import{t as d}from"./util-LfLgGEA6.js";import{f as l,l as g}from"./index-CLDixfYs.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./index-BvWrhx6F.js";import"./index-C4DVoboI.js";import"./index-DlFZmAEG.js";const F=`<!-- @license CC0-1.0 -->
+
+# Case Card
+
+Toont meta-informatie over een zaak en biedt de mogelijkheid hier naartoe te navigeren.
+
+## References
+
+[https://nldesignsystem.nl/case-card/](https://nldesignsystem.nl/case-card/)
+`,ee={component:B,tags:["autodocs"],argTypes:{title:{type:"string"},subTitle:{type:"string"},context:{type:"string"},href:{type:"string"},appearance:{type:"string",options:[void 0,"archived"],control:{type:"select"}}},parameters:{docs:{description:{component:F}}}},a=[A=>t.jsx("div",{style:{maxWidth:"360px"},children:t.jsx(A,{})})],e={args:{title:"Shrimp and Chorizo Paella fwaefawef awef awe",subTitle:"This impressive paella is a perfect party dish and a fun meal to cook.",context:"",href:"#"},decorators:a},r={args:{...e.args,context:t.jsx("time",{dateTime:new Date("2020-01-22").toISOString(),children:l({dateTime:"2020-01-22",locale:d,format:g})[0]})},decorators:a},s={args:{...e.args,context:"VTH-TEST-2024-02437"},decorators:a},n={args:{...e.args,appearance:"archived"},decorators:a},o={args:{...e.args,appearance:"archived",context:t.jsx("time",{dateTime:new Date("2020-01-22").toISOString(),children:l({dateTime:"2020-01-22",locale:d,format:g})[0]})},decorators:a},c={args:{...e.args,appearance:"archived",context:"VTH-TEST-2024-02437"},decorators:a},i={args:{...e.args,appearance:"list"}},m={args:{...e.args,appearance:"list",context:t.jsx("time",{dateTime:new Date("2020-01-22").toISOString(),children:l({dateTime:"2020-01-22",locale:d,format:g})[0]})}},p={args:{...e.args,appearance:"list",context:"VTH-TEST-2024-02437"}};var u,f,h;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    title: 'Shrimp and Chorizo Paella fwaefawef awef awe',
+    subTitle: 'This impressive paella is a perfect party dish and a fun meal to cook.',
+    context: '',
+    href: '#'
+  },
+  decorators
+}`,...(h=(f=e.parameters)==null?void 0:f.docs)==null?void 0:h.source}}};var T,D,S;r.parameters={...r.parameters,docs:{...(T=r.parameters)==null?void 0:T.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    context: <time dateTime={new Date('2020-01-22').toISOString()}>
+        {formatDate({
+        dateTime: '2020-01-22',
+        locale: templateLocale,
+        format: longDateOptions
+      })[0]}
+      </time>
+  },
+  decorators
+}`,...(S=(D=r.parameters)==null?void 0:D.docs)==null?void 0:S.source}}};var x,v,C;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    context: 'VTH-TEST-2024-02437'
+  },
+  decorators
+}`,...(C=(v=s.parameters)==null?void 0:v.docs)==null?void 0:C.source}}};var w,W,y;n.parameters={...n.parameters,docs:{...(w=n.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'archived'
+  },
+  decorators
+}`,...(y=(W=n.parameters)==null?void 0:W.docs)==null?void 0:y.source}}};var I,O,b;o.parameters={...o.parameters,docs:{...(I=o.parameters)==null?void 0:I.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'archived',
+    context: <time dateTime={new Date('2020-01-22').toISOString()}>
+        {formatDate({
+        dateTime: '2020-01-22',
+        locale: templateLocale,
+        format: longDateOptions
+      })[0]}
+      </time>
+  },
+  decorators
+}`,...(b=(O=o.parameters)==null?void 0:O.docs)==null?void 0:b.source}}};var L,j,E;c.parameters={...c.parameters,docs:{...(L=c.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'archived',
+    context: 'VTH-TEST-2024-02437'
+  },
+  decorators
+}`,...(E=(j=c.parameters)==null?void 0:j.docs)==null?void 0:E.source}}};var H,N,V;i.parameters={...i.parameters,docs:{...(H=i.parameters)==null?void 0:H.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'list'
+  }
+}`,...(V=(N=i.parameters)==null?void 0:N.docs)==null?void 0:V.source}}};var k,z,P;m.parameters={...m.parameters,docs:{...(k=m.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'list',
+    context: <time dateTime={new Date('2020-01-22').toISOString()}>
+        {formatDate({
+        dateTime: '2020-01-22',
+        locale: templateLocale,
+        format: longDateOptions
+      })[0]}
+      </time>
+  }
+}`,...(P=(z=m.parameters)==null?void 0:z.docs)==null?void 0:P.source}}};var R,_,q;p.parameters={...p.parameters,docs:{...(R=p.parameters)==null?void 0:R.docs,source:{originalSource:`{
+  args: {
+    ...Default.args,
+    appearance: 'list',
+    context: 'VTH-TEST-2024-02437'
+  }
+}`,...(q=(_=p.parameters)==null?void 0:_.docs)==null?void 0:q.source}}};const ae=["Default","WithDate","WithCaseNumber","Inactive","InactiveWithDate","InactiveWithCaseNumber","List","ListWithDate","ListWithCaseNumber"];export{e as Default,n as Inactive,c as InactiveWithCaseNumber,o as InactiveWithDate,i as List,p as ListWithCaseNumber,m as ListWithDate,s as WithCaseNumber,r as WithDate,ae as __namedExportsOrder,ee as default};
