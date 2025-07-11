a31b3ee653e3caaed5c26fc34339d8832513a3fb
eslook <42849307+eslook@users.noreply.github.com>
2025-07-11 09:08:31 +0000

    Deploying to gh-pages from @ nl-design-system/denhaag@6430b16f490f0d37bb3b2bfcd540910273eead79 🚀

--- assets/SideNavigation.stories-Baa4U10_.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./jsx-runtime-D_zvdyIk.js";import{G as o,d as a,q as i,r as c,U as m}from"./index-C4DVoboI.js";import{e as d}from"./index-CGLOogeq.js";import"./index-raSRI-5R.js";import"./_commonjsHelpers-BosuxZz1.js";import"./clsx-B-dksMZM.js";import"./index-BtU0A77O.js";const l=`# Side Navigation
-
-Verticale navigatie die de mogelijkheid biedt om tussen pagina's te navigeren.
-
-## References
-
-[https://nldesignsystem.nl/side-navigation](https://nldesignsystem.nl/side-navigation)
-`,p={items:[[{href:"#",label:"Overzicht",icon:e.jsx(o,{}),current:!0}],[{href:"#",label:"Mijn taken",icon:e.jsx(a,{})},{href:"#",label:"Mijn berichten",icon:e.jsx(i,{}),counter:2},{href:"#",label:"Mijn zaken",icon:e.jsx(c,{})}],[{href:"#",label:"Mijn gegevens",icon:e.jsx(m,{})}]]},b={component:d,args:p,tags:["autodocs"],parameters:{docs:{description:{component:l}}}},n={};var s,r,t;n.parameters={...n.parameters,docs:{...(s=n.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(t=(r=n.parameters)==null?void 0:r.docs)==null?void 0:t.source}}};const I=["Default"];export{n as Default,I as __namedExportsOrder,b as default};
