fe09890a2fd185979de9b2bdb3c59ff50af529fc
YourivHDenHaag <youri.vanheumen@denhaag.nl>
2025-09-01 16:19:48 +0200

    fix: cleanup footer from link-group

---
+++ packages/storybook/src/react/Footer.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import { Footer } from '@gemeente-denhaag/footer';
+import { contactData, footerLegalData, footerSocialData, newsletterData } from '../templates/util';
+
+const exampleArgs = {
+  contactData: contactData,
+  socialData: footerSocialData,
+  newsletterData: newsletterData,
+  legalData: footerLegalData,
+};
+
+const meta = {
+  id: 'react-page-elements-footer',
+  title: 'React/Page Elements/Footer',
+  component: Footer,
+  args: exampleArgs,
+  tags: ['autodocs'],
+} as Meta<typeof Footer>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
