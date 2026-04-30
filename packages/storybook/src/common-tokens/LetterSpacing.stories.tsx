6ccddce334b50533e871c803ceb7216e82518c86
Borai van Uden <info@borai.nl>
2026-04-30 16:33:16 +0200

    feat: add common font stories

---
+++ packages/storybook/src/common-tokens/LetterSpacing.stories.tsx
@@ -0,0 +1,127 @@
+import { Meta, StoryObj } from '@storybook/react-vite';
+import tokens from '../../../../proprietary/tokens/dist/index.json';
+import { Title, Stories } from '@storybook/addon-docs/blocks';
+import React from 'react';
+import { Paragraph } from '@gemeente-denhaag/paragraph';
+import { path2css } from '../denhaag/util';
+
+type Story = StoryObj<typeof meta>;
+type Tokens = typeof tokens;
+type BasisTokens = Tokens['basis']['text']['letter-spacing'];
+
+const basisTokens: BasisTokens = tokens['basis']['text']['letter-spacing'];
+
+const meta: Meta = {
+  title: 'Common Tokens/Letter Spacing',
+  tags: ['autodocs', '!dev'],
+  component: Paragraph,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  parameters: {
+    chromatic: { disableSnapshot: true },
+    docs: {
+      canvas: { sourceState: 'none' },
+      page: () => (
+        <>
+          <Title />
+          <Stories title={<></>} />
+        </>
+      ),
+    },
+  },
+};
+
+export default meta;
+
+export const TextXS: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens.xs.path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens.xs.value },
+  },
+};
+
+export const TextSM: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens.sm.path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens.sm.value },
+  },
+};
+
+export const TextMD: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens.md.path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens.md.value },
+  },
+};
+
+export const TextLG: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens.lg.path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens.lg.value },
+  },
+};
+
+export const TextXL: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens.xl.path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens.xl.value },
+  },
+};
+
+export const Text2XL: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens['2xl'].path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens['2xl'].value },
+  },
+};
+
+export const Text3XL: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: path2css(basisTokens['3xl'].path),
+      },
+    },
+  },
+  args: {
+    style: { letterSpacing: basisTokens['3xl'].value },
+  },
+};
