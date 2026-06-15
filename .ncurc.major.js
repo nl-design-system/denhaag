6da973dc75e3afe9ffe23eaf80511b80d57359b1
YourivHDenHaag <108077148+YourivHDenHaag@users.noreply.github.com>
2026-06-15 10:04:06 +0200

    chore: dependency updates + build time optimalisation

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -1,5 +1,6 @@
 const minorConfig = require('./.ncurc.minor');
 
+// ESLint cannot be upgraded to v10 until eslint-plugin-react, eslint-plugin-jsx-a11y and eslint-plugin-import are supporting ESLint v10.
 module.exports = {
   ...minorConfig,
   reject: ['eslint', ...minorConfig.reject],
