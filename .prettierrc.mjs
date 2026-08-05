fbde942e84b3962217074f5add958e5d8eae58dc
Richard van Heuven van Staereling <richadr@users.noreply.github.com>
2026-08-05 16:02:36 +0200

    chore: use global options where possible

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,41 +2,43 @@
  * @type {import('prettier').Config}
  */
 export default {
+  printWidth: 120,
+  singleQuote: true,
   overrides: [
     {
       files: ['*.html'],
       options: {
         parser: 'html',
-        printWidth: 120,
       },
     },
     {
       files: ['*.js', '*.jsx', '*.mjs'],
       options: {
         parser: 'flow',
-        printWidth: 120,
-        singleQuote: true,
       },
     },
     {
       files: ['*.ts', '*.tsx'],
       options: {
         parser: 'typescript',
-        printWidth: 120,
-        singleQuote: true,
       },
     },
     {
       files: ['*.json'],
       options: {
         parser: 'json',
-        printWidth: 120,
       },
     },
     {
       files: ['package.json'],
       options: {
         parser: 'json-stringify',
+      },
+    },
+    {
+      files: ['*.css', '*.scss'],
+      options: {
+        singleQuote: false,
       },
     },
   ],
