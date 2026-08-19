71bb87b76b291b44da8eda6564bddee45367fb41
Borai van Uden <2182460+Borai@users.noreply.github.com>
2026-08-19 09:13:35 +0200

    feat: add option for link

--- components/QuickLinks/src/QuickLinks.tsx
+++ components/QuickLinks/src/QuickLinks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { AnchorHTMLAttributes, ComponentType } from 'react';
 import { NumberBadge } from '@gemeente-denhaag/number-badge';
 import { ArrowRightIcon } from '@gemeente-denhaag/icons';
 import QuickLinksBase from './QuickLinksBase';
@@ -6,23 +6,24 @@ import QuickLinksItem from './QuickLinksItem';
 import QuickLinksIconWrapper from './QuickLinksIconWrapper';
 import QuickLinksLabel from './QuickLinksLabel';
 
-export interface QuickLinksProps {
+export interface QuickLinksProps extends React.HTMLAttributes<HTMLDivElement> {
   items: {
     Icon?: React.ElementType;
     label: string;
     href: string;
     count?: number;
   }[];
+  Link?: ComponentType<AnchorHTMLAttributes<HTMLAnchorElement>>;
   Arrow?: React.ElementType;
 }
 
-export const QuickLinks = ({ items, Arrow = ArrowRightIcon }: QuickLinksProps) => {
+export const QuickLinks = ({ items, Link, Arrow = ArrowRightIcon, ...props }: QuickLinksProps) => {
   const hasIcon = items.some((item) => item.Icon !== undefined);
 
   return (
-    <QuickLinksBase>
+    <QuickLinksBase {...props}>
       {items.map(({ Icon, label, href, count }, index) => (
-        <QuickLinksItem key={index} href={href}>
+        <QuickLinksItem key={index} href={href} Link={Link}>
           {hasIcon && (
             <QuickLinksIconWrapper>{Icon && <Icon className="denhaag-quick-links__icon" />}</QuickLinksIconWrapper>
           )}
