a5fc971d4211aebc36fe7226a4865e75d2546fc7
Jiro Ghianni <118177951+jiromaykin@users.noreply.github.com>
2026-08-31 16:48:50 +0200

    feat(BaseCard): correct decoration structure as in Figma

--- components/BaseCard/src/BaseCardDecoration.tsx
+++ components/BaseCard/src/BaseCardDecoration.tsx
@@ -6,13 +6,7 @@ export type BaseCardDecorationProps = HTMLAttributes<HTMLDivElement>;
 export const BaseCardDecoration = ({ className, ...props }: BaseCardDecorationProps) => {
   const classNames = clsx('denhaag-base-card__decoration', className);
 
-  return (
-    <div className={classNames} {...props}>
-      <span className="denhaag-base-card__decoration-rim">
-        <span className="denhaag-base-card__decoration-paper" />
-      </span>
-    </div>
-  );
+  return <div className={classNames} {...props}></div>;
 };
 
 export default BaseCardDecoration;
